8f8694c37e72075577607df4f6ba02e85255e69c
Vesa Lahdenperä <vesa.lahdenpera@solita.fi>
2020-02-06 12:43:24 +0200

    AE-18: Add Autocomplete-component

---
+++ src/components/Autocomplete/Autocomplete.svelte
@@ -0,0 +1,32 @@
+<script>
+  import { slide } from 'svelte/transition';
+
+  import Input from '../Input/Input.svelte';
+  import DropdownList from '../DropdownList/DropdownList.svelte';
+
+  let showDropdown = false;
+
+  export let state = {};
+</script>
+
+<style>
+  .autocomplete {
+    @apply relative;
+  }
+  .dropdownwrapper {
+    @apply absolute top-auto left-0 right-0;
+  }
+</style>
+
+<div class="autocomplete">
+  <Input
+    {...state.input}
+    on:focus={_ => (showDropdown = true)}
+    on:blur={_ => (showDropdown = false)}
+    on:click={_ => (showDropdown = !showDropdown)} />
+  {#if showDropdown && state.dropdown}
+    <div class="dropdownwrapper">
+      <DropdownList state={state.dropdown} />
+    </div>
+  {/if}
+</div>
