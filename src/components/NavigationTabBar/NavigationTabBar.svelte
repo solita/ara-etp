6db45768ae58d93f52b3076aeb68f567dcf7b497
Vesa Lahdenperä <vesa.lahdenpera@solita.fi>
2020-02-11 12:39:03 +0200

    AE-19: Add NavigationTabBar

---
+++ src/components/NavigationTabBar/NavigationTabBar.svelte
@@ -0,0 +1,17 @@
+<script>
+  import NavigationTab from '../NavigationTab/NavigationTab.svelte';
+
+  export let links;
+</script>
+
+<style>
+  .navigationtabbar {
+    @apply flex;
+  }
+</style>
+
+<div class="navigationtabbar">
+  {#each links as link}
+    <NavigationTab text={link.text} />
+  {/each}
+</div>
