3dee2badf33ff54b2d7103c4399ba5c63203d852
Vesa Lahdenperä <vesa.lahdenpera@solita.fi>
2020-02-11 12:38:42 +0200

    AE-19: Add NavigationTab

--- src/Tailwindcss.svelte
+++ src/Tailwindcss.svelte
@@ -7,6 +7,12 @@
     src: url(../fonts/Montserrat-Regular.ttf) format('truetype');
   }
 
+  @font-face {
+    font-family: Montserrat-ExtraBold;
+    font-weight: 400;
+    src: url(../fonts/MontSerrat-ExtraBold.ttf) format('truetype');
+  }
+
   @tailwind components;
   @tailwind utilities;
 </style>
---
+++ src/components/NavigationTab/NavigationTab.svelte
@@ -0,0 +1,29 @@
+<script>
+  export let text;
+</script>
+
+<style>
+  .navigationtab {
+    @apply flex flex-grow relative mb-4 py-4 px-8 font-eb justify-center border-dark border-b-2 cursor-pointer uppercase;
+  }
+
+  .navigationtab:active {
+    @apply text-hover;
+  }
+
+  .navigationtab:active::after {
+    content: '';
+    @apply bg-hover absolute top-100 left-0 right-0 h-3;
+  }
+
+  .navigationtab:hover {
+    @apply border-hover;
+  }
+
+  .navigationtab:hover::after {
+    content: '';
+    @apply bg-hover absolute top-100 left-0 right-0 h-3;
+  }
+</style>
+
+<a class="navigationtab">{text}</a>
