c8325f2956b2142cc2f15be378c39408d4d8c29e
Vesa Lahdenperä <vesa.lahdenpera@solita.fi>
2020-02-11 13:29:18 +0200

    AE-19: Add common pseudo-selector rules for NavigationTab

--- src/components/NavigationTab/NavigationTab.svelte
+++ src/components/NavigationTab/NavigationTab.svelte
@@ -7,23 +7,20 @@
     @apply flex flex-grow relative mb-4 py-4 px-8 font-eb justify-center border-dark border-b-2 cursor-pointer uppercase;
   }
 
+  .navigationtab:active::after,
+  .navigationtab:hover::after,
+  .navigationtab:focus::after {
+    content: '';
+    @apply bg-hover absolute top-100 left-0 right-0 h-3;
+  }
+
+  .navigationtab:focus {
+    @apply outline-none;
+  }
+
   .navigationtab:active {
     @apply text-hover;
   }
-
-  .navigationtab:active::after {
-    content: '';
-    @apply bg-hover absolute top-100 left-0 right-0 h-3;
-  }
-
-  .navigationtab:hover {
-    @apply border-hover;
-  }
-
-  .navigationtab:hover::after {
-    content: '';
-    @apply bg-hover absolute top-100 left-0 right-0 h-3;
-  }
 </style>
 
-<a class="navigationtab">{text}</a>
+<a class="navigationtab" tabindex="0">{text}</a>
