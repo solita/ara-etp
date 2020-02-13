c8349337c48c784d71e3e9d8f706623489dbf027
Vesa Lahdenperä <vesa.lahdenpera@solita.fi>
2020-02-13 16:20:50 +0200

    AE-40: Add responsive layout for Company

--- src/components/Company/Company.svelte
+++ src/components/Company/Company.svelte
@@ -26,13 +26,13 @@
   <div class="w-full">
     <NavigationTabBar {links} />
   </div>
-  <div class="w-8/12">
+  <div class="xl:w-10/12 w-full">
     <H1 text="Perustiedot" />
-    <div class="flex flex-row py-4 -mx-4">
-      <div class="w-1/2 px-4">
+    <div class="flex lg:flex-row flex-col py-4 -mx-4">
+      <div class="lg:w-1/2 lg:py-0 w-full px-4 py-4">
         <Input id={'companyid'} label={$_('yritys.y-tunnus')} required={true} />
       </div>
-      <div class="w-1/2 px-4">
+      <div class="lg:w-1/2 lg:py-0 w-full px-4 py-4">
         <Input id={'companyname'} label={$_('yritys.nimi')} required={true} />
       </div>
     </div>
@@ -47,26 +47,26 @@
       <div class="py-4">
         <Input id={'billingaddress'} label={$_('yritys.katuosoite')} />
       </div>
-      <div class="flex flex-row py-4 -mx-4">
-        <div class="w-1/3 px-4">
+      <div class="flex lg:flex-row flex-col py-4 -mx-4">
+        <div class="lg:w-1/3 lg:py-0 w-full px-4 py-4">
           <Input id={'postnumber'} label={$_('yritys.postinumero')} />
         </div>
-        <div class="w-1/3 px-4">
+        <div class="lg:w-1/3 lg:py-0 w-full px-4 py-4">
           <Input
             id={'postarea'}
             label={$_('yritys.postitoimipaikka')}
             caret={true} />
         </div>
-        <div class="w-1/3 px-4">
+        <div class="lg:w-1/3 lg:py-0 w-full px-4 py-4">
           <Input id={'country'} label={$_('yritys.maa')} caret={true} />
         </div>
       </div>
     </div>
     <H1 text={$_('yritys.verkkolaskutusosoite')} />
-    <div class="w-1/4">
+    <div class="lg:w-1/4 w-full">
       <Input id={'networkbillingaddress'} label={$_('yritys.ovt-tunnus')} />
     </div>
-    <div class="flex -mx-4 pt-4">
+    <div class="flex -mx-4 pt-8">
       <div class="px-4">
         <Button text={$_('peruuta')} type={'secondary'} />
       </div>
