585ca941dc878705267cdb26d4072000f00b08f7
Vesa Lahdenperä <vesa.lahdenpera@solita.fi>
2020-02-13 14:34:20 +0200

    AE-40: Add localization to Company.svelte -component

--- src/components/Company/Company.svelte
+++ src/components/Company/Company.svelte
@@ -1,10 +1,15 @@
 <script>
+  import { _ } from '../../i18n';
+
   import NavigationTabBar from '../NavigationTabBar/NavigationTabBar.svelte';
   import H1 from '../H1/H1.svelte';
   import Input from '../Input/Input.svelte';
   import Button from '../Button/Button.svelte';
 
-  const links = [{ text: 'Uusi yritys' }, { text: 'Laatijat' }];
+  $: links = [
+    { text: $_('yritys.uusi_yritys') },
+    { text: $_('yritys.laatijat') }
+  ];
 </script>
 
 <style type="text/postcss">
@@ -21,40 +26,53 @@
   <div class="w-full">
     <NavigationTabBar {links} />
   </div>
-  <H1 text="Perustiedot" />
-  <div class="w-1/5">
-    <Input id={'companyname'} label={'Nimi'} required={true} />
-  </div>
-  <div class="w-1/12">
-    <Input id={'companyid'} label={'Y-Tunnus'} required={true} />
-  </div>
-  <H1 text={'Laskutusosoite'} />
-  <div class="flex flex-col w-8/12 -my-4">
+  <div class="w-8/12">
+    <H1 text="Perustiedot" />
+    <div class="flex flex-row py-4 -mx-4">
+      <div class="w-1/2 px-4">
+        <Input id={'companyid'} label={$_('yritys.y-tunnus')} required={true} />
+      </div>
+      <div class="w-1/2 px-4">
+        <Input id={'companyname'} label={$_('yritys.nimi')} required={true} />
+      </div>
+    </div>
     <div class="py-4">
-      <Input id={'billingaddress'} label={'Katuosoite'} />
+      <Input
+        id={'wwwaddress'}
+        label={$_('yritys.www-osoite')}
+        required={false} />
     </div>
-    <div class="py-4 flex flex-row -mx-4">
-      <div class="w-1/3 px-4">
-        <Input id={'postnumber'} label={'Postinumero'} />
+    <H1 text={$_('yritys.laskutusosoite')} />
+    <div class="flex flex-col -my-4">
+      <div class="py-4">
+        <Input id={'billingaddress'} label={$_('yritys.katuosoite')} />
       </div>
-      <div class="w-1/3 px-4">
-        <Input id={'postarea'} label={'Postitoimipaikka'} caret={true} />
-      </div>
-      <div class="w-1/3 px-4">
-        <Input id={'country'} label={'Maa'} caret={true} />
+      <div class="flex flex-row py-4 -mx-4">
+        <div class="w-1/3 px-4">
+          <Input id={'postnumber'} label={$_('yritys.postinumero')} />
+        </div>
+        <div class="w-1/3 px-4">
+          <Input
+            id={'postarea'}
+            label={$_('yritys.postitoimipaikka')}
+            caret={true} />
+        </div>
+        <div class="w-1/3 px-4">
+          <Input id={'country'} label={$_('yritys.maa')} caret={true} />
+        </div>
       </div>
     </div>
-  </div>
-  <H1 text={'Verkkolaskutusosoite'} />
-  <div class="w-1/4">
-    <Input id={'networkbillingaddress'} label={'OVT-Tunnus'} />
-  </div>
-  <div class="flex -mx-4 pt-4">
-    <div class="px-4">
-      <Button text={'Peruuta'} type={'secondary'} />
+    <H1 text={$_('yritys.verkkolaskutusosoite')} />
+    <div class="w-1/4">
+      <Input id={'networkbillingaddress'} label={$_('yritys.ovt-tunnus')} />
     </div>
-    <div class="px-4">
-      <Button text={'Tallenna'} />
+    <div class="flex -mx-4 pt-4">
+      <div class="px-4">
+        <Button text={$_('peruuta')} type={'secondary'} />
+      </div>
+      <div class="px-4">
+        <Button text={$_('tallenna')} />
+      </div>
     </div>
   </div>
 </section>
