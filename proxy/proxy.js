a2aa01fffee05dbbc110782e933ee6722d83a5ee
Vesa Lahdenperä <vesa.lahdenpera@solita.fi>
2020-02-14 09:45:13 +0200

    Add CSP-configuration for proxy

--- proxy/proxy.js
+++ proxy/proxy.js
@@ -1,11 +1,33 @@
 const express = require('express');
 const proxy = require('http-proxy-middleware');
+const csp = require('helmet-csp');
+const cors = require('cors');
 
 const config = require('./proxy.config');
 
 var app = express();
 
-app.use('/api', proxy({target: `http://localhost:${config['api']}`, changeOrigin: true}));
-app.use('/', proxy({target: `http://localhost:${config['static']}`, changeOrigin: true}));
+app.use(cors());
+
+app.use(
+  csp({
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'", 'localhost:35729'],
+      connectSrc: ["'self'", 'ws://localhost:35729'],
+      styleSrc: ["'self'", 'fonts.googleapis.com'],
+      fontSrc: ["'self'", 'fonts.gstatic.com']
+    }
+  })
+);
+
+app.use(
+  '/api',
+  proxy({ target: `http://localhost:${config['api']}`, changeOrigin: true })
+);
+app.use(
+  '/',
+  proxy({ target: `http://localhost:${config['static']}`, changeOrigin: true })
+);
 
 app.listen(3000);
