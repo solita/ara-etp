4a462a78e0daaecbd668cfd3828ea31e7f2be2e6
Vesa Lahdenperä <vesa.lahdenpera@solita.fi>
2020-02-19 07:06:05 +0200

    Update i18n library

--- src/i18n.js
+++ src/i18n.js
@@ -1,23 +1,27 @@
-import {init, locale, _, locales, register, isLoading} from 'svelte-i18n';
+import {
+  init,
+  locale,
+  _,
+  locales,
+  register,
+  isLoading,
+  getLocaleFromNavigator
+} from 'svelte-i18n';
 
 const fetchLocale = locale => {
   const localeUrl = `language/${locale}.json`;
   return fetch(localeUrl).then(response => response.json());
-}
+};
 
 const setupI18n = () => {
-  locale.set('fi');
-
   register('fi', () => fetchLocale('fi'));
   register('sv', () => fetchLocale('sv'));
   register('en', () => fetchLocale('en'));
 
   init({
     fallbackLocale: 'fi',
-    initialLocale: {
-      navigator: true
-    }
+    initialLocale: getLocaleFromNavigator()
   });
-}
+};
 
-export { setupI18n, _, locale, locales, isLoading};
+export { setupI18n, _, locale, locales, isLoading };
