c11765508ff2963512235ce3be7414b2e9f7a855
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2020-02-24 17:51:34 +0200

    AE-63 implement client redirecting if not authenticated

--- src/App.svelte
+++ src/App.svelte
@@ -5,17 +5,34 @@
 
   import { setupI18n, _, locale, locales, isLoading } from './i18n.js';
 
+  import * as R from 'ramda';
+
   import Header from './components/Header/Header.svelte';
   import Breadcrumb from './components/Breadcrumb/Breadcrumb.svelte';
   import Footer from './components/Footer/Footer.svelte';
 
   import Tailwindcss from './Tailwindcss.svelte';
 
-  import { fetchUser, currentUser } from './components/User/user';
+  import * as Future from './utils/future-utils';
+  import * as ErrorUtils from './utils/error-utils';
+  import * as UserUtils from './components/User/user-utils';
+  import { currentUserStore, errorStore } from './stores.js'
 
   setupI18n();
 
-  currentUser.set(fetchUser('current'));
+  Future.fork(R.compose(
+    errorStore.set,
+    ErrorUtils.httpError({})
+  ),
+  currentUserStore.set,
+  UserUtils.userFuture(fetch, 'current'));
+
+  $: {
+    if($errorStore && $errorStore.statusCode === 401 && !$currentUserStore) {
+      window.location.href= 'http://example.com';
+    }
+  }
+
 </script>
 
 <style type="text/postcss">
@@ -29,8 +46,9 @@
 </style>
 
 <Tailwindcss />
-
-{#if $locale && !$isLoading}
+{#if !$currentUserStore}
+  Loading...
+{:else if $locale && !$isLoading}
   <div class="container">
     <Header />
     <Breadcrumb />
