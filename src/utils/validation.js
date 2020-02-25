d760dc2e9a38eda11347988fc95f371b887bfc56
Mika Haapakorpi <mika.haapakorpi@solita.fi>
2020-02-25 15:07:32 +0200

    ae-40 - do not use non strict operators

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -4,7 +4,7 @@ export function ytunnusChecksum(ytunnus) {
   const digits = R.map(parseInt, R.slice(0, 7, ytunnus));
   const sum = R.reduce(R.add, 0, R.zipWith(R.multiply, digits, [7, 9, 10, 5, 8, 4, 2]));
   const remainder = sum % 11;
-  return remainder == 0 ? 0 : 11 - remainder;
+  return remainder === 0 ? 0 : 11 - remainder;
 }
 
 export function validYtunnus(ytunnus) {
