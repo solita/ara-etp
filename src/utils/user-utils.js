30d4dbd9c200ac12192ac2d092e784601f99d4f6
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2020-02-27 16:25:28 +0200

    AE-63 fix cyclic dependency to itself

--- src/utils/user-utils.js
+++ src/utils/user-utils.js
@@ -2,7 +2,6 @@ import * as R from 'ramda';
 import * as Future from './future-utils';
 import * as Fetch from './fetch-utils';
 import * as Error from './error-utils';
-import * as User from './user-utils';
 import { currentUserStore, errorStore } from '../stores';
 
 const userApi = `/api/users/`;
@@ -19,5 +18,5 @@ export const fetchAndStoreUser = () => {
     Error.httpError({})
   ),
   currentUserStore.set,
-  User.userFuture(fetch, 'current'));
+  userFuture(fetch, 'current'));
 }
