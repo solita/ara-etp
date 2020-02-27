171424ea6e71b8bb1f68085f4e0bea3f6166cc9e
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2020-02-27 15:55:13 +0200

    AE-63 implement probably non-working code for debugging Cognito

--- src/components/Login/Login.svelte
+++ src/components/Login/Login.svelte
@@ -3,7 +3,33 @@
 
   export let redirectTimeout = 0;
 
-  RedirectUtils.redirectAfterTimeout(RedirectUtils.loginPageUrl(), redirectTimeout);
+  // TODO this should also come from config.json similar to client id.
+  export const loginPageBaseUrl = "https://kehitys-energiatodistuspalvelu-com.auth.eu-central-1.amazoncognito.com/login";
+
+  // TODO should include state query parameter to cognito and verify it upon returning.
+  export const loginPageUrl = () => `${loginPageBaseUrl}?client_id=${CONFIG.COGNITOCLIENTID}&redirect_uri=${encodeURIComponent(document.location.href)}&response_type=code`;
+
+  // TODO should come from config.json as well.
+  export const tokenUrl = code => `https://kehitys.energiatodistuspalvelu.com/oauth2/idpresponse?code=${code}`
+
+  export const urlParams = new URLSearchParams(window.location.search);
+  export const oauthCode = urlParams.get('code');
+
+  // Test code for seeing how Cognito works...
+  if(!oauthCode) {
+    RedirectUtils.redirectAfterTimeout(loginPageUrl(), redirectTimeout);
+  }
+  else {
+    const response = fetch(tokenUrl(oauthCode), {method: 'POST'})
+      .then(function(response) {
+        console.log(response);
+        document.cookie = `AWSELBAuthSessionCookie=${response.body.access_token}`
+      })
+      .catch(function(e) {
+        console.log(e);
+      });
+  }
+
 </script>
 
 <style type="text/postcss">
--- src/utils/redirect-utils.js
+++ src/utils/redirect-utils.js
@@ -1,12 +1,6 @@
-// TODO this should also come from config.json similar to client id.
-export const loginPageBaseUrl = "https://kehitys-energiatodistuspalvelu-com.auth.eu-central-1.amazoncognito.com/login";
-
 export const redirect = url => window.location.href = url;
 
 export const redirectAfterTimeout = (url, timeout) => setTimeout(() =>
   redirect(url),
   timeout
 );
-
-// TODO should include state query parameter to cognito and verify it upon returning.
-export const loginPageUrl = () => `${loginPageBaseUrl}?client_id=${CONFIG.COGNITOCLIENTID}&redirect_uri=${encodeURIComponent(document.location.href)}&response_type=code`;
