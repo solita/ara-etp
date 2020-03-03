058d329ff713a134174bb996b58ca3dd8d661c26
Toni Pulkkinen <toni.pulkkinen@solita.fi>
2020-03-03 14:19:56 +0200

    AE-34: Basic table component

---
+++ src/components/Table/Table.svelte
@@ -0,0 +1,76 @@
+<script>
+  import * as R from 'ramda';
+  import Pagination from './../Pagination/Pagination';
+  import TableColumnAction from './TableColumnAction';
+  import TableColumnDefault from './TableColumnDefault';
+  export let fields = [];
+  export let tablecontents = [];
+</script>
+
+<style>
+  table {
+    @apply w-full;
+  }
+
+  th {
+    @apply px-4 py-2 text-center;
+  }
+
+  tr {
+    @apply px-4 py-2;
+  }
+
+  td {
+    @apply text-center;
+  }
+
+  tr:nth-child(even) {
+    @apply bg-background;
+  }
+
+  .pagination {
+    @apply mt-4 flex justify-center;
+  }
+</style>
+
+<table>
+  <thead>
+    <tr>
+      {#each fields as field}
+        <th>
+          {#if field.title}{field.title}{/if}
+          {#if R.equals(field.sort, 'ascend')}
+            <span class="font-icon">keyboard_arrow_down</span>
+          {/if}
+          {#if R.equals(field.sort, 'descend')}
+            <span class="font-icon">keyboard_arrow_up</span>
+          {/if}
+        </th>
+      {/each}
+    </tr>
+  </thead>
+  <tbody>
+    {#each tablecontents as row, index}
+      <tr>
+        {#each fields as field}
+          <td>
+            {#if R.equals(field.type, 'action')}
+              <svelte:component
+                this={TableColumnAction}
+                actions={field.actions}
+                {index} />
+            {:else}
+              <svelte:component
+                this={TableColumnDefault}
+                value={R.prop(field.id, row)} />
+            {/if}
+          </td>
+        {/each}
+      </tr>
+    {/each}
+  </tbody>
+</table>
+
+<div class="pagination">
+  <Pagination pageCount={10} pageNum={5} />
+</div>
---
+++ src/components/Table/TableColumnAction.svelte
@@ -0,0 +1,22 @@
+<script>
+  import * as R from 'ramda';
+
+  export let actions;
+  export let index;
+</script>
+
+<style type="text/postcss">
+  span {
+    @apply cursor-pointer;
+  }
+</style>
+
+{#each actions as action}
+  <div>
+    {#if R.equals(action.type, 'remove')}
+      <span class="font-icon" on:click={e => action.update(index)}>
+        remove_circle_outline
+      </span>
+    {/if}
+  </div>
+{/each}
