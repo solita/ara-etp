3e701bba92f8785754f912651cf7910c02567718
Vesa Lahdenperä <vesa.lahdenpera@solita.fi>
2020-03-04 10:45:01 +0200

    Add aliases for storybook imports

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,40 +1,56 @@
 const R = require('ramda');
+const path = require('path');
 
 const configModuleRulesLens = R.lensPath(['module', 'rules']);
 
-const ruleLens = (config, testString) => R.compose(
-  configModuleRulesLens,
+const ruleLens = (config, testString) =>
   R.compose(
-    R.lensIndex,
-    R.findIndex(R.compose(R.test(R.__, testString), R.prop('test'))),
-    R.view(configModuleRulesLens)
-  )(config)
-);
+    configModuleRulesLens,
+    R.compose(
+      R.lensIndex,
+      R.findIndex(R.compose(R.test(R.__, testString), R.prop('test'))),
+      R.view(configModuleRulesLens)
+    )(config)
+  );
 
 module.exports = async ({ config, mode }) => {
+  config.resolve.alias = {
+    ...(config.resolve.alias || {}),
+    '@Component': path.resolve(__dirname, '../src/components'),
+    '@Utility': path.resolve(__dirname, '../src/utils'),
+    '@Language': path.resolve(__dirname, '../src/language'),
+    '@': path.resolve(__dirname, '../src')
+  };
+
   config.module.rules.push({
     test: /\.css$/,
-    loaders: [{
-      loader: 'postcss-loader',
-      options: {
-        sourceMap: true,
-        config: {
-          path: './.storybook/'
+    loaders: [
+      {
+        loader: 'postcss-loader',
+        options: {
+          sourceMap: true,
+          config: {
+            path: './.storybook/'
+          }
         }
       }
-    }]
+    ]
   });
 
   const svelteLens = ruleLens(config, '.svelte');
 
-  return R.over(svelteLens, R.mergeLeft({
-    options: { 
-      preprocess: require('svelte-preprocess')({
-        postcss: {
-          sourceMap: true,
-          path: './.storybook/'
-        }
-      })
-    }
-  }), config);
-}
+  return R.over(
+    svelteLens,
+    R.mergeLeft({
+      options: {
+        preprocess: require('svelte-preprocess')({
+          postcss: {
+            sourceMap: true,
+            path: './.storybook/'
+          }
+        })
+      }
+    }),
+    config
+  );
+};
