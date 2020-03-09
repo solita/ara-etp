db942fb6817dc60a7c173369061a7b64f5724195
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2020-03-09 17:51:20 +0200

    AE-78 add i18n localization decorator for storybooks

---
+++ .storybook/Localized.svelte
@@ -0,0 +1,6 @@
+<script>
+  import { setupI18n, _ } from '@Language/i18n';
+  setupI18n();
+</script>
+
+<slot></slot>
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,18 @@
-import { configure, addParameters, addDecorator } from '@storybook/svelte';
-import { withA11y } from '@storybook/addon-a11y';
 import './tailwind.css';
+import { makeDecorator } from '@storybook/addons';
+import { addParameters, addDecorator } from '@storybook/svelte';
+import { withA11y } from '@storybook/addon-a11y';
+import Localized from './Localized.svelte'
 
 addDecorator(withA11y);
 addParameters({ viewport: { defaultViewport: 'responsive' } });
+
+const localizationDecorator = makeDecorator({
+  wrapper: (storyFn, context) => {
+    const story = storyFn(context);
+    story.Wrapper = Localized;
+    return story;
+  },
+});
+
+addDecorator(localizationDecorator);
