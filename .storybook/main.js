629c1c5b29a4dacab9217bb46fbc87539691df9f
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2020-03-09 15:04:59 +0200

    Migrate to new storybook configuration system

---
+++ .storybook/main.js
@@ -0,0 +1,9 @@
+module.exports = {
+  stories: ['../src/**/*.stories.js'],
+  addons: [
+      '@storybook/addon-actions/register',
+      '@storybook/addon-links/register',
+      '@storybook/addon-a11y/register',
+      '@storybook/addon-viewport/register'
+    ]
+}
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,6 @@
 import { configure, addParameters, addDecorator } from '@storybook/svelte';
 import { withA11y } from '@storybook/addon-a11y';
-
 import './tailwind.css';
-
-configure(require.context('../src', true, /\.stories\.js$/), module);
 
 addDecorator(withA11y);
 addParameters({ viewport: { defaultViewport: 'responsive' } });
