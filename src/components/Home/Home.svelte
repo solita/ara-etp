8fa81ea4b9fcba444038f7728804d521c58cdb9a
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2020-03-10 15:31:39 +0200

    AE-84 add dummy home view

---
+++ src/components/Home/Home.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { _ } from '@Language/i18n';
+  import { currentUserStore } from '@/stores';
+  import Link from '@Component/Link/Link';
+  // This component should use currentUserStore to check if the user role and if
+  // the user is laatija. Then render a real component based on that.
+</script>
+
+<style type="text/postcss">
+</style>
+
+<div>
+  <h1 class="text-3xl font-fold">Laatijan etusivu</h1>
+  <Link href="#/yritys/new" text="{$_('yritys.uusi_yritys')}" />
+</div>
--- src/components/routes.js
+++ src/components/routes.js
@@ -1,8 +1,10 @@
+import Home from '@Component/Home/Home';
 import NewYritys from '@Component/Yritys/NewYritys';
 import ExistingYritys from '@Component/Yritys/ExistingYritys';
 import NotFound from '@Component/NotFound/NotFound';
 
 export const routes = {
+  '/': Home,
   '/yritys/new': NewYritys,
   '/yritys/:id': ExistingYritys,
   '*': NotFound
