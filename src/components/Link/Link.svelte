e4ceb06e535c6d9eb2f57187910d2131498f56df
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2020-03-10 15:04:44 +0200

    AE-84 replace applied styles with classes directly in html in Link component

--- src/components/Link/Link.svelte
+++ src/components/Link/Link.svelte
@@ -4,13 +4,6 @@
 </script>
 
 <style type="text/postcss">
-  a {
-    @apply text-link;
-  }
-
-  a:hover {
-    @apply underline;
-  }
 </style>
 
-<a href="{href}">{text}</a>
+<a class="text-link hover:underline" href="{href}">{text}</a>
