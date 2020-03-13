9ef35e5abefae7151a8b8349b1061c3d286d7793
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2020-03-13 17:03:49 +0200

    AE-36 add store for breadcrumb

--- src/stores.js
+++ src/stores.js
@@ -3,5 +3,5 @@ import * as Either from '@Utility/either-utils';
 
 export const currentUserStore = writable();
 export const errorStore = writable();
-
 export const countryStore = writable(Either.Left('Not initialized'));
+export const breadcrumbStore = writable([]);
