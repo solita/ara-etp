5bcdd1bd1a6f615ddebee81031387b98c5f5d3ae
Vesa Lahdenperä <vesa.lahdenpera@solita.fi>
2020-04-16 10:18:49 +0300

    Refine HR-component styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,10 +26,14 @@ module.exports = {
       },
       borderWidth: {
         '1': '1px',
-        '3': '3px'
+        '3': '3px',
+        '7': '7px'
       },
       inset: {
         '100': '100%'
+      },
+      opacity: {
+        '15': '0.15'
       },
       boxShadow: {
         'hover-2-primary': '0 0.5em #538000',
