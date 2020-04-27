817625854e1343958688e60cd6fe29eb776462aa
Mika Haapakorpi <mika.haapakorpi@solita.fi>
2020-04-27 13:29:10 +0300

    AE-122 - move toolbar only to et forms

--- src/components/Energiatodistus/Energiatodistus.svelte
+++ src/components/Energiatodistus/Energiatodistus.svelte
@@ -6,7 +6,6 @@
   import Energiatodistukset from './Energiatodistukset';
   import Allekirjoitus from './Allekirjoitus';
   import FlashMessage from '@Component/FlashMessage/FlashMessage';
-  import ToolBar from '@Component/ToolBar/ToolBar';
   import { flashMessageStore } from '@/stores';
 
   const prefix = '/energiatodistus';
@@ -21,15 +20,7 @@
 <svelte:window
   on:hashchange={_ => flashMessageStore.flush('Energiatodistus')} />
 
-<div class="w-full relative flex">
-  <div class="w-5/6">
-    <Router {routes} {prefix} />
-  </div>
-  <div class="sticky top-3em w-1/6 self-start flex justify-end">
-    <ToolBar />
-  </div>
-</div>
-
+<Router {routes} {prefix} />
 <div class="w-full min-h-3em">
   <FlashMessage module={'Energiatodistus'} />
 </div>
