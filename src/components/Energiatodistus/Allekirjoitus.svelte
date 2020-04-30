0820ae7c6fe6a8b3a88802484ab7d521c410bfbb
Vesa Lahdenperä <vesa.lahdenpera@solita.fi>
2020-04-30 10:23:29 +0300

    AE-98: Implement new signing process

--- src/components/Energiatodistus/Allekirjoitus.svelte
+++ src/components/Energiatodistus/Allekirjoitus.svelte
@@ -35,6 +35,10 @@
 
   let signatureStatus = statuses.NOT_STARTED;
 
+  const etFuture = R.curry((apiMethod, params) =>
+    R.converge(apiMethod(fetch), [R.prop('version'), R.prop('id')])(params)
+  );
+
   Future.fork(
     e => console.error(e),
     ([info, et]) => {
@@ -45,35 +49,34 @@
     R.compose(
       Future.parallel(5),
       R.append(R.__, [sgApi.versionInfo(fetch)]),
-      R.converge(etApi.getEnergiatodistusById(fetch), [
-        R.prop('version'),
-        R.prop('id')
-      ])
+      etFuture(etApi.getEnergiatodistusById)
     )(params)
   );
 
   const signProcess = R.compose(
     Future.fork(
-      _ => (signatureStatus = statuses.FAILURE),
-      _ => (signatureStatus = statuses.SUCCESS)
-    ),
-    R.map(
-      R.tap(_ => {
+      _ => {
         overlay = false;
-      })
+        signatureStatus = statuses.FAILURE;
+      },
+      _ => {
+        overlay = false;
+        signatureStatus = statuses.SUCCESS;
+      }
     ),
-    R.chain(etApi.signEnergiatodistus(fetch, params.version, params.id)),
+    R.chain(_ => etFuture(etApi.finishSign, params)),
+    R.chain(etFuture(etApi.signPdf, params)),
     R.map(R.tap(_ => (signatureStatus = statuses.SIGNATURE))),
     R.chain(sgApi.getSignature(fetch)),
     R.map(R.tap(_ => (signatureStatus = statuses.DIGEST))),
-    R.converge(etApi.getEnergiatodistusDigestById(fetch), [
-      R.prop('version'),
-      R.prop('id')
-    ]),
-    R.tap(_ => {
-      overlay = true;
-      signatureStatus = statuses.STARTED;
-    })
+    R.chain(_ => etFuture(etApi.digest, params)),
+    R.map(
+      R.tap(_ => {
+        overlay = true;
+        signatureStatus = statuses.STARTED;
+      })
+    ),
+    etFuture(etApi.startSign)
   );
 </script>
 
@@ -82,30 +85,44 @@
 </style>
 
 <H1 text={'Allekirjoittaminen'} />
-
-<Overlay {overlay}>
-  <form slot="content" on:submit|preventDefault={() => signProcess(params)}>
-    {#if signatureStatus === statuses.NOT_STARTED}
-      Tarkistetaan allekirjoitusohjelmiston olemassaoloa...
-    {:else if signatureStatus === statuses.MPOLLUX}
-      <span>
-        {R.compose( Maybe.orSome(''), R.map(et => `Allekirjoitetaan energiatodistusta ${et.id}`) )(energiatodistus)}
-      </span>
-      <Button text="Siirry allekirjoittamaan" type="submit" />
-    {:else if signatureStatus === statuses.SUCCESS}
-      Allekirjoitus onnistui.
-    {:else if signatureStatus === statuses.FAILURE}
-      Allekirjoitus epäonnistui
-    {:else}Allekirjoitetaan...{/if}
-  </form>
-  <div slot="overlay-content" class="flex flex-col items-center justify-center">
-    <Spinner />
-    <span>
-      {#if signatureStatus === statuses.STARTED}
-        Allekirjoitettavaa tiedostoa muodostetaan.
-      {:else if signatureStatus === statuses.DIGEST || signatureStatus === statuses.SIGNATURE}
-        Tiedostoa allekirjoitetaan.
+{#if R.allPass([
+  Maybe.isSome,
+  R.compose(
+    Maybe.isSome,
+    R.chain(R.prop('allekirjoitusaika'))
+  )
+])(energiatodistus)}
+  Energiatodistus on jo allekirjoitettu.
+{:else}
+  <Overlay {overlay}>
+    <form slot="content" on:submit|preventDefault={() => signProcess(params)}>
+      {#if signatureStatus === statuses.NOT_STARTED}
+        <span>Tarkistetaan allekirjoitusohjelmiston olemassaoloa...</span>
+      {:else if signatureStatus === statuses.MPOLLUX}
+        <span>
+          {R.compose( Maybe.orSome(''), R.map(et => `Allekirjoitetaan energiatodistusta ${et.id}`) )(energiatodistus)}
+        </span>
+        <Button text="Siirry allekirjoittamaan" type="submit" />
+      {:else if signatureStatus === statuses.SUCCESS}
+        <span>Allekirjoitus onnistui.</span>
+      {:else if signatureStatus === statuses.FAILURE}
+        <span>Allekirjoitus epäonnistui</span>
+        <Button text="Siirry allekirjoittamaan" type="submit" />
+      {:else}
+        <span>Allekirjoitetaan...</span>
       {/if}
-    </span>
-  </div>
-</Overlay>
+    </form>
+    <div
+      slot="overlay-content"
+      class="flex flex-col items-center justify-center">
+      <Spinner />
+      <span>
+        {#if signatureStatus === statuses.STARTED}
+          Allekirjoitettavaa tiedostoa muodostetaan.
+        {:else if signatureStatus === statuses.DIGEST || signatureStatus === statuses.SIGNATURE}
+          Tiedostoa allekirjoitetaan.
+        {/if}
+      </span>
+    </div>
+  </Overlay>
+{/if}
