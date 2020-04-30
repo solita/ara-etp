737b5b3935c35ebef4f859446ada496d086815f1
Toni Pulkkinen <toni.pulkkinen@solita.fi>
2020-04-30 11:41:13 +0300

    AE-164: basic menu for logout

--- src/components/Header/Header.svelte
+++ src/components/Header/Header.svelte
@@ -1,5 +1,5 @@
 <script>
-
+  let showDropdown = false;
 </script>
 
 <style type="text/postcss">
@@ -13,5 +13,21 @@
   <div>Tähän haku</div>
   <div>Laatijan ohjeet</div>
   <div>Työkalut</div>
-  <div>Laila Laatija</div>
+  <div class="relative">
+    <span
+      class="cursor-pointer"
+      on:click={() => (showDropdown = !showDropdown)}>
+      Laila Laatija
+    </span>
+    <span class="material-icons absolute">keyboard_arrow_down</span>
+    {#if showDropdown}
+      <div class="absolute mt-2 py-2 w-48 bg-light shadow-xl">
+        <a
+          class="px-4 py-2 text-dark text-center font-light normal-case w-full"
+          href="/api/logout">
+          Kirjaudu ulos
+        </a>
+      </div>
+    {/if}
+  </div>
 </header>
