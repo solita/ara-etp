8d4013a66aa7d184376764ebf9e67c52a10ef646
Vesa Lahdenperä <vesa.lahdenpera@solita.fi>
2020-05-04 14:48:09 +0300

    Fix patevyydentoteaja-infoscreen to show placeholder-text

--- src/components/Kayttaja/MyInfo.svelte
+++ src/components/Kayttaja/MyInfo.svelte
@@ -3,6 +3,10 @@
   import { replace } from 'svelte-spa-router';
   import { currentUserStore } from '@/stores';
 
+  import * as Maybe from '@Utility/maybe-utils';
+
+  import Patevyydentoteaja from '@Component/Kayttaja/Patevyydentoteaja';
+
   const routeForKayttaja = kayttaja => {
     if (kayttaja.rooli === 0) {
       replace(`/laatija/${kayttaja.id}`);
@@ -13,3 +17,7 @@
 
   R.forEach(routeForKayttaja, $currentUserStore);
 </script>
+
+{#if R.compose( Maybe.isSome, R.filter(R.compose( R.equals(1), R.prop('rooli') )) )($currentUserStore)}
+  Pätevyydentoteajan tiedot tulevat tänne.
+{/if}
