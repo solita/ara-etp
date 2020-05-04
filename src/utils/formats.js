9d0106733782cb973f25de4bce514cbf84aca5c1
Mika Haapakorpi <mika.haapakorpi@solita.fi>
2020-05-04 11:52:54 +0300

    AE-190 - use fi date formats

--- src/utils/formats.js
+++ src/utils/formats.js
@@ -1,3 +1,3 @@
 import * as dfns from 'date-fns';
 
-export const formatTimeInstant = time => dfns.format(time, 'yyyy.MM.dd HH:mm:ss')
+export const formatTimeInstant = time => dfns.format(time, 'd.M.yyyy HH:mm:ss')
