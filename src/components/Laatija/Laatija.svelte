e400a74974d3a194660b80ca4a65bc142e226f9d
Vesa Lahdenperä <vesa.lahdenpera@solita.fi>
2020-05-04 13:52:20 +0300

    Fix navigation-links for patevyydentoteaja

--- src/components/Laatija/Laatija.svelte
+++ src/components/Laatija/Laatija.svelte
@@ -67,7 +67,7 @@
     R.filter(Maybe.isSome)
   )([
     Maybe.of([{ text: '...', href: '' }]),
-    R.map(Navigation.linksForLaatija, $currentUserStore)
+    R.map(Navigation.linksForKayttaja, $currentUserStore)
   ]);
 </script>
 
