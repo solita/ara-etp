52c004d95b00db3abc12136ee006a9dd6782d33c
Vesa Lahdenperä <vesa.lahdenpera@solita.fi>
2020-06-01 14:28:56 +0300

    AE-206: Refactor NavigationTabBar

--- src/utils/ramda-utils.js
+++ src/utils/ramda-utils.js
@@ -3,3 +3,7 @@ import * as R from 'ramda';
 export const inRangeInclusive = R.curry((low, high, value) =>
   R.allPass([R.gte(high), R.lte(low)])(value)
 );
+
+export const fillAndTake = R.curry((n, item, arr) =>
+  R.compose(R.take(n), R.concat(arr), R.times)(item, n)
+);
