3983513df50c786e9fe06c3388796804b5892964
Vesa Lahdenperä <vesa.lahdenpera@solita.fi>
2020-07-06 12:15:20 +0300

    Enhancing querybuilder

--- src/components/Energiatodistus/querybuilder/queryblock.svelte
+++ src/components/Energiatodistus/querybuilder/queryblock.svelte
@@ -6,27 +6,32 @@
   import { _ } from '@Language/i18n';
 
   export let model;
-  export let lens = R.identity;
-  export let showConjugation = false;
-  export let block = [];
+  export let lens;
 
-  export let remove;
+  const conjunctionLens = R.compose(
+    lens,
+    R.lensProp('conjunction')
+  );
 
-  let group = 'and';
-
-  $: value = R.view(lens, model);
-
-  $: console.log(value);
+  $: ({ conjunction, block } = R.view(lens, model));
 </script>
 
 <style>
 
 </style>
 
-{#if showConjugation}
+{#if Maybe.isSome(conjunction)}
   <div class="flex justify-between w-1/6 my-10">
-    <Radio bind:group value={'and'} label={$_('energiatodistus.haku.and')} />
-    <Radio bind:group value={'or'} label={$_('energiatodistus.haku.or')} />
+    <Radio
+      bind:group={conjunction}
+      value={Maybe.Some('and')}
+      label={$_('energiatodistus.haku.and')}
+      on:click={evt => (model = R.set(conjunctionLens, Maybe.Some('and'), model))} />
+    <Radio
+      bind:group={conjunction}
+      value={Maybe.Some('or')}
+      label={$_('energiatodistus.haku.or')}
+      on:click={evt => (model = R.set(conjunctionLens, Maybe.Some('or'), model))} />
   </div>
 {/if}
 
--- src/components/Energiatodistus/querybuilder/querybuilder.svelte
+++ src/components/Energiatodistus/querybuilder/querybuilder.svelte
@@ -1,12 +1,42 @@
 <script>
+  import { slide } from 'svelte/transition';
+  import { flip } from 'svelte/animate';
+
   import qs from 'qs';
+  import { v4 as uuidv4 } from 'uuid';
   import * as R from 'ramda';
+  import * as Maybe from '@Utility/maybe-utils';
 
   import QueryBlock from './queryblock';
 
   export let where;
 
-  $: console.log(where);
+  const firstConjunctionLens = R.compose(
+    R.lensIndex(0),
+    R.lensProp('conjunction')
+  );
+
+  $: parsedWhere = R.compose(
+    R.map(([conjunction, block]) => ({ conjunction, block, uuid: uuidv4() })),
+    R.over(R.lensIndex(0), R.pair(Maybe.None())),
+    R.unnest,
+    R.converge(R.concat, [
+      R.take(1),
+      R.compose(
+        R.map(R.over(R.lensIndex(0), R.pair(Maybe.Some('or')))),
+        R.tail
+      )
+    ]),
+    R.map(
+      R.converge(R.concat, [
+        R.take(1),
+        R.compose(
+          R.map(R.pair(Maybe.Some('and'))),
+          R.tail
+        )
+      ])
+    )
+  )(where);
 </script>
 
 <style>
@@ -14,16 +44,15 @@
 </style>
 
 <div class="flex flex-col w-full">
-  {#each where as or, i}
-    {#each or as and, j}
-      <QueryBlock
-        bind:model={where}
-        lens={R.compose( R.lensIndex(i), R.lensIndex(j) )}
-        showConjugation={!(i === 0 && j === 0)} />
+  {#each parsedWhere as { conjunction, block, uuid }, index (uuid)}
+    <div
+      transition:slide|local={{ duration: 200 }}
+      animate:flip={{ duration: 200 }}>
+      <QueryBlock bind:model={parsedWhere} lens={R.lensIndex(index)} />
       <span
-        on:click={_ => (where = R.compose( R.filter(R.length), R.over(R.lensIndex(i), R.remove(j, 1)) )(where))}>
+        on:click={_ => (parsedWhere = R.compose( R.tap(console.log), R.set(firstConjunctionLens, Maybe.None()), R.tap(console.log), R.remove(index, 1) )(parsedWhere))}>
         X
       </span>
-    {/each}
+    </div>
   {/each}
 </div>
