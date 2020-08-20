6ba129a5612d5a5cb9ef56edc254770abe633ffb
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2020-08-20 09:56:40 +0300

    AE-411 add kuukausierittely field to empty energiatodistus

--- src/components/Energiatodistus/empty.js
+++ src/components/Energiatodistus/empty.js
@@ -165,6 +165,7 @@ export const energiatodistus2018 = _ => ({
       muusahko: Maybe.None(),
       lampopumppu: Maybe.None()
     },
+    'kuukausierittely': Maybe.None(),
     'tekniset-jarjestelmat': {
       'tilojen-lammitys': emptySahkoLampo(),
       'tuloilman-lammitys': emptySahkoLampo(),
