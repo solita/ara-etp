4eab475c2d591d6acd716d7d5ba9739d1aaefeff
Mika Haapakorpi <mika.haapakorpi@solita.fi>
2020-09-03 12:49:02 +0300

    remove copy paste from cached fetches

--- src/components/Geo/geo-api.js
+++ src/components/Geo/geo-api.js
@@ -1,17 +1,4 @@
-import * as R from 'ramda';
-import * as Future from '@Utility/future-utils';
 import * as Fetch from '@Utility/fetch-utils';
 
-const api = 'api/private';
-
-export const countries = R.compose(
-  Future.cache,
-  Fetch.responseAsJson,
-  Future.encaseP(Fetch.getFetch(fetch))
-)(api + '/countries/');
-
-export const toimintaalueet = R.compose(
-  Future.cache,
-  Fetch.responseAsJson,
-  Future.encaseP(Fetch.getFetch(fetch))
-)(api + '/toimintaalueet/');
+export const countries = Fetch.cached(fetch, '/countries/');
+export const toimintaalueet = Fetch.cached(fetch, '/toimintaalueet/');
