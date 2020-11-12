17e334cecda1a94d100597a42df3e17991a5b8ae
Mika Haapakorpi <mika.haapakorpi@solita.fi>
2020-11-12 10:31:33 +0200

    AE-657 - remove depracated api functions from kayttaja-utils

---
+++ src/components/Kayttaja/kayttaja-api_test.js
@@ -0,0 +1,112 @@
+import { assert } from 'chai';
+import * as Future from '@Utility/future-utils';
+import * as api from './kayttaja-api';
+import * as R from 'ramda';
+
+describe('Kayttaja-api-suite: ', () => {
+  describe('kayttajaForId', () => {
+    it('should return proper url for given id', () => {
+      const id = 1234;
+      const expected = 'api/private/kayttajat/1234';
+
+      assert.equal(api.url.id(id), expected);
+    });
+  });
+
+  describe('laatijaForId', () => {
+    it('should return proper url for given id', () => {
+      const id = 1234;
+      const expected = 'api/private/kayttajat/1234/laatija';
+
+      assert.equal(api.url.laatija(id), expected, );
+    });
+  });
+
+  describe('kayttajaFuture', () => {
+    it('should return proper kayttaja', done => {
+      const expected = {
+        id: 1,
+        nimi: 'Paavo Pesusieni'
+      };
+
+      const response = {
+        status: 200,
+        ok: true,
+        json: () => new Promise((resolve, _) => resolve(expected))
+      };
+
+      const fetch = url => new Promise((resolve, _) => resolve(response));
+
+      Future.fork(
+        _ => {},
+        kayttaja => {
+          assert.deepEqual(expected, kayttaja);
+          done();
+        },
+        api.getKayttajaById(fetch, 1234)
+      );
+    });
+
+    it('should fail when rejected', done => {
+      const response = {
+        status: 404,
+        ok: false
+      };
+
+      const fetch = url => new Promise((_, reject) => reject(response));
+
+      Future.fork(
+        reject => {
+          assert.equal(404, reject.status);
+          done();
+        },
+        _ => {},
+        api.getKayttajaById(fetch, 1234)
+      );
+    });
+  });
+
+  describe('laatijaFuture', () => {
+    it('should return proper laatija', done => {
+      const expected = {
+        id: 1,
+        nimi: 'Laatija'
+      };
+
+      const response = {
+        status: 200,
+        ok: true,
+        json: () => new Promise((resolve, _) => resolve(expected))
+      };
+
+      const fetch = url => new Promise((resolve, _) => resolve(response));
+
+      Future.fork(
+        _ => {},
+        laatija => {
+          assert.deepEqual(R.pick(['id', 'nimi'], laatija), expected);
+          done();
+        },
+        api.getLaatijaById(fetch, 1234)
+      );
+    });
+
+    it('should fail when rejected', done => {
+      const response = {
+        status: 400,
+        ok: false
+      };
+
+      const fetch = url => new Promise((_, reject) => reject(response));
+
+      Future.fork(
+        reject => {
+          assert.equal(400, reject.status);
+          done();
+        },
+        _ => {},
+        api.getLaatijaById(fetch, 1234)
+      );
+    });
+  });
+});
--- src/components/Kayttaja/kayttaja-utils.js
+++ src/components/Kayttaja/kayttaja-utils.js
@@ -1,53 +1,8 @@
 import * as R from 'ramda';
 
-import * as Future from '@Utility/future-utils';
-import * as Fetch from '@Utility/fetch-utils';
-import * as Maybe from '@Utility/maybe-utils';
-
 export const laatijaRole = 0;
 export const patevyydentoteajaRole = 1;
 export const paakayttajaRole = 2;
-
-export const currentKayttajaApi = `/api/private/whoami`;
-
-export const currentKayttajaFuture = fetch =>
-  Fetch.fetchUrl(fetch, currentKayttajaApi);
-
-export const kayttajaApi = `/api/private/kayttajat`;
-
-export const kayttajaForId = id => `${kayttajaApi}/${id}`;
-
-export const laatijaForKayttajaId = id => `${kayttajaForId(id)}/laatija`;
-
-export const deserialize = R.evolve({
-  login: R.compose(R.map(Date.parse), Maybe.fromNull),
-  cognitoid: Maybe.fromNull,
-  henkilotunnus: Maybe.fromNull
-});
-
-export const kayttajaFuture = R.curry((fetch, id) =>
-  R.compose(R.map(deserialize), Fetch.fetchUrl(fetch), kayttajaForId)(id)
-);
-
-export const kayttajanLaatijaFuture = R.curry((fetch, id) =>
-  R.compose(Fetch.fetchUrl(fetch), laatijaForKayttajaId)(id)
-);
-
-export const putKayttajanLaatijaFuture = R.curry((fetch, id, laatija) =>
-  Future.encaseP(Fetch.fetchWithMethod(fetch, 'put', kayttajaForId(id)))(
-    laatija
-  )
-);
-
-export const kayttajaAndLaatijaFuture = R.curry((fetch, id) =>
-  R.compose(
-    R.map(([kayttaja, laatija]) => R.assoc('laatija', laatija, kayttaja)),
-    R.converge(Future.both, [
-      kayttajaFuture(fetch),
-      kayttajanLaatijaFuture(fetch)
-    ])
-  )(id)
-);
 
 export const kayttajaHasAccessToResource = R.curry((roolit, kayttaja) =>
   R.compose(R.applyTo(roolit), R.includes, R.prop('rooli'))(kayttaja)
