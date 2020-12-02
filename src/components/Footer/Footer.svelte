a2d2c49d8ec492a3ed66bf931ad663c71ba36021
Pasi Semykine <23694709+pasisem@users.noreply.github.com>
2020-12-02 12:29:52 +0200

    AE-683: Add new footer layout

--- src/components/Footer/Footer.svelte
+++ src/components/Footer/Footer.svelte
@@ -1,41 +1,35 @@
 <script>
-  import Version from '@Component/Version/Version';
+  import Button from '@Component/Button/Button';
 </script>
 
-<style type="text/postcss">
-  footer {
-    @apply flex flex-col;
-  }
-
-  footer > * {
-    @apply px-20;
-  }
-
-  .info {
-    @apply flex bg-background h-64 items-stretch;
-  }
-
-  .info > * {
-    @apply w-1/4;
-  }
-
-  .arafooter {
-    @apply flex justify-end items-end w-1/2;
-  }
-
-  .version {
-    @apply flex items-end;
-  }
-</style>
-
-<footer>
-  <section class="info">
-    <div class="version">
-      <Version />
+<footer class="flex flex-col w-full text-sm">
+  <div class="flex flex-col md:flex-row justify-between p-8">
+    <div class="flex flex-col space-y-4">
+      <span class="uppercase font-bold text-secondary">
+        Viesti paakayttajalle
+      </span>
+      <span class="font-bold text-secondary">
+        Onko onglemia palvelun kaytossa?
+      </span>
+      <Button style="primary" text={'Laheta viesti'} />
     </div>
-    <div class="contact" />
-    <div class="arafooter">
-      <img class="araprimary" src="images/ara_logo_simple_nega.svg" alt="Ara" />
+    <div class="flex flex-col space-y-4">
+      <span class="uppercase font-bold text-secondary">
+        Tietoa sivustosta
+      </span>
+      <a class="font-bold text-primary" href="/"> Rekisteriseloste </a>
     </div>
-  </section>
+    <div class="flex flex-col text-primary">
+      <span class="uppercase font-bold text-secondary"> yhteystiedot </span>
+      <span class="font-bold"> asumisen rahoitus ja kehittamiskeskus </span>
+      <span class="font-bold"> PL 30 </span>
+      <span class="font-bold"> 15141 </span>
+      <br />
+      <span class="font-bold"> Puhelin: 029 525 0800 </span>
+      <span class="font-bold"> Sahkoposti: energiatodistus@ara.fi </span>
+    </div>
+  </div>
+  <div class="w-full border-t border-tableborder mx-4 pt-4 pb-16">
+    <img class="text-primary" src="images/ara_logo_simple_nega.svg" alt="Ara" />
+  </div>
 </footer>
