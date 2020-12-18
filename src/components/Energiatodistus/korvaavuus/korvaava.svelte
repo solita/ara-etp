a479bc10051f506db696fec3fb6a363cade7089f
Mika Haapakorpi <mika.haapakorpi@solita.fi>
2020-12-18 13:38:57 +0200

    AE-883 - if draft et is replacing show different header + info message

--- src/components/Energiatodistus/korvaavuus/korvaava.svelte
+++ src/components/Energiatodistus/korvaavuus/korvaava.svelte
@@ -9,6 +9,7 @@
   import EtTable from './energiatodistus-table';
 
   import * as EtApi from '../energiatodistus-api';
+  import * as ET from '../energiatodistus-utils';
 
   import * as Maybe from '@Utility/maybe-utils';
   import * as Future from '@Utility/future-utils';
@@ -58,13 +59,21 @@ h3 {
 </style>
 
 {#if korvaavaEnergiatodistusId.isSome()}
-  <h3> {$_('energiatodistus.korvaavuus.header.korvaava')} </h3>
+  <h3> {Maybe.exists(ET.isDraft, korvaavaEnergiatodistus) ?
+    $_('energiatodistus.korvaavuus.header.korvaava-draft') :
+    $_('energiatodistus.korvaavuus.header.korvaava')} </h3>
   <div class="flex flex-col -mx-4 mt-2">
     {#if !overlay}
       {#each Maybe.toArray(korvaavaEnergiatodistus) as et}
         <div class="w-full px-4 py-4 relative"
             transition:slide|local={{ duration: 200 }}>
           <EtTable energiatodistus={et} {postinumerot}/>
+          {#if Maybe.exists(ET.isDraft, korvaavaEnergiatodistus)}
+            <p class="text-sm flex mt-2">
+              <span class="font-icon mr-1">info</span>
+              {$_('energiatodistus.korvaavuus.korvaava-draft-info')}
+            </p>
+          {/if}
         </div>
       {/each}
     {:else}
