10468fa086f07ae799d81e42805ad35a575fd04b
Vesa Lahdenperä <vesa.lahdenpera@solita.fi>
2021-03-05 09:42:42 +0200

    Add yritys-name to navigation

--- src/utils/navigation.js
+++ src/utils/navigation.js
@@ -132,35 +132,42 @@ export const parseKayttaja = R.curry(
   }
 );
 
-export const linksForYritys = R.curry((i18n, id) => [
-  {
-    label: `${i18n('navigation.yritys')} ${id}`,
-    href: `#/yritys/${id}`
-  },
+export const linksForYritys = R.curry((i18n, idTranslate, id) => [
+  R.hasPath(['yritys', parseInt(id, 10)], idTranslate)
+    ? {
+        label: R.path(['yritys', parseInt(id, 10)], idTranslate),
+        href: `#/yritys/${id}`
+      }
+    : {
+        label: `${i18n('navigation.yritys')} ${id}`,
+        href: `#/yritys/${id}`
+      },
   {
     label: i18n('navigation.yritys-laatijat'),
     href: `#/yritys/${id}/laatijat`
   }
 ]);
 
-export const parseYritys = R.curry((i18n, kayttaja, locationParts) => {
-  if (R.isEmpty(locationParts)) {
-    return [];
+export const parseYritys = R.curry(
+  (i18n, kayttaja, idTranslate, locationParts) => {
+    if (R.isEmpty(locationParts)) {
+      return [];
+    }
+    const id = locationParts[0];
+    if (R.equals('new', id)) {
+      return [
+        {
+          label: `${i18n('navigation.new-yritys')}`,
+          href: `#/yritys/new`
+        }
+      ];
+    } else if (R.equals('all', id)) {
+      return [];
+    } else {
+      return linksForYritys(i18n, idTranslate, id);
+    }
   }
-  const id = locationParts[0];
-  if (R.equals('new', id)) {
-    return [
-      {
-        label: `${i18n('navigation.new-yritys')}`,
-        href: `#/yritys/new`
-      }
-    ];
-  } else if (R.equals('all', id)) {
-    return [];
-  } else {
-    return linksForYritys(i18n, id);
-  }
-});
+);
 
 export const linksForPaakayttaja = R.curry((isDev, i18n, kayttaja) => [
   {
@@ -220,7 +227,7 @@ export const navigationParse = R.curry(
         ],
         [
           R.compose(R.equals('yritys'), R.head),
-          R.compose(parseYritys(i18n, kayttaja), R.tail)
+          R.compose(parseYritys(i18n, kayttaja, idTranslate), R.tail)
         ],
         [
           R.compose(
