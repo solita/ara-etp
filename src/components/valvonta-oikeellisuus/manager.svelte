9388d7f9a0a02b0c120e1718dc1fb94a363a8fa3
Mika Haapakorpi <mika.haapakorpi@solita.fi>
2021-04-13 17:07:07 +0300

    refactoring: extract management component from valvonta page

---
+++ src/components/valvonta-oikeellisuus/manager.svelte
@@ -0,0 +1,101 @@
+<script>
+  import * as R from 'ramda';
+  import * as Maybe from '@Utility/maybe-utils';
+  import * as Either from '@Utility/either-utils';
+  import * as Locales from '@Language/locale-utils';
+
+  import { _, locale } from '@Language/i18n';
+  import * as Router from '@Component/Router/router';
+
+  import * as Toimenpiteet from './toimenpiteet';
+
+  import NewToimenpideDialog from './new-toimenpide-dialog.svelte';
+
+  import Button from '@Component/Button/Button.svelte';
+  import TextButton from '@Component/Button/TextButton.svelte';
+  import Select from '@Component/Select/Select.svelte';
+  import H2 from '@Component/H/H2.svelte';
+
+  export let energiatodistus;
+  export let valvojat;
+  export let valvonta;
+  export let toimenpiteet;
+  export let toimenpidetyypit;
+
+  export let saveValvonta;
+  export let reload;
+
+  let newToimenpide = Maybe.None();
+  let toimenpideTyyppi = Maybe.None();
+
+  const openNewToimenpide = type => {
+    if (type == Toimenpiteet.type.audit.report) {
+      // toimenpiteet, jotka käsitellään omalla näytöllä
+      Router.push(`#/valvonta/oikeellisuus/${energiatodistus.versio}/${energiatodistus.id}/new/${Toimenpiteet.type.audit.report}`);
+    } else {
+      // toimenpiteet, jotka käsitellään dialogi-näytöllä
+      newToimenpide = Maybe.Some({
+        'type-id': type,
+        'deadline-date': Either.Right(Toimenpiteet.defaultDeadline(type)),
+        'document': Maybe.None()
+      });
+    }
+  };
+
+  const isAuditCase = toimenpiteet => !R.isEmpty(toimenpiteet) && Toimenpiteet.isAuditCase(R.last(toimenpiteet));
+
+  const saveKasittelija = id => saveValvonta({ 'valvoja-id': id });
+
+  const fullName = valvojat => R.compose(
+    R.join(' '),
+    R.juxt([R.prop('etunimi'), R.prop('sukunimi')]),
+    R.find(R.__, valvojat),
+    R.propEq('id'));
+
+  const load = _ => {
+    newToimenpide = Maybe.None();
+    toimenpideTyyppi = Maybe.None();
+    reload();
+  }
+</script>
+
+{#each Maybe.toArray(newToimenpide) as toimenpide}
+  <NewToimenpideDialog id={energiatodistus.id} {toimenpide} reload={load}/>
+{/each}
+
+<div class="lg:w-1/2 w-full mb-5">
+  <Select label="Valitse käsittelijä"
+          model={valvonta} lens={R.lensProp('valvoja-id')}
+          on:change={event => saveKasittelija(parseInt(event.target.value))}
+          format={fullName(valvojat)}
+          items={R.pluck('id', valvojat)}/>
+</div>
+
+<div class="flex space-x-4 mb-5">
+  <TextButton icon="add_comment" text="Lisää muistiinpano" on:click={load} />
+  <TextButton disabled={isAuditCase(toimenpiteet)}
+              icon="verified"
+              text="Merkitse katsotuksi"
+              on:click={_ => openNewToimenpide(Toimenpiteet.type.verified)} />
+  <TextButton disabled={isAuditCase(toimenpiteet)}
+              icon="bug_report"
+              text="Ilmoita poikkeamasta"
+              on:click={_ => openNewToimenpide(Toimenpiteet.type.anomaly)} />
+</div>
+
+{#if !isAuditCase(toimenpiteet)}
+  <div class="mb-5">
+    <Button text="Aloita valvonta" on:click={_ => openNewToimenpide(Toimenpiteet.type.case)} />
+  </div>
+{:else}
+  <H2 text="Uusi toimenpide"/>
+
+  <div class="lg:w-1/2 w-full mb-5">
+    <Select label="Valitse toimenpide"
+            model={toimenpideTyyppi} lens={R.lens(R.identity, R.identity)}
+            inputValueParse={R.prop('id')}
+            format={Locales.label($locale)}
+            on:change={event => openNewToimenpide(parseInt(event.target.value))}
+            items={R.filter(Toimenpiteet.isAuditCaseToimenpideType, toimenpidetyypit)}/>
+  </div>
+{/if}
--- src/components/valvonta-oikeellisuus/valvonta.svelte
+++ src/components/valvonta-oikeellisuus/valvonta.svelte
@@ -3,15 +3,11 @@
   import * as Future from '@Utility/future-utils';
   import * as Response from '@Utility/response';
   import * as Maybe from '@Utility/maybe-utils';
-  import * as Either from '@Utility/either-utils';
-  import * as Locales from '@Language/locale-utils';
 
   import { _, locale } from '@Language/i18n';
   import { flashMessageStore } from '@/stores';
-  import * as Router from '@Component/Router/router';
 
   import * as ETViews from '@Component/Energiatodistus/views';
-  import * as Toimenpiteet from './toimenpiteet';
 
   import * as EnergiatodistusApi from '@Component/Energiatodistus/energiatodistus-api';
   import * as KayttajaApi from '@Component/Kayttaja/kayttaja-api';
@@ -19,24 +15,18 @@
 
   import Overlay from '@Component/Overlay/Overlay.svelte';
   import Spinner from '@Component/Spinner/Spinner.svelte';
-
-  import NewToimenpideDialog from './new-toimenpide-dialog.svelte';
-  import Toimenpide from './toimenpide.svelte';
-
-  import Button from '@Component/Button/Button.svelte';
-  import TextButton from '@Component/Button/TextButton.svelte';
-  import Select from '@Component/Select/Select.svelte';
   import Address from '@Component/Energiatodistus/address.svelte';
   import H1 from '@Component/H/H1.svelte';
   import H2 from '@Component/H/H2.svelte';
+
+  import Manager from './manager.svelte';
+  import Toimenpide from './toimenpide.svelte';
 
   const i18nRoot = 'valvonta.oikeellisuus.existing';
 
   export let params;
   export let resources = Maybe.None();
-
   let overlay = true;
-  let newToimenpide = Maybe.None();
 
   $: load(params);
 
@@ -53,12 +43,10 @@
               Response.localizationKey(response)
             )
           );
-        newToimenpide = Maybe.None();
         flashMessageStore.add('valvonta-oikeellisuus', 'error', msg);
       },
       response => {
         resources = Maybe.Some(response);
-        newToimenpide = Maybe.None();
         overlay = false;
       },
       Future.parallelObject(5, {
@@ -74,28 +62,6 @@
   };
 
   const kayttotarkoitusTitle = ETViews.kayttotarkoitusTitle($locale);
-
-  const fullName = valvojat => R.compose(
-    R.join(' '),
-    R.juxt([R.prop('etunimi'), R.prop('sukunimi')]),
-    R.find(R.__, valvojat),
-    R.propEq('id'));
-
-  const openNewToimenpide = type => {
-    if (type == Toimenpiteet.type.audit.report) {
-      // toimenpiteet, jotka käsitellään omalla näytöllä
-      Router.push(`#/valvonta/oikeellisuus/${params.version}/${params.id}/new/${Toimenpiteet.type.audit.report}`);
-    } else {
-      // toimenpiteet, jotka käsitellään dialogi-näytöllä
-      newToimenpide = Maybe.Some({
-        'type-id': type,
-        'deadline-date': Either.Right(Toimenpiteet.defaultDeadline(type)),
-        'document': Maybe.None()
-      });
-    }
-  };
-
-  const isAuditCase = toimenpiteet => !R.isEmpty(toimenpiteet) && Toimenpiteet.isAuditCase(R.last(toimenpiteet));
 
   const saveValvonta = valvonta => {
     overlay = true;
@@ -121,8 +87,6 @@
       ValvontaApi.putValvonta(params.id, valvonta)
     )
   };
-
-  const saveKasittelija = id => saveValvonta({ 'valvoja-id': id});
 </script>
 
 <style>
@@ -131,9 +95,6 @@
 <Overlay {overlay}>
   <div slot="content" class="w-full mt-3">
     <H1 text="Oikeellisuuden valvonta"/>
-      {#each Maybe.toArray(newToimenpide) as toimenpide}
-        <NewToimenpideDialog id={params.id} {toimenpide} reload={_ => load(params)}/>
-      {/each}
 
     {#each Maybe.toArray(resources) as { energiatodistus, luokittelut, toimenpiteet, toimenpidetyypit, valvojat, valvonta, whoami }}
 
@@ -146,43 +107,16 @@
         <Address postinumerot={luokittelut.postinumerot} {energiatodistus} />
       </div>
 
-      <div class="lg:w-1/2 w-full mb-5">
-        <Select label="Valitse käsittelijä"
-                model={valvonta} lens={R.lensProp('valvoja-id')}
-                on:change={event => saveKasittelija(parseInt(event.target.value))}
-                format={fullName(valvojat)}
-                items={R.pluck('id', valvojat)}/>
-      </div>
-
-      <div class="flex space-x-4 mb-5">
-        <TextButton icon="add_comment" text="Lisää muistiinpano" on:click={_ => load(params)} />
-        <TextButton disabled={isAuditCase(toimenpiteet)}
-                    icon="verified"
-                    text="Merkitse katsotuksi"
-                    on:click={_ => openNewToimenpide(Toimenpiteet.type.verified)} />
-        <TextButton disabled={isAuditCase(toimenpiteet)}
-                    icon="bug_report"
-                    text="Ilmoita poikkeamasta"
-                    on:click={_ => openNewToimenpide(Toimenpiteet.type.anomaly)} />
-      </div>
-
-      {#if !isAuditCase(toimenpiteet)}
-        <div class="mb-5">
-          <Button text="Aloita valvonta" on:click={_ => openNewToimenpide(Toimenpiteet.type.case)} />
-        </div>
-      {:else}
-        <H2 text="Uusi toimenpide"/>
-        <div class="lg:w-1/2 w-full mb-5">
-          <Select label="Valitse toimenpide"
-                  model={Maybe.None()} lens={R.lens(R.identity, R.identity)}
-                  inputValueParse={R.prop('id')}
-                  format={Locales.label($locale)}
-                  on:change={event => openNewToimenpide(parseInt(event.target.value))}
-                  items={R.filter(Toimenpiteet.isAuditCaseToimenpideType, toimenpidetyypit)}/>
-        </div>
-      {/if}
+      <Manager
+          {energiatodistus}
+          {valvojat} {valvonta}
+          {toimenpiteet}
+          {toimenpidetyypit}
+          {saveValvonta}
+          reload={_ => load(params)}/>
 
       <H2 text="Toimenpiteet"/>
+
       {#each R.reverse(toimenpiteet) as toimenpide}
         <Toimenpide {toimenpidetyypit} {toimenpide}/>
       {/each}
