ac9a6c79b7e157459df18ac94006ae520872969c
Vesa Lahdenperä <vesa.lahdenpera@solita.fi>
2021-05-05 09:37:46 +0300

    AE-1210: Active valvonta when käsittelijä is selected

--- src/components/valvonta-oikeellisuus/manager.svelte
+++ src/components/valvonta-oikeellisuus/manager.svelte
@@ -43,7 +43,7 @@
   const isAuditCase = toimenpiteet =>
     !R.isEmpty(toimenpiteet) && Toimenpiteet.isAuditCase(R.last(toimenpiteet));
 
-  const saveKasittelija = id => saveValvonta({ 'valvoja-id': id });
+  const saveKasittelija = id => saveValvonta({ active: !isNaN(id), 'valvoja-id': id });
 
   const fullName = valvojat =>
     R.compose(
