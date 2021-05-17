5f924bbc2e9db641406dfa53288e03905d156577
Vesa Lahdenperä <vesa.lahdenpera@solita.fi>
2021-05-17 13:39:46 +0300

    AE-1210: add pendingExecution flag to Toolbar

--- src/components/ToolBar/tyojono-button.svelte
+++ src/components/ToolBar/tyojono-button.svelte
@@ -1,14 +1,11 @@
 <script>
-  import Spinner from '@Component/Spinner/Spinner';
-
   export let valvonta;
   export let disabled;
 </script>
 
 
 <!-- purgecss: text-primary text-darkgrey -->
-{#if !disabled}
-<button disabled={valvonta.ongoing} class="min-h-3em" class:hover:text-primary={!valvonta.ongoing} class:text-primary={valvonta.pending && !valvonta.ongoing} class:text-darkgrey={valvonta.ongoing} class:hover:text-darkgrey={valvonta.ongoing} on:click>
+<button disabled={valvonta.ongoing || disabled} class="min-h-3em" class:hover:text-primary={!valvonta.ongoing} class:text-primary={valvonta.pending && !valvonta.ongoing} class:text-darkgrey={valvonta.ongoing} class:hover:text-darkgrey={valvonta.ongoing} on:click>
   <span class="font-icon text-2xl">
     {#if !valvonta.pending}
       star_border
@@ -17,8 +14,3 @@
     {/if}
   </span>
 </button>
-{:else}
-<div class="flex justify-center items-center">
-  <Spinner />
-</div>
-{/if}
