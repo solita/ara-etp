526f10e62ad6c36f9ecd0ad113880420d8f1b574
Vesa Lahdenperä <vesa.lahdenpera@solita.fi>
2021-05-21 08:23:14 +0300

    AE-1243: Fix overeager unread-fetching

--- src/components/navigation/badge-provider.svelte
+++ src/components/navigation/badge-provider.svelte
@@ -1,5 +1,6 @@
 <script>
   import { onMount } from 'svelte';
+  import * as R from 'ramda';
   import * as Maybe from '@Utility/maybe-utils';
   import * as Future from '@Utility/future-utils';
 
@@ -9,12 +10,15 @@
   let badge = Maybe.None();
 
   const getBadgeContent = _ => {
-    cancel();
-    cancel = Future.fork(
-      _ => {},
-      value => (badge = Maybe.Some(value)),
-      badgeFuture
-    );
+    cancel = R.compose(
+      Future.fork(
+        _ => {},
+        value => (badge = Maybe.Some(value))
+      ),
+      R.chain(R.always(badgeFuture)),
+      Future.after(200),
+      R.tap(cancel)
+    )(true);
   };
 
   onMount(getBadgeContent);
