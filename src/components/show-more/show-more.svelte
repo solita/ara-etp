ffccd9d7cff58044de023f85e5bea4dbf5d5f206
Vesa Lahdenperä <vesa.lahdenpera@solita.fi>
2021-06-15 11:23:17 +0300

    Add multiline support for show-more

--- src/components/show-more/show-more.svelte
+++ src/components/show-more/show-more.svelte
@@ -3,16 +3,17 @@
   import TextButton from '@Component/Button/TextButton';
 
   export let truncate = true;
+  let multiline = false;
 
   let node;
   let truncated;
 
+  let lineHeight;
+
   onMount(() => {
-    truncated =
-      !!node &&
-      !!node.children &&
-      !!node.children[0] &&
-      node.children[0].offsetWidth < node.children[0].scrollWidth;
+    lineHeight = node.children[0].offsetHeight;
+    truncated = node.children[0].offsetWidth < node.children[0].scrollWidth;
+    multiline = lineHeight < node.children[0].offsetHeight;
   });
 </script>
 
@@ -23,20 +24,29 @@
 </style>
 
 <svelte:window
-  on:resize={() =>
-    (truncated =
+  on:resize={() => {
+    truncated =
       !!node &&
       !!node.children &&
       !!node.children[0] &&
-      node.children[0].offsetWidth < node.children[0].scrollWidth)} />
+      node.children[0].offsetWidth < node.children[0].scrollWidth;
+    multiline =
+      !!node &&
+      !!node.children &&
+      !!node.children[0] &&
+      lineHeight < node.children[0].offsetHeight;
+  }} />
 
 <div bind:this={node} class="min-w-0" class:truncate-children={truncate}>
   <slot />
 </div>
 
-{#if truncated}
+{#if truncated || multiline}
   <TextButton
-    on:click={_ => (truncate = !truncate)}
+    on:click={_ => {
+      truncate = !truncate;
+      multiline = !multiline;
+    }}
     type={'button'}
     icon={truncate ? 'expand_more' : 'expand_less'}
     text={truncate ? 'Näytä lisää' : 'Näytä vähemmän'} />
