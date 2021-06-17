4b808c841af8dca5fddbf733fd613e15bf981bea
Mika Haapakorpi <mika.haapakorpi@solita.fi>
2021-06-17 10:11:26 +0300

    Käytönvalvonnan näyttöjen pohjat kopioitu oikeellisuuden valvonnasta

---
+++ src/components/valvonta-kaytto/kohde.svelte
@@ -0,0 +1,9 @@
+<script>
+  import H1 from '@Component/H/H1.svelte';
+  import { _ } from '@Language/i18n';
+
+  const i18n = $_;
+  const i18nRoot = 'valvonta.kaytto.kohde';
+</script>
+
+<H1 text={i18n(i18nRoot + '.title')} />
---
+++ src/components/valvonta-kaytto/links.js
@@ -0,0 +1,6 @@
+const root = '#/valvonta/kaytto';
+
+export const valvonta = valvonta => `${root}/${valvonta.id}/valvonta`;
+
+export const kohde = valvonta => `${root}/${valvonta.id}/kohde`;
+
