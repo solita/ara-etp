114c16b84e1bc7d031ce34a7514f2f7e298f1ce1
Mika Haapakorpi <mika.haapakorpi@solita.fi>
2021-09-12 22:02:00 +0300

    VK: käytönvalvonnan success/error viestit + use errorKey404

--- src/pages/valvonta-kaytto/valvonta.svelte
+++ src/pages/valvonta-kaytto/valvonta.svelte
@@ -36,7 +36,7 @@
       response => {
         overlay = false;
         flashMessageStore.add('valvonta-kaytto', 'error',
-          i18n(Response.errorKey(i18nRoot, 'load', response)));
+          i18n(Response.errorKey404(i18nRoot, 'load', response)));
       },
       response => {
         resources = Maybe.Some(response);
@@ -66,7 +66,7 @@
     Future.fork(
       response => {
         flashMessageStore.add('valvonta-kaytto', 'error',
-          i18n(Response.errorKey(i18nRoot, key, response)));
+          i18n(Response.errorKey404(i18nRoot, key, response)));
         overlay = false;
       },
       response => {
