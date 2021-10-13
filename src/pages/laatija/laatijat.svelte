042cae93504792a67c16367047310abf787e07dd
Mika Haapakorpi <mika.haapakorpi@solita.fi>
2021-10-13 11:21:48 +0300

    AE-1455 - overlay

--- src/pages/laatija/laatijat.svelte
+++ src/pages/laatija/laatijat.svelte
@@ -25,11 +25,14 @@
   import H1 from '@Component/H/H1';
   import Select from '@Component/Select/Select';
   import Results from './laatijat-results.svelte';
-  import Label from '../../components/Label/Label.svelte';
+  import Label from '@Component/Label/Label.svelte';
+  import Overlay from '@Component/Overlay/Overlay.svelte';
+  import Spinner from '@Component/Spinner/Spinner.svelte';
 
   const i18n = $_;
   const i18nRoot = 'laatijat';
   let resources = Maybe.None();
+  let overlay = true;
   let cancel = () => {
   };
 
@@ -38,9 +41,11 @@
     response => {
       flashMessageStore.add('Laatija', 'error',
         i18n(Response.errorKey404(i18nRoot, 'load', response)));
+      overlay = false;
     },
     response => {
       resources = Maybe.Some(response);
+      overlay = false;
     },
     Future.parallelObject(5, {
       laatijat: LaatijaApi.laatijat,
@@ -132,8 +137,8 @@
     })
   )(query);
 
-  const isAfter = d2 => d1 => dfns.isAfter(d1, d2);
-  const isBefore = d2 => d1 => dfns.isBefore(d1, d2);
+  const isAfter = d2 => d1 => dfns.isAfter(d1, dfnstz.zonedTimeToUtc(d2, 'Europe/Helsinki'));
+  const isBefore = d2 => d1 => dfns.isBefore(d1, dfnstz.zonedTimeToUtc(d2, 'Europe/Helsinki'));
   const propSatisfies = (binaryPredicate, name) =>
     R.compose(R.propSatisfies(R.__, name), binaryPredicate);
 
@@ -147,12 +152,9 @@
       filter: R.nth(R.__, filters),
       'patevyystaso-id': R.propEq('patevyystaso'),
       'toimintaalue-id': R.compose(R.propEq('toimintaalue'), Maybe.Some),
-      'voimassaolo-paattymisaika-after': R.compose(
-        propSatisfies(isAfter, 'voimassaolo-paattymisaika'),
-        d => dfnstz.zonedTimeToUtc(d, 'Europe/Helsinki')),
+      'voimassaolo-paattymisaika-after': propSatisfies(isAfter, 'voimassaolo-paattymisaika'),
       'voimassaolo-paattymisaika-before': R.compose(
         propSatisfies(isBefore, 'voimassaolo-paattymisaika'),
-        d => dfnstz.zonedTimeToUtc(d, 'Europe/Helsinki'),
         // this is for inclusive end date range
         d => dfns.add(d, { days: 1, hours: 1 }))
     })),
@@ -170,50 +172,52 @@
 <div class="w-full mt-3">
   <H1 text={i18n(i18nRoot + '.title')} />
 
-  {#each Maybe.toArray(resources) as { laatijat, yritykset, patevyydet, toimintaalueet, whoami }}
-    <div class="flex flex-wrap">
-      <div class="lg:w-1/3 w-full px-4 py-4">
-        <Select
-          label={i18n(i18nRoot + '.filters.label')}
-          disabled={false}
-          bind:model={query}
-          lens={R.lensProp('filter')}
-          format={formatFilter}
-          parse={Maybe.Some}
-          noneLabel={i18nRoot + '.filters.all'}
-          items={R.range(0, R.length(filters))} />
-      </div>
+  <Overlay {overlay}>
+    <div slot='content'>
+      {#each Maybe.toArray(resources) as { laatijat, yritykset, patevyydet, toimintaalueet, whoami }}
+        <div class='flex flex-wrap'>
+          <div class='lg:w-1/3 w-full px-4 py-4'>
+            <Select
+              label={i18n(i18nRoot + '.filters.label')}
+              disabled={false}
+              bind:model={query}
+              lens={R.lensProp('filter')}
+              format={formatFilter}
+              parse={Maybe.Some}
+              noneLabel={i18nRoot + '.filters.all'}
+              items={R.range(0, R.length(filters))} />
+          </div>
 
-      <div class="lg:w-1/3 w-full px-4 py-4">
-        <Select
-            label={i18n(i18nRoot + '.patevyystaso')}
-            disabled={false}
-            bind:model={query}
-            lens={R.lensProp('patevyystaso-id')}
-            format={Locales.labelForId($locale, patevyydet)}
-            parse={Maybe.Some}
-            noneLabel={i18nRoot + '.filters.all'}
-            items={R.pluck('id', patevyydet)} />
-      </div>
+          <div class='lg:w-1/3 w-full px-4 py-4'>
+            <Select
+              label={i18n(i18nRoot + '.patevyystaso')}
+              disabled={false}
+              bind:model={query}
+              lens={R.lensProp('patevyystaso-id')}
+              format={Locales.labelForId($locale, patevyydet)}
+              parse={Maybe.Some}
+              noneLabel={i18nRoot + '.filters.all'}
+              items={R.pluck('id', patevyydet)} />
+          </div>
 
-      <div class="lg:w-1/3 w-full px-4 py-4">
-        <Select
-            label={i18n(i18nRoot + '.toimintaalue')}
-            disabled={false}
-            bind:model={query}
-            lens={R.lensProp('toimintaalue-id')}
-            format={Locales.labelForId($locale, toimintaalueet)}
-            parse={Maybe.Some}
-            noneLabel={i18nRoot + '.filters.all'}
-            items={R.pluck('id', toimintaalueet)} />
-      </div>
+          <div class='lg:w-1/3 w-full px-4 py-4'>
+            <Select
+              label={i18n(i18nRoot + '.toimintaalue')}
+              disabled={false}
+              bind:model={query}
+              lens={R.lensProp('toimintaalue-id')}
+              format={Locales.labelForId($locale, toimintaalueet)}
+              parse={Maybe.Some}
+              noneLabel={i18nRoot + '.filters.all'}
+              items={R.pluck('id', toimintaalueet)} />
+          </div>
 
-      <div class="lg:w-1/2 w-full px-4 py-4">
-        <Input
-            label={i18n(i18nRoot + '.keyword-search')}
-            model={Maybe.orSome('', query.search)}
-            search={true}
-            on:input={evt => {
+          <div class='lg:w-1/2 w-full px-4 py-4'>
+            <Input
+              label={i18n(i18nRoot + '.keyword-search')}
+              model={Maybe.orSome('', query.search)}
+              search={true}
+              on:input={evt => {
             cancel = R.compose(
               Future.value(val => {
                 query = R.mergeRight(query, {
@@ -224,45 +228,50 @@
               Future.after(200),
               R.tap(cancel)
             )(evt.target.value);
-          }}/>
-      </div>
+          }} />
+          </div>
 
-      <div class="lg:w-1/2 w-full px-4 py-4 flex flex-wrap">
-        <div class="w-full">
-          <Label label={i18n(i18nRoot + '.voimassaolo-paattymisaika.label')}/>
+          <div class='lg:w-1/2 w-full px-4 py-4 flex flex-wrap'>
+            <div class='w-full'>
+              <Label label={i18n(i18nRoot + '.voimassaolo-paattymisaika.label')} />
+            </div>
+            <div class='lg:w-1/3 w-full'>
+              <Datepicker
+                label={i18n(i18nRoot + '.voimassaolo-paattymisaika.after')}
+                compact='true'
+                bind:model={query}
+                lens={R.lensProp('voimassaolo-paattymisaika-after')}
+                parse={parseDate}
+                transform={Maybe.Some}
+                format={Maybe.fold('', Formats.formatDateInstant)} />
+            </div>
+            <span class='w-min px-4 invisible lg:visible'>-</span>
+            <div class='lg:w-1/3 w-full'>
+              <Datepicker
+                label={i18n(i18nRoot + '.voimassaolo-paattymisaika.before')}
+                compact='true'
+                bind:model={query}
+                lens={R.lensProp('voimassaolo-paattymisaika-before')}
+                parse={parseDate}
+                transform={Maybe.Some}
+                format={Maybe.fold('', Formats.formatDateInstant)} />
+            </div>
+          </div>
         </div>
-        <div class="lg:w-1/3 w-full">
-          <Datepicker
-              label={i18n(i18nRoot + '.voimassaolo-paattymisaika.after')}
-              compact="true"
-              bind:model={query}
-              lens={R.lensProp('voimassaolo-paattymisaika-after')}
-              parse={parseDate}
-              transform={Maybe.Some}
-              format={Maybe.fold('', Formats.formatDateInstant)}/>
-        </div>
-        <span class="w-min px-4 invisible lg:visible">-</span>
-        <div class="lg:w-1/3 w-full">
-          <Datepicker
-              label={i18n(i18nRoot + '.voimassaolo-paattymisaika.before')}
-              compact="true"
-              bind:model={query}
-              lens={R.lensProp('voimassaolo-paattymisaika-before')}
-              parse={parseDate}
-              transform={Maybe.Some}
-              format={Maybe.fold('', Formats.formatDateInstant)}/>
-        </div>
-      </div>
-    </div>
 
-    <div>
-      <Results laatijat={R.filter(predicate(query), laatijat)}
-               page={query.page}
-               {toPage}
-               {yritykset}
-               {patevyydet}
-               {toimintaalueet}
-               {whoami}/>
+        <div>
+          <Results laatijat={R.filter(predicate(query), laatijat)}
+                   page={query.page}
+                   {toPage}
+                   {yritykset}
+                   {patevyydet}
+                   {toimintaalueet}
+                   {whoami} />
+        </div>
+      {/each}
     </div>
-  {/each}
+    <div slot='overlay-content'>
+      <Spinner />
+    </div>
+  </Overlay>
 </div>
