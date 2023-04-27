6cd31f02d4c9082609ed46014e2c8b352028a123
Juho Leinonen <juho.leinonen@solita.fi>
2023-04-27 15:50:08 +0300

    AE-1718: Fix formatting

--- src/pages/valvonta-oikeellisuus/valvonta-api.js
+++ src/pages/valvonta-oikeellisuus/valvonta-api.js
@@ -16,7 +16,8 @@ export const url = {
   valvonnat: 'api/private/valvonta/oikeellisuus',
   valvonta: id => `${url.valvonnat}/${id}`,
   toimenpiteet: id => `${url.valvonta(id)}/toimenpiteet`,
-  anomaly: (id, language) => `${url.valvonta(id)}/toimenpiteet?language=${language}`,
+  anomaly: (id, language) =>
+    `${url.valvonta(id)}/toimenpiteet?language=${language}`,
   preview: id => `${url.valvonta(id)}/toimenpiteet/preview`,
   toimenpide: (id, toimenpideId) => `${url.toimenpiteet(id)}/${toimenpideId}`,
   document: (id, toimenpideId, filename) =>
@@ -151,7 +152,9 @@ export const postToimenpide = R.curry((id, toimenpide) =>
 export const postAnomaly = R.curry((id, toimenpide, language) =>
   R.compose(
     Fetch.responseAsJson,
-    Future.encaseP(Fetch.fetchWithMethod(fetch, 'post', url.anomaly(id, language))),
+    Future.encaseP(
+      Fetch.fetchWithMethod(fetch, 'post', url.anomaly(id, language))
+    ),
     serializeToimenpide
   )(toimenpide)
 );
