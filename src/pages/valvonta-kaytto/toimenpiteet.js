632ce888d0b4490204220840b61c83406ea5f46b
Juho Leinonen <juho.leinonen@solita.fi>
2023-05-25 13:16:46 +0300

    AE-1856: Add new uhkasakkoprosessi toimenpidetypes to isAuditCaseToimenpideType function

--- src/pages/valvonta-kaytto/toimenpiteet.js
+++ src/pages/valvonta-kaytto/toimenpiteet.js
@@ -31,7 +31,10 @@ export const hasDeadline = R.propSatisfies(isDeadlineType, 'type-id');
 export const isCloseCase = isType(type.closed);
 export const isAuditCase = R.complement(isType(type.closed));
 export const isAuditCaseToimenpideType = R.propSatisfies(
-  R.includes(R.__, [1, 2, 3, 4, 5]),
+  R.includes(
+    R.__,
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21]
+  ),
   'id'
 );
 
