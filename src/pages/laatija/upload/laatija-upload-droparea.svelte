8295972c3db97907767549096ec3c1c459cfdfd3
Joonas Sarajärvi <joonas.sarajarvi@solita.fi>
2023-12-21 16:20:26 +0100

    AE-1770 Use announce module in laatija pages

--- src/pages/laatija/upload/laatija-upload-droparea.svelte
+++ src/pages/laatija/upload/laatija-upload-droparea.svelte
@@ -4,21 +4,17 @@
   import FileDropArea from '@Component/FileDropArea/FileDropArea';
   import * as LaatijaUploadUtils from './laatija-upload-utils';
   import { _ } from '@Language/i18n';
-  import { flashMessageStore } from '@/stores';
+  import { announcementsForModule } from '@Utility/announce';
 
   export let laatijat;
   export let files = [];
 
   const i18n = $_;
+  const { announceError } = announcementsForModule('Laatija');
 
   $: R.compose(
     Future.fork(
-      _ =>
-        flashMessageStore.add(
-          'Laatija',
-          'error',
-          i18n('errors.file-read-error')
-        ),
+      _ => announceError(i18n('errors.file-read-error')),
       result => (laatijat = result)
     ),
     R.map(LaatijaUploadUtils.deserialize),
