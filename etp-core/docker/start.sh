66ac8da1b4b0909ece71adf3455761ad570b12ac
Joonas Sarajärvi <joonas.sarajarvi@solita.fi>
2024-01-02 14:16:52 +0100

    Use docker compose instead of docker-compose

--- etp-core/docker/start.sh
+++ etp-core/docker/start.sh
@@ -5,7 +5,7 @@ cd "$(dirname "$0")"
 mkdir -p smtp/received-emails
 find sftp/ssh -iname "*_key" -exec chmod 600 {} \;
 
-docker-compose up -d
+docker compose up -d
 
 # Wait naively for PostgreSQL to start
 sleep 2
