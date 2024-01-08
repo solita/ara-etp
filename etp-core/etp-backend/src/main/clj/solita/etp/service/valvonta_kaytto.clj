aaa48b2b4ee17c7717c608be970b66989651de46
Juho Leinonen <juho.leinonen@solita.fi>
2024-01-08 08:46:44 +0200

    AE-1969: Add test and documentation for how count-valvonnat handles limit and offset parameters

--- etp-core/etp-backend/src/main/clj/solita/etp/service/valvonta_kaytto.clj
+++ etp-core/etp-backend/src/main/clj/solita/etp/service/valvonta_kaytto.clj
@@ -64,7 +64,12 @@
        (pmap #(assoc % :henkilot (find-henkilot db (:id %))
                        :yritykset (find-yritykset db (:id %))))))
 
-(defn count-valvonnat [db query]
+(defn count-valvonnat
+  "Returns the number of valvonnat that matches the query.
+   Takes the same query parameters as find-valvonnat
+   but does not respect limit and offset as the count is used to
+   determine the need for pagination for find-valvonnat."
+  [db query]
   (first
     (valvonta-kaytto-db/select-valvonnat-count
       db (merge default-valvonta-query query))))
