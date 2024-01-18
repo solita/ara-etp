3d8d444e25f14283ab639213681a71712d5ada83
Juho Leinonen <juho.leinonen@solita.fi>
2024-01-18 14:32:07 +0200

    Update nvd-clojure to version 4 and setup api token for nist-nvd in GitHub Actions

--- etp-core/etp-backend/nvd.sh
+++ etp-core/etp-backend/nvd.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e
 
-clojure -Ttools install nvd-clojure/nvd-clojure '{:mvn/version "3.1.0"}' :as nvd
+clojure -Ttools install nvd-clojure/nvd-clojure '{:mvn/version "4.0.0"}' :as nvd
 clojure -J-Dclojure.main.report=stderr -Tnvd nvd.task/check :classpath \""$(clojure -Spath)\"" :config-filename \""nvd-config.edn\""
