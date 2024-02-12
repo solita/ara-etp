b13401eb69fa53433aa3ac8a6c90a4e8e596a1d4
Juho Haapakoski <juho.haapakoski@solita.fi>
2024-02-12 13:54:02 +0200

    AE-2031: Removo TODO from `flyway.sh`

--- etp-core/docker/flyway.sh
+++ etp-core/docker/flyway.sh
@@ -12,6 +12,5 @@ cd ../etp-db
 # Don't run test migrations to template db
 clojure -M -m solita.etp.db.flywaydb $1
 
-# TODO: This needs to go to env also. Or maybe can just run this inside of a container.
 # Run test migrations to etp_dev
 DB_URL="jdbc:postgresql://localhost:5432/etp_dev" clojure -M:test -m solita.etp.db.flywaydb $1
