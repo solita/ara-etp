f2589bca17b1da8e58d22d29f2b7053af5e9ba04
Joonas Sarajärvi <joonas.sarajarvi@solita.fi>
2024-02-12 13:25:16 +0100

    AE-2132 Grab facus in H1 components
    This is quite opinionated in that it expects that any given page
    will only have a single H1 element. WCAG shares this opinion, so
    we probably want to maintain that constraint in any case.
    
    This should mean that after changing the page in the private site,
    tab navigation should always begin from that sole h1 element in the
    page. Also the focusing is normally expected to trigger the screen
    reader to read the h1 contents aloud.

--- etp-front/src/components/H/H1.svelte
+++ etp-front/src/components/H/H1.svelte
@@ -1,11 +1,25 @@
 <script>
   export let text = '';
+
+  export let focus = true;
+
+  const setFocus = e => {
+    if (focus) {
+      e.focus();
+    }
+  };
 </script>
 
 <style type="text/postcss">
+  /* outline-none is generally discouraged, but for h1 we do not expect
+   * the user to want to focus there. The intended effect here is that
+   * an average, able user will not perceive it as a focusable item in any
+   * way, but we can set the focus there so that the screen reader will read
+   * it out and we get the tab context into some consistent place.
+   */
   h1 {
-    @apply text-secondary font-bold uppercase text-lg mb-8 tracking-xl;
+    @apply text-secondary font-bold uppercase text-lg mb-8 tracking-xl outline-none;
   }
 </style>
 
-<h1>{text}</h1>
+<h1 tabindex="-1" use:setFocus>{text}</h1>
