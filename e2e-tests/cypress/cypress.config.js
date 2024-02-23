4e437c73c2f0e82524ac23c94cb2abeb484cd198
Juho Leinonen <juho.leinonen@solita.fi>
2024-02-23 10:11:53 +0200

    Enable Cypress video recording

--- e2e-tests/cypress/cypress.config.js
+++ e2e-tests/cypress/cypress.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   viewportHeight: 900,
   viewportWidth: 1440,
   defaultCommandTimeout: 15000,
-  video: false,
+  video: true,
   e2e: {
     setupNodeEvents(on, config) {},
     baseUrl: 'https://localhost:3009'
