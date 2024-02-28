09a932e8912915fbded986e103aa5bd90e67abe8
Antti Möttönen <antti.mottonen@solita.fi>
2024-02-28 13:19:54 +0200

    Start frontend on the background for e2e tests

--- e2e-tests/cypress/start-e2e-env.sh
+++ e2e-tests/cypress/start-e2e-env.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
+# Optimised start order
+# Start building frontend
+# Start migration runner, which requires database to be ready
+# When migrations are ready, create cypress_test database
+# Start backend and wait for it and frontend to be ready
+
+docker compose up --build -d frontend
 docker compose up --build migration-runner
 docker compose exec db dropdb -U postgres cypress_test --if-exists --force
 docker compose exec db createdb -U postgres -T etp_dev cypress_test
 echo -e "\e[1;33m Waiting for frontend to be healthy. Can take ~20s. \e[0m"
-docker compose up --build --wait frontend
+docker compose up --build --wait frontend --wait backend
