1c7133b7e781cd9a7c77102ed2c29d4cf115857a
Antti Möttönen <antti.mottonen@solita.fi>
2024-03-11 13:15:26 +0200

    Fix flakyness in e2e test setup
    Docker compose does not care about exit codes of jobs. Migration runner
    failed sometimes, perhaps because DB was not ready.
    
    Added a check for migration-runner exit code in e2e start script.
    
    Added a healthcheck for db in compose, and require it to be healthy
    before migration runner is run.

--- e2e-tests/cypress/start-e2e-env.sh
+++ e2e-tests/cypress/start-e2e-env.sh
@@ -8,7 +8,7 @@ set -euxo pipefail
 # Start backend and wait for it and frontend to be ready
 
 docker compose up --build -d frontend
-docker compose up --build migration-runner
+docker compose up --build migration-runner --exit-code-from migration-runner
 docker compose exec db dropdb -U postgres cypress_test --if-exists --force
 docker compose exec db createdb -U postgres -T etp_dev cypress_test
 echo -e "\e[1;33m Waiting for frontend to be healthy. Can take ~20s. \e[0m"
