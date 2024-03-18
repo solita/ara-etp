5958e0014e217192246cf5aed5554f3e2433d643
Juho Leinonen <juho.leinonen@solita.fi>
2024-03-18 14:52:38 +0200

    AE-2063: Add more käytönvalvonta csv tests

--- etp-core/etp-backend/src/test/clj/solita/etp/valvonta_kaytto/valvonta_csv_test.clj
+++ etp-core/etp-backend/src/test/clj/solita/etp/valvonta_kaytto/valvonta_csv_test.clj
@@ -57,13 +57,47 @@
                  [csv-header-line
                   "1;\"3139000812\";\"ARA-05.03.01-2024-159\";\"Testitie 5\";\"90100\";\"OULU\";1;\"Valvonnan aloitus\";2024-03-18T02:00;\n"]))))))
 
+(t/deftest valvonta.csv-toimenpidetype-test
+  (t/testing "Every toimenpide created on valvonta results in a row in the csv"
+    (let [toimenpidetype-ids [0 1 2 3 4 5 6 7 8 9 10 11 12 14 15 16 17 18 19 21]
+          valvonta-id (valvonta-service/add-valvonta! ts/*db* {:katuosoite        "Testitie 5"
+                                                               :postinumero       "90100"
+                                                               :ilmoituspaikka-id 0
+                                                               :rakennustunnus    "3139000812"})]
+      (doseq [toimenpidetype-id toimenpidetype-ids]
+        (jdbc/insert! ts/*db*
+                      :vk_toimenpide
+                      {:valvonta_id   valvonta-id
+                       :type_id       toimenpidetype-id
+                       :create_time   (-> (LocalDate/of 2024 3 18)
+                                          (.atStartOfDay (ZoneId/systemDefault))
+                                          .toInstant)
+                       :publish_time  (-> (LocalDate/of 2024 3 18)
+                                          (.atStartOfDay (ZoneId/systemDefault))
+                                          .toInstant)
+                       :deadline_date (LocalDate/of 2024 2 7)
+                       :diaarinumero  "ARA-05.03.01-2024-734"}))
+
+      (let [output (atom [])
+            call-count (atom 0)
+            csv-producer (valvonta-service/csv ts/*db*)]
+        (csv-producer (partial handle-output call-count output))
+
+        (t/is (= (inc (count toimenpidetype-ids))
+                 @call-count)
+              "Call count is how many toimenpidetypes there are plus the header row")
+
+        (t/is (= (inc (count toimenpidetype-ids))
+                 (count @output))
+              "Number of rows in the output is how many toimenpidetypes there are plus the header row")))))
+
 (t/deftest valvonta.csv-generated-valvonnat-test
   (t/testing "Creating csv when there are 100 valvontas returns 101 unique rows with header row included"
     (test-kayttajat/insert-virtu-paakayttaja!
-      {:etunimi  "Asian"
-       :sukunimi "Tuntija"
-       :email    "testi@ara.fi"
-       :puhelin  "0504363675457"
+      {:etunimi    "Asian"
+       :sukunimi   "Tuntija"
+       :email      "testi@ara.fi"
+       :puhelin    "0504363675457"
        :titteli-fi "energia-asiantuntija"
        :titteli-sv "energiexpert"})
     (let [valvonnat (repeatedly 100 #(generator/complete {:ilmoituspaikka-id 1
