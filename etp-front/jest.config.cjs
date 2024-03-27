59aa0b248588cc4e5b3bdc8b3d8d144a33b163aa
Juho Leinonen <juho.leinonen@solita.fi>
2024-03-27 13:25:07 +0200

    AE-2005: Add jest-junit reporter to etp-front, publish test report in GitHub Actions

--- etp-front/jest.config.cjs
+++ etp-front/jest.config.cjs
@@ -98,7 +98,10 @@ const config = {
   // projects: undefined,
 
   // Use this configuration option to add custom reporters to Jest
-  // reporters: undefined,
+  reporters: [
+    'default',
+    ['jest-junit', { outputName: 'test_report.xml' }]
+  ],
 
   // Automatically reset mock state before every test
   // resetMocks: false,
