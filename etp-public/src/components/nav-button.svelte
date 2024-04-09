eecbd147a47381bec4eefedf5a812696bb2dfd1f
Antti Möttönen <antti.mottonen@solita.fi>
2024-04-09 14:57:03 +0300

    Remove dynamic generation of Tailwind classes
    Earlier some Tailwind class names were generated dynamically, which
    broke some styles when purge comments were removed.
    
    Simplify code by removing dynamic name generation.

--- etp-public/src/components/nav-button.svelte
+++ etp-public/src/components/nav-button.svelte
@@ -1,20 +1,28 @@
 <script context="module">
   export const styles = {
     lightgreen: {
-      text: 'black',
-      background: 'lightgreen'
+      text: 'text-black',
+      border: 'border-black',
+      lg_border: 'border-lightgreen',
+      background: 'bg-lightgreen'
     },
     green: {
-      text: 'white',
-      background: 'green'
+      text: 'text-white',
+      border: 'border-white',
+      lg_border: 'border-green',
+      background: 'bg-green'
     },
     darkgreen: {
-      text: 'white',
-      background: 'altgreen'
+      text: 'text-white',
+      border: 'border-white',
+      lg_border: 'border-altgreen',
+      background: 'bg-altgreen'
     },
     ashblue: {
-      text: 'white',
-      background: 'ashblue'
+      text: 'text-white',
+      border: 'border-white',
+      lg_border: 'border-ashblue',
+      background: 'bg-ashblue'
     }
   };
 </script>
@@ -36,11 +44,11 @@
 
 <a
   class="flex box-border lg:flex-full flex-auto py-4 xl:py-6 px-2 navcontainer
-  bg-{background} text-{text} border-{text} focus:z-10 hover:z-10 hover:underline
+  {background} {text} {text} focus:z-10 hover:z-10 hover:underline
   focus:outline-none xl:flex-row lg:flex-col flex-row lg:items-center
   xl:items-center md:items-start {$activePath === link
     ? 'border-l-4'
-    : 'border-l-0'} lg:border-l-0 lg:border-{background} focus:underline"
+    : 'border-l-0'} lg:border-l-0 lg:{background} focus:underline"
   class:shadow-lightgreen={$activePath === link && background === 'lightgreen'}
   class:shadow-green={$activePath === link && background === 'green'}
   class:shadow-altgreen={$activePath === link && background === 'altgreen'}
