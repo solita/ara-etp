d43edb3001dad7e75131782c14e9bd529bd9d49e
Juho Leinonen <juho.leinonen@solita.fi>
2024-04-09 09:57:54 +0300

    AE-2005: Make H1 component test text query case-sensitive

--- etp-front/src/components/H/H1.test.mjs
+++ etp-front/src/components/H/H1.test.mjs
@@ -9,7 +9,7 @@ import H1 from './H1.svelte';
 test('H1 renders correctly', () => {
   render(H1, { text: 'Hello World!', dataCy: 'heading' });
 
-  const heading = screen.queryByText(/Hello/iu);
+  const heading = screen.queryByText(/Hello/u);
 
   expect(heading).toBeInTheDocument();
 });
