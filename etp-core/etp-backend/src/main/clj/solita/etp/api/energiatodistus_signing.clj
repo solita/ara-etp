1cc3b1339567e776d5814d084a224fbe603807fb
Juho Haapakoski <juho.haapakoski@solita.fi>
2024-04-11 02:18:06 +0300

    AE-2165: Put the system signature endpoint behind a feature flag

--- etp-core/etp-backend/src/main/clj/solita/etp/api/energiatodistus_signing.clj
+++ etp-core/etp-backend/src/main/clj/solita/etp/api/energiatodistus_signing.clj
@@ -57,30 +57,29 @@
                            [{:type :name-does-not-match :response 403}
                             {:type :signed-pdf-exists :response 409}
                             {:type :expired-signing-certificate :response 400}]))}}]
-   ;; (if config/allow-new-signature-implementation
-   ["/system-sign/:language"
-    {:put {:summary    "Luo järjestelmällä allekirjoitettu PDF"
-           :parameters {:path {:id       common-schema/Key
-                               :language schema/Str}}
-           :access     rooli-service/laatija?
-           :responses  {200 {:body nil}
-                        404 {:body schema/Str}}
-           :handler    (fn [{{{:keys [id language]} :path} :parameters :keys [db aws-s3-client aws-kms-client whoami]}]
-                         (api-response/with-exceptions
-                           #(api-response/signature-response
-                              (energiatodistus-pdf-service/sign-with-system
-                                {:db             db
-                                 :aws-s3-client  aws-s3-client
-                                 :aws-kms-client aws-kms-client
-                                 :whoami         whoami
-                                 :now            (Instant/now)
-                                 :id             id
-                                 :language       language})
-                              (str id "/" language))
-                           [{:type :name-does-not-match :response 403}
-                            {:type :signed-pdf-exists :response 409}
-                            {:type :expired-signing-certificate :response 400}]))}}]
-   ;;)
+   (if config/allow-new-signature-implementation
+     ["/system-sign/:language"
+      {:put {:summary    "Luo järjestelmällä allekirjoitettu PDF"
+             :parameters {:path {:id       common-schema/Key
+                                 :language schema/Str}}
+             :access     rooli-service/laatija?
+             :responses  {200 {:body nil}
+                          404 {:body schema/Str}}
+             :handler    (fn [{{{:keys [id language]} :path} :parameters :keys [db aws-s3-client aws-kms-client whoami]}]
+                           (api-response/with-exceptions
+                             #(api-response/signature-response
+                                (energiatodistus-pdf-service/sign-with-system
+                                  {:db             db
+                                   :aws-s3-client  aws-s3-client
+                                   :aws-kms-client aws-kms-client
+                                   :whoami         whoami
+                                   :now            (Instant/now)
+                                   :id             id
+                                   :language       language})
+                                (str id "/" language))
+                             [{:type :name-does-not-match :response 403}
+                              {:type :signed-pdf-exists :response 409}
+                              {:type :expired-signing-certificate :response 400}]))}}])
    ["/finish"
     {:post {:summary    "Siirrä energiatodistus allekirjoitettu-tilaan"
             :parameters {:path {:id common-schema/Key}}
