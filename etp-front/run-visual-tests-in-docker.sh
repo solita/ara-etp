73a0184de4d44c5a232a50385489580f70442b5a
Juho Leinonen <juho.leinonen@solita.fi>
2024-04-11 15:18:42 +0300

    Copy visual test image snapshots from the container every time, not just when using update
    - Copies the diffs on fail now too
    - Document the script better

--- etp-front/run-visual-tests-in-docker.sh
+++ etp-front/run-visual-tests-in-docker.sh
@@ -6,16 +6,20 @@ then
 else
   update=false
 fi
+
 docker build -t etp-front-visual-tests -f visual-tests.Dockerfile .
 
+# Run the visual tests inside a container, pass the flag whether to update image snapshots or not
 docker run --name etp-front-visual-tests-container etp-front-visual-tests ./run-visual-tests.sh "$update"
 status=$?
 
-if [ "$update" == true ]
-then
-  docker cp etp-front-visual-tests-container:/visual-tests/__snapshots__ .
-fi
+# Delete previous diffs
+rm -rf __snapshots__/__diff_output__
+
+# Copy the results to local machine from the container
+docker cp etp-front-visual-tests-container:/visual-tests/__snapshots__ .
 
 docker rm etp-front-visual-tests-container
 
+# Exit with the code returned by the test runner in the container
 exit $status
