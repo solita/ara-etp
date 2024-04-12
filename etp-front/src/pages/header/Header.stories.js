3bb9e7d4257662e0b5fd613045f213838abb7ed4
Juho Leinonen <juho.leinonen@solita.fi>
2024-04-12 08:31:53 +0300

    Load static assets in storybook and add background to Header story using decorator

--- etp-front/src/pages/header/Header.stories.js
+++ etp-front/src/pages/header/Header.stories.js
@@ -1,6 +1,11 @@
 import Header from './Header';
+import HeaderDecorator from '@Pages/header/HeaderDecorator.svelte';
 
-export default { title: 'Header' };
+export default {
+  title: 'Header',
+  // Add background to the Header, as its contents are only in white
+  decorators: [() => HeaderDecorator]
+};
 
 export const withText = () => ({
   Component: Header,
---
+++ etp-front/src/pages/header/HeaderDecorator.svelte
@@ -0,0 +1,9 @@
+<style type="text/postcss">
+  div {
+    @apply bg-secondary;
+  }
+</style>
+
+<div>
+  <slot />
+</div>
