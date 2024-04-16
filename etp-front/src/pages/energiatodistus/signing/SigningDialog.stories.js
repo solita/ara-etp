e5aabd12e69a30105cc265771c74481e141b0b0f
Juho Leinonen <juho.leinonen@solita.fi>
2024-04-16 12:47:53 +0300

    AE-2164: Add story for SigningDialog

---
+++ etp-front/src/pages/energiatodistus/signing/SigningDialog.stories.js
@@ -0,0 +1,9 @@
+import SigningDialog from './SigningDialog.svelte';
+import { energiatodistus2018 } from '@Pages/energiatodistus/empty';
+
+export default { title: 'SigningDialog' };
+
+export const signingWithCardNoConnection = () => ({
+  Component: SigningDialog,
+  props: { energiatodistus: energiatodistus2018() }
+});
