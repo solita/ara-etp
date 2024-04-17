ab7fcee8d441f9ea6e9d4a77f85445b8f1f67770
Juho Leinonen <juho.leinonen@solita.fi>
2024-04-17 12:40:46 +0300

    AE-2164: Move general dialog related code back to SigningDialog from CardSigning

--- etp-front/src/pages/energiatodistus/signing/CardSigning.svelte
+++ etp-front/src/pages/energiatodistus/signing/CardSigning.svelte
@@ -175,14 +175,6 @@
 </script>
 
 <style type="text/postcss">
-  .content {
-    @apply relative bg-light w-2/3 py-10 px-10 rounded-md shadow-lg flex flex-col justify-center;
-  }
-
-  h1 {
-    @apply text-secondary font-bold uppercase text-lg mb-4 pb-2 border-b-1 border-tertiary tracking-xl;
-  }
-
   p {
     @apply mt-2;
   }
@@ -196,9 +188,7 @@
   }
 </style>
 
-<div class="content">
-  <h1>{i18n('energiatodistus.signing.header')}</h1>
-
+<div>
   {#if mPollux.failure.isSome()}
     <p>
       {i18n('energiatodistus.signing.messages.connection-failed')}
--- etp-front/src/pages/energiatodistus/signing/SigningDialog.svelte
+++ etp-front/src/pages/energiatodistus/signing/SigningDialog.svelte
@@ -1,16 +1,31 @@
 <script>
+  import { _ } from '@Language/i18n';
+
   import CardSigning from './CardSigning.svelte';
 
   export let energiatodistus;
   export let reload;
+
+  const i18n = $_;
 </script>
 
 <style type="text/postcss">
   dialog {
     @apply fixed top-0 w-screen left-0 z-50 h-screen bg-hr cursor-default flex justify-center items-center;
   }
+
+  .content {
+    @apply relative bg-light w-2/3 py-10 px-10 rounded-md shadow-lg flex flex-col justify-center;
+  }
+
+  h1 {
+    @apply text-secondary font-bold uppercase text-lg mb-4 pb-2 border-b-1 border-tertiary tracking-xl;
+  }
 </style>
 
 <dialog on:click|stopPropagation>
-  <CardSigning {energiatodistus} {reload} />
+  <div class="content">
+    <h1>{i18n('energiatodistus.signing.header')}</h1>
+    <CardSigning {energiatodistus} {reload} />
+  </div>
 </dialog>
