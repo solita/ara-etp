ba57142ac97dca25714743f2d9fdbd5b6a40dd2b
Juho Leinonen <juho.leinonen@solita.fi>
2024-04-30 10:41:28 +0300

    AE-2214: Add Swedish version of laatijarekisteri tietosuojaseloste

--- etp-public/src/pages/tietosuojaseloste-laatijarekisteri.svelte
+++ etp-public/src/pages/tietosuojaseloste-laatijarekisteri.svelte
@@ -1,10 +1,11 @@
 <script>
   import { locale } from '@Localization/localization';
   import TietosuojaselosteLaatijarekisteriFi from '@Page/tietosuojaseloste-laatijarekisteri-fi';
+  import TietosuojaselosteLaatijarekisteriSv from '@Page/tietosuojaseloste-laatijarekisteri-sv';
 </script>
 
 {#if $locale === 'sv'}
-  <p>TBD</p>
+  <TietosuojaselosteLaatijarekisteriSv />
 {:else}
   <TietosuojaselosteLaatijarekisteriFi />
 {/if}
