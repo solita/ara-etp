d52e31a96f38148e51f70f34e428a1a3cbc4656a
Juho Haapakoski <juho.haapakoski@solita.fi>
2024-05-08 12:31:37 +0300

    AE-2165: Put default KMS key and certificates to git instead of generating them via scripts
    These are used only in local development environment and CI.

--- etp-core/docker/start.sh
+++ etp-core/docker/start.sh
@@ -5,9 +5,6 @@ cd "$(dirname "$0")"
 mkdir -p smtp/received-emails
 find sftp/ssh -iname "*_key" -exec chmod 600 {} \;
 
-#./kms/generate-key-and-certificates.sh
-./kms/use-default-key-and-certs.sh
-
 docker compose up -d
 
 docker compose cp minio/files minio:/files
