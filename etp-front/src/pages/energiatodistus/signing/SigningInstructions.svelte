be053ca815cde6fdd4db48167c83977fbda53fcc
Juho Leinonen <juho.leinonen@solita.fi>
2024-05-16 11:37:48 +0300

    AE-2208: Add preliminary signing istructions to signing dialog

--- etp-front/src/pages/energiatodistus/signing/SigningInstructions.svelte
+++ etp-front/src/pages/energiatodistus/signing/SigningInstructions.svelte
@@ -1,3 +1,9 @@
 <div data-cy="signing-instructions">
-  <p>Allekirjoittamiseen liittyvät ohjeet tulevat tähän</p>
+  <p>
+    Voit allekirjoittaa energiatodistuksen käyttäen henkilökohtaista
+    henkilökorttia ja Mpollux-kortinlukuohjelmaa. Vaihtoehtoisesti voit
+    allekirjoittaa todistuksen vahvaa tunnistautumista ja
+    Energiatodistuspalvelun allekirjoitussertifikaattia käyttäen, jolloin et
+    tarvitse henkilökorttia.
+  </p>
 </div>
