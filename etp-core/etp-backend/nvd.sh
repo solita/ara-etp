f730cd5aa25cf3ade2fd942e531add18462f9ecd
Juho Haapakoski <juho.haapakoski@solita.fi>
2024-08-13 15:03:29 +0300

    AE-2264: Fix NVD tool by manually updating dependency-check-core

--- etp-core/etp-backend/nvd.sh
+++ etp-core/etp-backend/nvd.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-clojure -Ttools install nvd-clojure/nvd-clojure '{:mvn/version "4.0.0"}' :as nvd
-clojure -J-Dclojure.main.report=stderr -Tnvd nvd.task/check :classpath \""$(clojure -Spath)\"" :config-filename \""nvd-config.edn\""
+# FIXME: dependency-check-core is currently updated manually. It also required updating org.slf4j/slf4j-api. Remove when new version of nvd-clojure is released.
+clojure -Ttools install nvd-clojure/nvd-clojure '{:mvn/version "4.0.0" :exclusions [org.owasp/dependency-check-core]}' :as nvd
+clojure -J-Dclojure.main.report=stderr -Sdeps '{:deps {org.slf4j/slf4j-api {:mvn/version "2.0.10"} org.owasp/dependency-check-core {:mvn/version "10.0.3"}}}' -Tnvd nvd.task/check :classpath \""$(clojure -Spath)\"" :config-filename \""nvd-config.edn\""
