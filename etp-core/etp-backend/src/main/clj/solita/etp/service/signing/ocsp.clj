61f8bb7efedbec01d7bed2fd4aab435eb8e59a7b
Juho Haapakoski <juho.haapakoski@solita.fi>
2024-11-21 13:47:28 +0200

    Get the certificates from a signed pdf

--- etp-core/etp-backend/src/main/clj/solita/etp/service/signing/ocsp.clj
+++ etp-core/etp-backend/src/main/clj/solita/etp/service/signing/ocsp.clj
@@ -4,10 +4,10 @@
             [solita.etp.exception :as exception]
             [clojure.tools.logging :as log])
   (:import
-    (java.io ByteArrayInputStream File FileInputStream FileOutputStream InputStream)
+    (java.io ByteArrayInputStream File FileInputStream FileOutputStream IOException InputStream)
     (java.nio.charset StandardCharsets)
     (java.util Collection Comparator HashSet Optional)
-    (java.security MessageDigest NoSuchAlgorithmException Security)
+    (java.security GeneralSecurityException InvalidKeyException MessageDigest NoSuchAlgorithmException Provider PublicKey Security SignatureException)
     (java.security.cert CertificateException X509Certificate CertificateFactory)
     (org.apache.pdfbox.cos COSBase COSName)
     (org.apache.pdfbox.pdmodel PDDocument)
@@ -21,6 +21,8 @@
     (org.bouncycastle.operator DefaultDigestAlgorithmIdentifierFinder)
     (org.bouncycastle.operator.jcajce JcaDigestCalculatorProviderBuilder)
     (org.bouncycastle.util Store)))
+
+(def max-certificate-chain-depth 5)
 
 (defn get-ocsp-uri
   "Gets the OCSP URI from the certificate's Authority Information Access"
@@ -66,15 +68,15 @@
 
 (defn get-cert-information [^X509Certificate cert ^X509Certificate issuer-cert]
   {:certificate            cert
-   :signatureHash          ""
-   :self-signed?           ""
+   :signatureHash          nil
+   :self-signed?           nil
    :ocsp-url               (get-ocsp-uri cert)
-   :issuer-url             ""
-   :crl-url                ""
-   :issuer-certificate     ""
-   :cert-chain             ""
-   :tsa-certs              ""
-   :alternative-cert-chain "what even is this?"})
+   :issuer-url             nil
+   :crl-url                nil
+   :issuer-certificate     nil
+   :cert-chain             nil
+   :tsa-certs              nil
+   :alternative-cert-chain nil})
 
 (defn get-last-relevant-signature [^PDDocument document]
   (let [comparatorByOffset (Comparator/comparing (fn [^PDSignature sig] (-> sig .getByteRange (aget 1))))
@@ -116,12 +118,19 @@
     (try
       (-> certificate-holder
           get-cert-from-holder
-          )
+          (#(certificate-set (.add %))))
       (catch Exception e
-        ))))
+        (if (= (-> e ex-data :type) :certificate-processing-exception)
+          (log/warn "Certificate Exception getting Certificate from certHolder." e)
+          (throw e))))))
 
 
-(defn process-signer-store [^CMSSignedData signed-data {:keys [certificate]}]
+(defn traverse-chain [certificate {:keys [^X509Certificate certificate] :as cert-info} max-certificate-chain-depth]
+  (let [^bytes authority-extension-value (-> certificate (.getExtensionValue (-> Extension/authorityInfoAccess .getId)))]
+    (when authority-extension-value
+      #_(get-authority-info-extension-value))))
+
+(defn process-signer-store [^CMSSignedData signed-data {:keys [certificate] :as cert-info}]
   (let [^Collection signers (-> signed-data .getSignerInfos .getSigners)
         ^SignerInformation signer-information (-> signers .iterator .next)
         ^Store certificates-store (-> signed-data .getCertificates)
@@ -129,9 +138,11 @@
         ^X509Certificate certificate (-> matches .iterator .next get-cert-from-holder)
         certificate-set (-> (HashSet.) (.add certificate))
         ^Collection all-certs (-> certificates-store (.getMatches nil))]
-    (add-all-certs all-certs certificate-set)))
+    (add-all-certs all-certs certificate-set)
+    (traverse-chain certificate cert-info max-certificate-chain-depth)
+    ))
 
-(defn get-cert-info [signature-content]
+(defn get-cert-info [^bytes signature-content]
   (let [root-cert-info {:signature-hash (get-sha1-hash signature-content)}]
     (try
       (let [^CMSSignedData signed-data (CMSSignedData. signature-content)
@@ -149,11 +160,15 @@
     (let [signature-content (.getContents signature)]
       (get-cert-info signature-content))))
 
+
 (defn do-validation [filename output {:keys [document]}]
   (let [^PDSignature signature (get-last-relevant-signature document)
         cert-info (get-last-cert-info signature filename)
 
-        ]))
+        ]
+    (.saveIncremental document output)
+
+    ))
 
 (defn validate-signature [^File in-file ^File out-file]
   ;; TODO: Handle file errors? Will change to S3 eventually anyway...
@@ -175,3 +190,56 @@
     (validate-signature in-file out-file)))
 
 
+(defn- self-signed? [^X509Certificate certificate]
+  (try
+    (let [^PublicKey key (-> certificate .getPublicKey)]
+      (-> certificate (.verify key (-> (SecurityProvider/getProvider) .getName))))
+    ;; If nothing is thrown the certificate is self-signed.
+    true
+    (catch Exception e
+      (if (or (instance? SignatureException e)
+              (instance? InvalidKeyException e)
+              (instance? IOException e))
+        false
+        (throw e)))))
+
+(defn- find-issuer [certificate all-certificates]
+  (if (self-signed? certificate)
+    {:self-signed? true}
+    (some
+      (fn [potential-issuer]
+        (try
+          (-> certificate (.verify (-> potential-issuer .getPublicKey) (-> (SecurityProvider/getProvider) .getName)))
+          ;; If nothing was thrown we have found the issuer
+          {:issuer potential-issuer}
+
+          ;; This means that this is not the issuer.
+          (catch GeneralSecurityException e)))
+      all-certificates)))
+
+(defn get-certificates [^PDDocument document]
+  (let [last-relevant-signature (get-last-relevant-signature document)
+        ^bytes signature-content (.getContents last-relevant-signature)
+        signed-data (CMSSignedData. signature-content)
+
+        signers (-> signed-data .getSignerInfos .getSigners)
+        ^SignerInformation signer-information (-> signers .iterator .next)
+        certificate-store (-> signed-data .getCertificates)
+        matches (-> certificate-store (.getMatches (-> signer-information .getSID)))
+
+        cert-converter (JcaX509CertificateConverter.)
+
+        certificate-holder (-> matches .iterator .next)
+        leaf-cert (-> cert-converter (.getCertificate certificate-holder))
+
+        all-cert-holders (-> certificate-store (.getMatches nil))
+        all-certs (map #(-> cert-converter (.getCertificate %)) all-cert-holders)
+
+        intermediate-cert (:issuer (find-issuer leaf-cert all-certs))
+        root-cert (:issuer (find-issuer intermediate-cert all-certs))]
+    {:root-certificate         root-cert
+     :intermediate-certificate intermediate-cert
+     :leaf-certificate         leaf-cert}))
+
+(def pdf-file (PDDocument/load (File. "src/test/resources/energiatodistukset/signed-with-ocsp-information.pdf")))
+(get-certificates pdf-file)
