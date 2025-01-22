c9c777d318fd72e63fef68401ebf77112b4abac9
Joonas Sarajärvi <joonas.sarajarvi@solita.fi>
2025-01-22 10:45:58 +0100

    Reformat etp-public

--- etp-public/src/pages/saavutettavuusseloste-fi.svelte
+++ etp-public/src/pages/saavutettavuusseloste-fi.svelte
@@ -156,7 +156,8 @@
       </p>
 
       <p>
-        <Link href="https://www.saavutettavuusvaatimukset.fi/fi/kayttajan-oikeudet"
+        <Link
+          href="https://www.saavutettavuusvaatimukset.fi/fi/kayttajan-oikeudet"
           >Palvelun käyttäjän oikeudet (saavutettavuusvaatimukset.fi)</Link>
       </p>
 
