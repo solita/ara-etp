1721649c17ebe1433874af0b469c2fa0c7fc77fb
etahto <elias.tahto@solita.fi>
2025-02-05 10:35:19 +0200

    Update text-editor visual tests to not have user input tested

--- etp-front/src/components/text-editor/text-editor.stories.js
+++ etp-front/src/components/text-editor/text-editor.stories.js
@@ -2,13 +2,12 @@ import TextEditor from './text-editor.svelte';
 
 export default { title: 'TextEditor' };
 
-export const withText = () => ({
+export const withDefaults = () => ({
   Component: TextEditor,
   props: {
     id: 'test',
     name: 'test',
     label: 'test',
     required: true,
-    model: 'test content'
   }
 });
