f410863e640b9b604fe3d6d8e40d779d172241c8
etahto <elias.tahto@solita.fi>
2025-02-10 12:00:03 +0200

    AE-2430: Change Oikeellisuden Valvonta Swedish texts to include new customer organization

--- etp-core/etp-db/src/main/sql/migration/repeatable/vo-template/r-vo-template-tietopyynto-sv.sql
+++ etp-core/etp-db/src/main/sql/migration/repeatable/vo-template/r-vo-template-tietopyynto-sv.sql
@@ -13,11 +13,11 @@ values (7, 'Tietopyyntö (sv)', 'Begäran om inlämning (sv)', 2, 3, 'sv',
     {{#energiatodistus}}
     <table class="sarake">
         <tr>
-            <td>Objekt:</td> 
+            <td>Objekt:</td>
             <td>
                 <div class="nowrap">{{nimi}}</div>
-                <div class="nowrap">{{katuosoite}}</div>  
-                <div class="nowrap">{{postinumero}} {{postitoimipaikka}}</div> 
+                <div class="nowrap">{{katuosoite}}</div>
+                <div class="nowrap">{{postinumero}} {{postitoimipaikka}}</div>
             </td>
         </tr>
     </table>
@@ -26,12 +26,12 @@ values (7, 'Tietopyyntö (sv)', 'Begäran om inlämning (sv)', 2, 3, 'sv',
     {{/energiatodistus}}
 </p>
 
-<p>Finansierings- och utvecklingscentralen för boendet (ARA) har till uppgift att övervaka riktigheten hos
+<p>Centralen för statligt stött bostadsbyggande (Varke) har till uppgift att övervaka riktigheten hos
     energicertifikat. Riktighetskontrollerna rör energicertifikatens utgångsuppgifter, beräkningen av
     energieffektivitetstal och riktigheten hos besparingsrekommendationer.</p>
 
-<p><b>ARA kommer att kontrollera det här certifikatets riktighet.</b> Vi ber er skicka följande bakgrundsmaterial som
-    använts vid upprättandet av certifikatet till ARA:s energicertifikatsregister senast {{määräpäivä}}:</p>
+<p><b>Varke kommer att kontrollera det här certifikatets riktighet.</b> Vi ber er skicka följande bakgrundsmaterial som
+    använts vid upprättandet av certifikatet till Varkes energicertifikatsregister senast {{määräpäivä}}:</p>
 <ul>
     <li>Huvudritningar (plan-, fasad- och sektionsritningar samt U-värden)</li>
     <li>Beräkningar av ventilations-, kylnings- och värmesystem samt tekniska uppgifter (ej planritningar) som påverkar
@@ -44,16 +44,16 @@ values (7, 'Tietopyyntö (sv)', 'Begäran om inlämning (sv)', 2, 3, 'sv',
 
 <div class="sivunvaihto"></div>
 
-<p>ARA har rätt att få de uppgifter och dokument som behövs för övervakning, inklusive uppgifter om uppdraget.
+<p>Varke har rätt att få de uppgifter och dokument som behövs för övervakning, inklusive uppgifter om uppdraget.
     Upprättaren ska bevara beredningshandlingarna, beräkningarna och övriga uppgifter som han eller hon har utarbetat
     eller skaffat för upprättandet av energicertifikat samt uppgifterna om observationer som gjorts på det objekt som
     certifikatet gäller. Upprättaren ska ha ett arkiv över certifikaten. Handlingarna, uppgifterna och certifikaten ska
     bevaras i minst 12 år.</p>
 
-<p>Om upprättaren av energicertifikatet inte uppfyller de reglerade skyldigheterna uppmanar ARA upprättaren att
+<p>Om upprättaren av energicertifikatet inte uppfyller de reglerade skyldigheterna uppmanar Varke upprättaren att
     korrigera saken och ställer upp en tidsfrist för korrigeringen. Om saken inte korrigeras inom tidsfristen tilldelar
-    ARA upprättaren en varning och en ny tidsfrist. Om saken alltjämt inte korrigeras kan ARA ge upprättaren ett
-    förpliktande beslut om order/beslut om användningsförbud, som kan förenas med vite. ARA kan också ge upprättaren ett
+    Varke upprättaren en varning och en ny tidsfrist. Om saken alltjämt inte korrigeras kan Varke ge upprättaren ett
+    förpliktande beslut om order/beslut om användningsförbud, som kan förenas med vite. Varke kan också ge upprättaren ett
     förbud om upprättande, om upprättaren har agerat i strid med bestämmelserna på ett väsentligt eller betydande sätt.
 </p>
 
@@ -75,7 +75,7 @@ values (7, 'Tietopyyntö (sv)', 'Begäran om inlämning (sv)', 2, 3, 'sv',
     </tr>
 </table>
 $$)
-on conflict (id) do update set 
+on conflict (id) do update set
   label_fi = excluded.label_fi,
   label_sv = excluded.label_sv,
   ordinal = excluded.ordinal,
