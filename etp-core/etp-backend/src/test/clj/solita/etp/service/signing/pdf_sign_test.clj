2d33d0038847ac9c53dffae3b8a6ec83d406ffb2
Juho Haapakoski <juho.haapakoski@solita.fi>
2025-02-12 11:33:24 +0200

    Add system signing test

--- etp-core/etp-backend/src/test/clj/solita/etp/service/signing/pdf_sign_test.clj
+++ etp-core/etp-backend/src/test/clj/solita/etp/service/signing/pdf_sign_test.clj
@@ -1,15 +1,21 @@
 (ns solita.etp.service.signing.pdf-sign-test
-  (:require [clojure.test :as t]
+  (:require [clojure.java.io :as io]
+            [clojure.test :as t]
+            [solita.common.time :as time]
             [solita.etp.config :as config]
             [solita.etp.service.energiatodistus :as energiatodistus-service]
-            [solita.etp.service.energiatodistus-signing :as et-signing]
+            [solita.etp.service.energiatodistus-signing :as energiatodistus-signing-service]
+            [solita.etp.service.file :as file-service]
             [solita.etp.service.laatija :as laatija-service]
+            [solita.etp.service.rooli :as rooli-service]
             [solita.etp.service.signing.pdf-sign :as pdf-sign]
             [solita.etp.test-data.laatija :as test-data.laatija]
             [solita.etp.test-data.energiatodistus :as test-data.energiatodistus]
             [solita.etp.test-system :as ts])
   (:import (eu.europa.esig.dss.spi.x509.tsp KeyEntityTSPSource)
+           (java.io File InputStream)
            (java.nio.charset StandardCharsets)
+           (org.apache.pdfbox.pdmodel PDDocument)
            (org.bouncycastle.cert.jcajce JcaX509v3CertificateBuilder)
            (org.bouncycastle.jce.provider BouncyCastleProvider)
            (org.bouncycastle.asn1.x500 X500Name)
@@ -67,43 +73,49 @@
 
 (defn get-allekirjoitus-id [laatija]
   ;; TODO: Need to update test-system to actually add an allekirjoitus-id
+  ;;       Maybe also schema?
+  ;;       Maybe create a generator?
   ;; For the tests it does not currently matter: The allekirjoitus-id will be wrong
   ;; in the tests but it does not matter.
   "1")
 
-#_(t/deftest sign-with-system-testing
-
+(t/deftest sign-with-system-testing
   (with-bindings
     ;; Use an already existing pdf.
     {#'solita.etp.service.signing.pdf-sign/get-tsp-source tsp-source-in-test}
-  (let [; Add laatija
-        laatija-add (->> (test-data.laatija/generate-adds 1) (first))
-        laatija-id (first (test-data.laatija/insert! [laatija-add]))
-        laatija (laatija-service/find-laatija-by-id
-                  ts/*db* {:id laatija-id :rooli 6} laatija-id)
-        laatija-allekirjoitus-id (get-allekirjoitus-id laatija)
-        certs {:root-cert config/system-signature-certificate-root
-               :int-cert  config/system-signature-certificate-intermediate
-               :leaf-cert config/system-signature-certificate-leaf}
+    (let [; Add laatija
+          laatija-add (->> (test-data.laatija/generate-adds 1) (first))
+          laatija-id (first (test-data.laatija/insert! [laatija-add]))
+          laatija (laatija-service/find-laatija-by-id
+                    ts/*db* {:id laatija-id :rooli 6} laatija-id)
+          whoami {:id laatija-id :rooli 0}
+          laatija-allekirjoitus-id (get-allekirjoitus-id laatija)
 
-        todistus-2018-fi (-> (test-data.energiatodistus/generate-add 2018 true) (assoc-in [:perustiedot :kieli] 0))
+          todistus-2018-fi (-> (test-data.energiatodistus/generate-add 2018 true) (assoc-in [:perustiedot :kieli] 0))
 
-        [todistus-2018-fi-id]
-        (test-data.energiatodistus/insert! [todistus-2018-fi] laatija-id)]
+          [todistus-2018-fi-id]
+          (test-data.energiatodistus/insert! [todistus-2018-fi] laatija-id)]
 
-    (t/testing "aaa"
-      (energiatodistus-service/start-energiatodistus-signing! ts/*db* {:rooli 0 :id laatija-id} todistus-2018-fi-id)
-      (let [digest (et-signing/find-energiatodistus-digest-new ts/*db* ts/*aws-s3-client* todistus-2018-fi-id "fi" laatija-allekirjoitus-id certs)
-            _ (println "D:" digest)
-            data (-> digest
-                     :digest
-                     (.getBytes StandardCharsets/UTF_8)
-                     (#(.decode (Base64/getDecoder) %)))
-            signature (pdf-sign/get-signature-from-system-cms-service ts/*aws-kms-client* data)
-            b-level-doc (et-signing/sign-energiatodistus-pdf-new ts/*db* ts/*aws-s3-client* todistus-2018-fi-id "fi" laatija-allekirjoitus-id certs signature)
-            _ (.save b-level-doc "WASD.pdf")
-            ])))))
+      (t/testing "The energiatodistus is LT level"
+        (energiatodistus-signing-service/sign-with-system
+          {:db                       ts/*db*
+           :aws-s3-client            ts/*aws-s3-client*
+           :aws-kms-client           ts/*aws-kms-client*
+           :whoami                   whoami
+           :laatija-allekirjoitus-id laatija-allekirjoitus-id
+           :now                      (time/now)
+           :id                       todistus-2018-fi-id})
+        ;;TODO: Do automatic testing
+        (-> (PDDocument/load ^InputStream (file-service/find-file
+                                            ts/*aws-s3-client*
+                                            (solita.etp.service.energiatodistus/file-key (str todistus-2018-fi-id) "fi")))
+            (.save "HELLO.pdf"))))))
 
 (t/deftest signed-pdf-has-laatija-id
+
+  )
+
+(t/deftest create-stateful-signature-parameters-test
+  ()
 
   )
