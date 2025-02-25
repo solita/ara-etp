b47377a69c030c76b801fa3415c628b7673c0c7c
Joonas Sarajärvi <joonas.sarajarvi@solita.fi>
2025-02-25 12:40:41 +0100

    Fix formation of path to resource

--- etp-core/etp-backend/src/test/clj/solita/etp/service/suomifi_viestit_test.clj
+++ etp-core/etp-backend/src/test/clj/solita/etp/service/suomifi_viestit_test.clj
@@ -1,12 +1,13 @@
 (ns solita.etp.service.suomifi-viestit-test
-  (:require [clojure.test :as t]
-            [solita.etp.test-system :as ts]
-            [clojure.java.io :as io]
-            [solita.etp.service.valvonta-kaytto.suomifi-viestit :as valvonta-kaytto.suomifi-viestit]
-            [solita.etp.service.suomifi-viestit :as service.suomifi-viestit]
+  (:require [clojure.java.io :as io]
             [clojure.string :as str]
-            [solita.etp.service.pdf :as pdf])
-  (:import (java.time LocalDate)
+            [clojure.test :as t]
+            [solita.etp.service.pdf :as pdf]
+            [solita.etp.service.suomifi-viestit :as service.suomifi-viestit]
+            [solita.etp.service.valvonta-kaytto.suomifi-viestit :as valvonta-kaytto.suomifi-viestit]
+            [solita.etp.test-system :as ts])
+  (:import (java.nio.file Paths)
+           (java.time LocalDate)
            (org.w3c.dom Node)
            (org.xmlunit.diff Comparison$Detail ComparisonListener ComparisonResult ComparisonType DOMDifferenceEngine DifferenceEvaluator DifferenceEvaluators)
            (org.xmlunit.builder Input)
@@ -132,7 +133,7 @@
                                                                                               202)
                   #'valvonta-kaytto.suomifi-viestit/now                      (fn [] "2021-09-08T06:21:03.625667Z")
                   #'valvonta-kaytto.suomifi-viestit/bytes->base64            (fn [_] "dGVzdGk=")}
-    (let [config-with-keystore (merge config {:keystore-file     (.getPath (io/resource "suomifi/store.jks"))
+    (let [config-with-keystore (merge config {:keystore-file     (-> (io/resource "suomifi/store.jks") .toURI Paths/get .toString)
                                               :keystore-password "password"
                                               :keystore-alias    "default"})]
       (t/is (= (:sanoma-tunniste (valvonta-kaytto.suomifi-viestit/send-message-to-osapuoli! valvonta toimenpide osapuoli document config-with-keystore))
