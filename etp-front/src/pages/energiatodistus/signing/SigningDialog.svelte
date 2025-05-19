897c5ef7e9354e54e5499bc6eca758621d5c2c47
Juho Haapakoski <juho.haapakoski@solita.fi>
2025-05-19 21:56:48 +0300

    AE-2490: Remove the option to sign with card from signing dialog
    There is no more need for the selection of the signing method either.

--- etp-front/src/pages/energiatodistus/signing/SigningDialog.svelte
+++ etp-front/src/pages/energiatodistus/signing/SigningDialog.svelte
@@ -14,21 +14,11 @@
   export let energiatodistus;
   export let reload;
 
-  export let selection = 'card';
-
   const i18n = $_;
-
-  const isSigningMethodCard = selectedMethod => selectedMethod === 'card';
 
   export let freshSession = false;
 
   let currentState = { status: Signing.status.not_started };
-
-  const isSigningMethodSelectionAllowed = state =>
-    R.includes(R.prop('status', state), [
-      Signing.status.not_started,
-      Signing.status.aborted
-    ]);
 
   Future.fork(
     _ => {
@@ -49,10 +39,6 @@
     @apply fixed top-0 w-screen left-0 z-50 h-screen bg-hr cursor-default flex justify-center items-center;
   }
 
-  .selection {
-    @apply flex mt-4;
-  }
-
   .content {
     @apply relative bg-light w-2/3 py-10 px-10 rounded-md shadow-lg flex flex-col justify-center;
   }
@@ -66,41 +52,15 @@
   <div class="content">
     <h1>{i18n('energiatodistus.signing.header')}</h1>
 
-    {#if isSigningMethodSelectionAllowed(currentState)}
-      <div class="mt-2" data-cy="signing-instructions">
-        <p>{i18n('energiatodistus.signing.instructions')}</p>
-      </div>
-
-      <div class="mt-2">
-        <div class="selection">
-          <div class="mr-3">
-            <Radio
-              label={i18n('energiatodistus.signing.options.card')}
-              bind:group={selection}
-              value="card"
-              name="Card" />
-          </div>
-          <div class="mr-3">
-            <Radio
-              label={i18n('energiatodistus.signing.options.system')}
-              bind:group={selection}
-              value="system"
-              name="System" />
-          </div>
-        </div>
-      </div>
-    {/if}
-
+    <div class="mt-2" data-cy="signing-instructions">
+      <p>{i18n('energiatodistus.signing.instructions')}</p>
+    </div>
     <div class="mt-4">
-      {#if isSigningMethodCard(selection)}
-        <CardSigning {energiatodistus} {reload} bind:currentState />
-      {:else}
-        <SystemSigning
-          {energiatodistus}
-          {reload}
-          bind:currentState
-          bind:freshSession />
-      {/if}
+      <SystemSigning
+        {energiatodistus}
+        {reload}
+        bind:currentState
+        bind:freshSession />
     </div>
   </div>
 </dialog>
