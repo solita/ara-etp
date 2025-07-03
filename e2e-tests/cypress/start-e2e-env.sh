8d1b8f9789f656d8dae2960296ec09233785c7f5
Joonas Sarajärvi <joonas.sarajarvi@solita.fi>
2025-07-03 16:40:59 +0300

    A more careful workaround

--- e2e-tests/cypress/start-e2e-env.sh
+++ e2e-tests/cypress/start-e2e-env.sh
@@ -7,10 +7,16 @@ set -euxo pipefail
 # When migrations are ready, create cypress_test database
 # Start backend and wait for it and frontend to be ready
 
-docker compose build
 docker compose up --build -d frontend
 docker compose up --build migration-runner --exit-code-from migration-runner
 docker compose exec db dropdb -U postgres cypress_test --if-exists --force
 docker compose exec db createdb -U postgres -T etp_dev cypress_test
+
+# Workaround for an apparently random issue when running under GitHub Actions
+# The dependencies of the backend service do not always seem to be built before
+# compose attempts to start the backend service, and that will cause the final
+# `docker compose up` command to fail.
+docker compose build ocsp-responder-root
+
 echo -e "\e[1;33m Waiting for services to be healthy. Can take ~20s. \e[0m"
 docker compose up --build --wait frontend --wait frontend-public --wait backend
