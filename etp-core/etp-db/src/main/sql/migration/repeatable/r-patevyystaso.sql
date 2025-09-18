75606ee1221da30c94ad375a89bfdf898ceb2cce
Areta Santos <areta.santos@solita.fi>
2025-09-18 09:14:51 +0300

    AE-2645: Add values to the patevyystaso in the repeatable and fix the label to be consistent

--- etp-core/etp-db/src/main/sql/migration/repeatable/r-patevyystaso.sql
+++ etp-core/etp-db/src/main/sql/migration/repeatable/r-patevyystaso.sql
@@ -1,7 +1,10 @@
-insert into patevyystaso (id, label_fi, label_sv)
+insert into patevyystaso (id, label_fi, label_sv, valid)
 values
-(1, 'Perustaso', 'Basnivå'),
-(2, 'Ylempi taso', 'Högre nivå')
+(1, 'Perustaso', 'Basnivå', true),
+(2, 'Ylempi taso', 'Högre nivå', true),
+(3, 'Perustaso + PPP', 'Basnivå + PPP', true),
+(4, 'Ylempi taso + PPP', 'Högre nivå + PPP', true)
 on conflict (id) do update set
   label_fi = excluded.label_fi,
-  label_sv = excluded.label_sv
+  label_sv = excluded.label_sv,
+  valid = excluded.valid;
