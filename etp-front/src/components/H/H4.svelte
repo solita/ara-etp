7293645bd87294855a60b05f3a1a1ac3629591cf
Joonas Sarajärvi <joonas.sarajarvi@solita.fi>
2025-10-15 13:29:20 +0300

    Tweak the H4 style

--- etp-front/src/components/H/H4.svelte
+++ etp-front/src/components/H/H4.svelte
@@ -5,7 +5,7 @@
 
 <style type="text/postcss">
   h4 {
-    @apply text-secondary w-full font-bold uppercase text-h4 mt-0 mb-6 tracking-xl leading-header;
+    @apply text-secondary w-full font-bold uppercase text-h4 mt-0 mb-2 leading-header;
   }
 
   .compact {
