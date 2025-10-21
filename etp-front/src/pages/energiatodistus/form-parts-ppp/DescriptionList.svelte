82cae96aa672a81db0a531aa75428ea6c2b0a812
Juho Haapakoski <juho.haapakoski@solita.fi>
2025-10-21 06:51:50 +0300

    AE-2662: Add DescriptionList

---
+++ etp-front/src/pages/energiatodistus/form-parts-ppp/DescriptionList.svelte
@@ -0,0 +1,10 @@
+<dl
+  class="grid
+         grid-cols-2
+         [&>:is(dt,dd)]:border-b
+         [&>:is(dt,dd)]:px-4
+         [&>:is(dt,dd)]:py-3
+         [&>dd]:font-bold
+         [&>dt]:after:content-[':']">
+  <slot />
+</dl>
