a01d76744d3f5e9eefd85e2681c01b9ec580a773
etahto <elias.tahto@solita.fi>
2025-10-21 10:34:22 +0300

    AE-2672: Enhance table layout in EnergianHinta component for improved readability

--- etp-front/src/pages/energiatodistus/form-parts-ppp/energian-hinta.svelte
+++ etp-front/src/pages/energiatodistus/form-parts-ppp/energian-hinta.svelte
@@ -25,24 +25,25 @@
 <H3 text={$_('perusparannuspassi.energian-hinta.header')} />
 
 <div class="min-w-full overflow-x-auto md:overflow-x-hidden">
-  <table class="et-table et-table__noborder border-r-0">
+  <table class="et-table et-table__noborder border-r-0 table-fixed">
     <thead class="et-table--thead">
       <tr class="et-table--tr">
-        <th class="et-table--th et-table--th__half et-table--th-left-aligned">
+        <th class="et-table--th et-table--th-left-aligned et-table--th__twocells">
           {$_('perusparannuspassi.energian-hinta.energia')}
         </th>
-        <th class="et-table--th et-table--th__half et-table--th-right-aligned">
+        <th class="et-table--th et-table--th-right-aligned et-table--th__fifth">
           {$_('perusparannuspassi.energian-hinta.hinta-snt-kwh')}
         </th>
+        <th class="et-table--th w-3/5 invisible border-0 p-0"></th>
       </tr>
     </thead>
     <tbody class="et-table--tbody">
       {#each energiamuodot as energiamuoto}
         <tr class="et-table--tr">
-          <td class="et-table--td">
+          <td class="et-table--td et-table--th__twocells">
             {$_(`perusparannuspassi.energian-hinta.labels.${energiamuoto}`)}
           </td>
-          <td class="et-table--td">
+          <td class="et-table--td et-table--td__fifth">
             <Input
               {disabled}
               {schema}
@@ -51,6 +52,7 @@
               path={['energian-hinta', energiamuoto]}
               i18nRoot="perusparannuspassi" />
           </td>
+          <td class="et-table--td w-3/5 invisible border-0 p-0"></td>
         </tr>
       {/each}
     </tbody>
