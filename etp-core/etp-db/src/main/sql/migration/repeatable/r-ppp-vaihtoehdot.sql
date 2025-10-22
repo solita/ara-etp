34b9b46c2d19963554960aaa8a3a26101abc3035
Areta Santos <areta.santos@solita.fi>
2025-10-22 10:25:34 +0300

    AE-2660: fill swedish labels to classifications

--- etp-core/etp-db/src/main/sql/migration/repeatable/r-ppp-vaihtoehdot.sql
+++ etp-core/etp-db/src/main/sql/migration/repeatable/r-ppp-vaihtoehdot.sql
@@ -1,24 +1,27 @@
-insert into perusparannuspassi_uusiutuva_energia (id, label_fi, ordinal)
-values (0, 'Ei ole', 1),
-       (1, 'Aurinkosähkö', 2),
-       (2, 'Aurinkolämpö', 3),
-       (3, 'Aurinkosähkö ja -lämpö', 4),
-       (4, 'Tuulivoima', 5),
-       (5, 'Muu', 6)
+insert into perusparannuspassi_uusiutuva_energia (id, label_fi, label_sv, ordinal)
+values (0, 'Ei ole','Ei ole-sv?', 1),
+       (1, 'Aurinkosähkö', 'Aurinkosähkö-sv?',2),
+       (2, 'Aurinkolämpö', 'Aurinkolämpö-sv?',3),
+       (3, 'Aurinkosähkö ja -lämpö', 'Aurinkosähkö ja -lämpö-sv?', 4),
+       (4, 'Tuulivoima', 'Tuulivoima-sv?',5),
+       (5, 'Muu', 'Muu-sv?',6)
 on conflict (id) do update set label_fi = excluded.label_fi,
+                               label_sv = excluded.label_sv,
                                ordinal  = excluded.ordinal;
 
-insert into perusparannuspassi_jaahdytys (id, label_fi, ordinal)
-values (0, 'Ei ole', 1),
-       (1, 'Aurinkosuojaukset', 2),
-       (2, 'Muu', 3)
+insert into perusparannuspassi_jaahdytys (id, label_fi, label_sv, ordinal)
+values (0, 'Ei ole', 'Ei ole-sv?',1),
+       (1, 'Aurinkosuojaukset', 'Aurinkosuojaukset-sv?', 2),
+       (2, 'Muu', 'Muu-sv?',3)
 on conflict (id) do update set label_fi = excluded.label_fi,
+                               label_sv = excluded.label_sv,
                                ordinal  = excluded.ordinal;
 
-insert into perusparannuspassi_liittymismahdollisuus(id, label_fi, ordinal)
-values (0, 'Kyllä, kaukolämpöön', 1),
-       (1, 'Kyllä, Kaukojäähdytykseen', 2),
-       (2, 'Kyllä, molempiin', 3),
-       (3, 'Ei kumpaankaan', 4)
+insert into perusparannuspassi_liittymismahdollisuus(id, label_fi, label_sv, ordinal)
+values (0, 'Kyllä, kaukolämpöön', 'Kyllä, kaukolämpöön-sv?',1),
+       (1, 'Kyllä, Kaukojäähdytykseen', 'Kyllä, kaukolämpöön-sv?', 2),
+       (2, 'Kyllä, molempiin', 'Kyllä, molempiin-sv?', 3),
+       (3,  'Ei kumpaankaan', 'Ei kumpaankaan-sv?',4)
 on conflict (id) do update set label_fi = excluded.label_fi,
+                               label_sv = excluded.label_sv,
                                ordinal  = excluded.ordinal;
