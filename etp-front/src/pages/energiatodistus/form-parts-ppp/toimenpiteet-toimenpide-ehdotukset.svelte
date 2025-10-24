975173244561a1301c243d4f3ae687f640ce8a47
Juho Haapakoski <juho.haapakoski@solita.fi>
2025-10-24 21:22:31 +0300

    AE-2662: Add form-parts-ppp/toimenpiteet-toimenpide-ehdotukset

---
+++ etp-front/src/pages/energiatodistus/form-parts-ppp/toimenpiteet-toimenpide-ehdotukset.svelte
@@ -0,0 +1,71 @@
+<script>
+  import * as R from 'ramda';
+  import * as Maybe from '@Utility/maybe-utils';
+  import { _, locale } from '@Language/i18n';
+  import * as ETUtils from '@Pages/energiatodistus/energiatodistus-utils';
+  import * as LocaleUtils from '@Language/locale-utils';
+
+  import H5 from '@Component/H/H5';
+  import Select from '@Component/Select/Select';
+
+  import Textarea from '@Pages/energiatodistus/Textarea.svelte';
+
+  const i18n = $_;
+
+  export let perusparannuspassi;
+  export let pppSchema;
+  export let vaihe;
+  export let inputLanguage;
+  export let toimenpideEhdotuksetLuokittelu;
+
+  const vaiheIndex = vaihe => vaihe['vaihe-nro'] - 1;
+</script>
+
+<div>
+  <H5
+    text={i18n(
+      'perusparannuspassi.vaiheet.0.toimenpiteet.toimenpide-ehdotukset.header'
+    )}
+    } />
+  <div class="grid grid-cols-2 gap-6">
+    {#each R.range(0, 6) as toimenpideN}
+      <div>
+        <Select
+          items={R.pluck('id', toimenpideEhdotuksetLuokittelu)}
+          format={ETUtils.selectFormat(
+            LocaleUtils.label($locale),
+            toimenpideEhdotuksetLuokittelu
+          )}
+          validation={false}
+          required={false}
+          allowNone={true}
+          bind:model={perusparannuspassi}
+          lens={R.lensPath([
+            'vaiheet',
+            vaiheIndex(vaihe),
+            'toimenpiteet',
+            'toimenpide-ehdotukset',
+            toimenpideN
+          ])}
+          label={i18n(
+            'perusparannuspassi.vaiheet.0.toimenpiteet.toimenpide-ehdotukset.toimenpide-ehdotus-n',
+            { values: { n: toimenpideN + 1 } }
+          )} />
+      </div>
+    {/each}
+    <div class="col-span-2">
+      <Textarea
+        schema={pppSchema}
+        showOrdinal={false}
+        inputLanguage={Maybe.Some(inputLanguage)}
+        bind:model={perusparannuspassi}
+        path={[
+          'vaiheet',
+          vaiheIndex(vaihe),
+          'toimenpiteet',
+          'toimenpideseloste'
+        ]}
+        i18nRoot="perusparannuspassi" />
+    </div>
+  </div>
+</div>
