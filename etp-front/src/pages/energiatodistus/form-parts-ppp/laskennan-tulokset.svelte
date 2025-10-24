8d37d7f122f359b9661a72395513b0413f4dd969
Joonas Sarajärvi <joonas.sarajarvi@solita.fi>
2025-10-24 11:44:40 +0300

    AE-2661 Fix formatting

--- etp-front/src/pages/energiatodistus/form-parts-ppp/laskennan-tulokset.svelte
+++ etp-front/src/pages/energiatodistus/form-parts-ppp/laskennan-tulokset.svelte
@@ -2,10 +2,10 @@
   import H3 from '@Component/H/H3';
   import { _ } from '@Language/i18n';
 
-  import LaskennanTuloksetVaiheistus from './laskennan-tulokset-vaiheistus.svelte';
-  import LaskennanTuloksetLaskennallinenOstoenergia from './laskennan-tulokset-laskennallinen-ostoenergia.svelte';
-  import LaskennanTuloksetUusiutuvaEnergia from './laskennan-tulokset-uusiutuva-energia.svelte';
-  import LaskennanTuloksetToteutunutOstoenergia from './laskennan-tulokset-toteutunut-ostoenergia.svelte';
+  import Vaiheistus from './laskennan-tulokset-vaiheistus.svelte';
+  import LaskennallinenOstoenergia from './laskennan-tulokset-laskennallinen-ostoenergia.svelte';
+  import UusiutuvaEnergia from './laskennan-tulokset-uusiutuva-energia.svelte';
+  import ToteutunutOstoenergia from './laskennan-tulokset-toteutunut-ostoenergia.svelte';
 
   export let energiatodistus;
   export let perusparannuspassi;
@@ -15,11 +15,11 @@
 <div class="flex flex-col gap-6">
   <H3 text={$_('perusparannuspassi.laskennan-tulokset.header')} />
 
-  <LaskennanTuloksetVaiheistus bind:perusparannuspassi {schema} />
-  <LaskennanTuloksetLaskennallinenOstoenergia
+  <Vaiheistus bind:perusparannuspassi {schema} />
+  <LaskennallinenOstoenergia
     bind:perusparannuspassi
     {energiatodistus}
     {schema} />
-  <LaskennanTuloksetUusiutuvaEnergia bind:perusparannuspassi {energiatodistus} {schema} />
-  <LaskennanTuloksetToteutunutOstoenergia bind:perusparannuspassi {energiatodistus} {schema} />
+  <UusiutuvaEnergia bind:perusparannuspassi {energiatodistus} {schema} />
+  <ToteutunutOstoenergia bind:perusparannuspassi {energiatodistus} {schema} />
 </div>
