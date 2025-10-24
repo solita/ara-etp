bbeec910cbdb8a869151d3666735c64634d7e5f5
Juho Haapakoski <juho.haapakoski@solita.fi>
2025-10-24 21:21:26 +0300

    AE-2662: Add AE-2663 component with TODOs
    This is here mainly just to see what the osio looks like

---
+++ etp-front/src/pages/energiatodistus/form-parts-ppp/toimenpiteet-energiankulutuksen-muutos.svelte
@@ -0,0 +1,31 @@
+<script>
+  /* TODO: AE-2663: Put here just to see what the osiot will look like when opened.
+   */
+
+  import H5 from '@Component/H/H5';
+  import DescriptionList from '@Pages/energiatodistus/form-parts-ppp/DescriptionList.svelte';
+</script>
+
+<div>
+  <H5 text="TODO: AE-2663" />
+  <DescriptionList>
+    <dt>TODO</dt>
+    <dd>todo</dd>
+    <dt>TODO</dt>
+    <dd>todo</dd>
+    <dt>TODO</dt>
+    <dd>todo</dd>
+    <dt>TODO</dt>
+    <dd>todo</dd>
+    <dt>TODO</dt>
+    <dd>todo</dd>
+    <dt>TODO</dt>
+    <dd>todo</dd>
+    <dt>TODO</dt>
+    <dd>todo</dd>
+    <dt>TODO</dt>
+    <dd>todo</dd>
+    <dt>TODO</dt>
+    <dd>todo</dd>
+  </DescriptionList>
+</div>
