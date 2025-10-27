00400389f2b2f8af165285c789fc88a83fc70025
Juho Haapakoski <juho.haapakoski@solita.fi>
2025-10-27 12:05:44 +0200

    AE-2662: Add toimenpide-ehdotus luokittelu to api

--- etp-core/etp-backend/src/main/clj/solita/etp/service/luokittelu.clj
+++ etp-core/etp-backend/src/main/clj/solita/etp/service/luokittelu.clj
@@ -22,6 +22,7 @@
 (def find-mahdollisuus-liittya #(select-luokittelu % "perusparannuspassi_liittymismahdollisuus"))
 (def find-uusiutuva-energia #(select-luokittelu % "perusparannuspassi_uusiutuva_energia"))
 (def find-jaahdytys #(select-luokittelu % "perusparannuspassi_jaahdytys"))
+(def find-toimenpide-ehdotus #(select-luokittelu % "toimenpide_ehdotus"))
 
 (def find-vastaanottajaryhmat #(select-luokittelu % "vastaanottajaryhma"))
 
