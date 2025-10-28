9f4ba0c040a7d4965d2237b0c1ec2c41385333a5
Juho Haapakoski <juho.haapakoski@solita.fi>
2025-10-28 16:44:45 +0200

    AE-2662: Remove unused imports

--- etp-front/src/pages/energiatodistus/form-parts-ppp/toimenpiteet-toimenpide-perustiedot.svelte
+++ etp-front/src/pages/energiatodistus/form-parts-ppp/toimenpiteet-toimenpide-perustiedot.svelte
@@ -4,7 +4,6 @@
   import * as ETUtils from '@Pages/energiatodistus/energiatodistus-utils';
   import { _ } from '@Language/i18n';
 
-  import H5 from '@Component/H/H5';
   import ViewModelValue from '@Pages/energiatodistus/form-parts-ppp/toimenpiteet/ViewModelValue.svelte';
 
   import DescriptionList from './toimenpiteet/DescriptionList.svelte';
