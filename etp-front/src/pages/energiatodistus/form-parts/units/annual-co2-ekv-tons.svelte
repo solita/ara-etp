53f80c95e915fc3fcfb55ce4f086717a5bd0c507
Juho Haapakoski <juho.haapakoski@solita.fi>
2025-11-05 11:48:41 +0200

    AE-2663: Add aria-label for annual-co2-ekv-tons.svelte

--- etp-front/src/pages/energiatodistus/form-parts/units/annual-co2-ekv-tons.svelte
+++ etp-front/src/pages/energiatodistus/form-parts/units/annual-co2-ekv-tons.svelte
@@ -2,4 +2,10 @@
   import { _ } from '@Language/i18n';
 </script>
 
-tCO2ekv/{$_('units.year')}
+<span
+  aria-label={$_('units.screen-reader.co2-ekv-tons-per-year')}
+  aria-hidden="false">
+  <span aria-hidden="true">
+    tCO2ekv/{$_('units.year')}
+  </span>
+</span>
