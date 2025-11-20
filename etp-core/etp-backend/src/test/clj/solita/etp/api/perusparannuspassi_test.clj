50ca786fc916a86e80f3275d4bf2a030bee101b3
Joonas Sarajärvi <joonas.sarajarvi@solita.fi>
2025-11-20 15:45:15 +0200

    AE-2669 Use assert-status throughout the test

--- etp-core/etp-backend/src/test/clj/solita/etp/api/perusparannuspassi_test.clj
+++ etp-core/etp-backend/src/test/clj/solita/etp/api/perusparannuspassi_test.clj
@@ -43,10 +43,10 @@
                                    (mock/header "Content-Type" "application/json")
                                    (mock/body et-body)
                                    (laatija-test-data/with-suomifi-laatija)))]
-      (t/is (= 201 (:status post-res)) "Expected status 201 from ET creation")
+      (assert-status post-res 201 "Expected status 201 from ET creation")
       (let [et-id (-> post-res :body (j/read-value object-mapper) :id)]
-        (t/is ( = 1 et-id))
-        (t/is (= 201 (:status post-res)))[])))
+        (t/is (= 1 et-id))
+        (assert-status post-res 201))))
 
   (t/testing "create perusparannuspassi to energiatodistus with ppp-pätevyys and same laatija-id"
     (let [ppp (-> (ppp-test-data/generate-add 1))
@@ -57,74 +57,74 @@
                                    (mock/body ppp-body)
                                    (laatija-test-data/with-suomifi-laatija)))
           ppp-id (-> post-res :body (j/read-value object-mapper) :id)]
-      (t/is (= 201 (:status post-res)))
+      (assert-status post-res 201)
 
-    (let [get-res (ts/handler (-> (mock/request :get (str "/api/private/perusparannuspassit/2026/" ppp-id))
-                                  (mock/header "Accept" "application/json")
-                                  (laatija-test-data/with-suomifi-laatija)))
-          body-str (slurp (clojure.java.io/reader (:body get-res)))
-          full-ppp (j/read-value body-str object-mapper)
-          created-ppp (dissoc full-ppp :id :tila-id :laatija-id)]
-      (t/is (= 200 (:status get-res)))
-
-      (t/testing "create perusparannuspassi to energiatodistus with different laatija-id"
-        (let [ppp (-> (ppp-test-data/generate-add 1))
-              ppp-body (-> ppp j/write-value-as-string)
-              post-res (ts/handler (-> (mock/request :post "/api/private/perusparannuspassit/2026")
-                                       (mock/header "Accept" "application/json")
-                                       (mock/header "Content-Type" "application/json")
-                                       (laatija-test-data/with-suomifi-laatija2)
-                                       (mock/body ppp-body)))]
-          (t/is (= 403 (:status post-res)))))
-
-      (t/testing "Modify ppp with the same laatija-id"
-      (let [modified-ppp (assoc-in created-ppp [:passin-perustiedot :tayttaa-a0-vaatimukset] true)
-            new-ppp-body (j/write-value-as-string modified-ppp)
-            put-res (ts/handler (-> (mock/request :put (str "/api/private/perusparannuspassit/2026/" ppp-id))
+      (let [get-res (ts/handler (-> (mock/request :get (str "/api/private/perusparannuspassit/2026/" ppp-id))
                                     (mock/header "Accept" "application/json")
-                                    (mock/header "Content-Type" "application/json")
-                                    (mock/body new-ppp-body)
-                                    (laatija-test-data/with-suomifi-laatija)))]
-        (t/is (= 200 (:status put-res)))))
+                                    (laatija-test-data/with-suomifi-laatija)))
+            body-str (slurp (clojure.java.io/reader (:body get-res)))
+            full-ppp (j/read-value body-str object-mapper)
+            created-ppp (dissoc full-ppp :id :tila-id :laatija-id)]
+        (assert-status get-res 200)
 
-      (t/testing "Can't modify ppp with the different laatija-id"
-        (let [modified-ppp (assoc-in full-ppp [:passin-perustiedot :tayttaa-a0-vaatimukset] true)
-              new-ppp-body (j/write-value-as-string modified-ppp)
-              put-res (ts/handler (-> (mock/request :put (str "/api/private/perusparannuspassit/2026/" ppp-id))
-                                      (mock/header "Accept" "application/json")
-                                      (mock/header "Content-Type" "application/json")
-                                      (laatija-test-data/with-suomifi-laatija2)
-                                      (mock/body new-ppp-body)))]
-          (t/is (= 400 (:status put-res)))))
-
-      (t/testing "owner can delete own perusparannuspassi"
-        (let [delete-res (ts/handler (-> (mock/request :delete (str "/api/private/perusparannuspassit/2026/" ppp-id))
+        (t/testing "create perusparannuspassi to energiatodistus with different laatija-id"
+          (let [ppp (-> (ppp-test-data/generate-add 1))
+                ppp-body (-> ppp j/write-value-as-string)
+                post-res (ts/handler (-> (mock/request :post "/api/private/perusparannuspassit/2026")
                                          (mock/header "Accept" "application/json")
+                                         (mock/header "Content-Type" "application/json")
+                                         (laatija-test-data/with-suomifi-laatija2)
+                                         (mock/body ppp-body)))]
+            (assert-status post-res 403)))
+
+        (t/testing "Modify ppp with the same laatija-id"
+          (let [modified-ppp (assoc-in created-ppp [:passin-perustiedot :tayttaa-a0-vaatimukset] true)
+                new-ppp-body (j/write-value-as-string modified-ppp)
+                put-res (ts/handler (-> (mock/request :put (str "/api/private/perusparannuspassit/2026/" ppp-id))
+                                        (mock/header "Accept" "application/json")
+                                        (mock/header "Content-Type" "application/json")
+                                        (mock/body new-ppp-body)
+                                        (laatija-test-data/with-suomifi-laatija)))]
+            (assert-status put-res 200)))
+
+        (t/testing "Can't modify ppp with the different laatija-id"
+          (let [modified-ppp (assoc-in full-ppp [:passin-perustiedot :tayttaa-a0-vaatimukset] true)
+                new-ppp-body (j/write-value-as-string modified-ppp)
+                put-res (ts/handler (-> (mock/request :put (str "/api/private/perusparannuspassit/2026/" ppp-id))
+                                        (mock/header "Accept" "application/json")
+                                        (mock/header "Content-Type" "application/json")
+                                        (laatija-test-data/with-suomifi-laatija2)
+                                        (mock/body new-ppp-body)))]
+            (assert-status put-res 400)))
+
+        (t/testing "owner can delete own perusparannuspassi"
+          (let [delete-res (ts/handler (-> (mock/request :delete (str "/api/private/perusparannuspassit/2026/" ppp-id))
+                                           (mock/header "Accept" "application/json")
+                                           (laatija-test-data/with-suomifi-laatija)))]
+            (assert-status delete-res 200)))
+
+        (t/testing "can't create perusparannuspassi to energiatodistus without ppp-pätevyys"
+          (laatija-test-data/insert-suomifi-laatija!
+            (-> (laatija-test-data/generate-adds 1) first (merge {:patevyystaso 2})))
+          (let [ppp (-> (ppp-test-data/generate-add 1))
+                ppp-body (-> ppp j/write-value-as-string)
+                post-res (ts/handler (-> (mock/request :post "/api/private/perusparannuspassit/2026")
+                                         (mock/header "Accept" "application/json")
+                                         (mock/header "Content-Type" "application/json")
+                                         (mock/body ppp-body)
                                          (laatija-test-data/with-suomifi-laatija)))]
-          (t/is (= 200 (:status delete-res)))))
+            (assert-status post-res 403)))
 
-      (t/testing "can't create perusparannuspassi to energiatodistus without ppp-pätevyys"
-        (laatija-test-data/insert-suomifi-laatija!
-          (-> (laatija-test-data/generate-adds 1) first (merge {:patevyystaso 2})))
-        (let [ppp (-> (ppp-test-data/generate-add 1))
-              ppp-body (-> ppp j/write-value-as-string)
-              post-res (ts/handler (-> (mock/request :post "/api/private/perusparannuspassit/2026")
-                                       (mock/header "Accept" "application/json")
-                                       (mock/header "Content-Type" "application/json")
-                                       (mock/body ppp-body)
-                                       (laatija-test-data/with-suomifi-laatija)))]
-          (t/is (= 403 (:status post-res)))))
+        (t/testing "can't delete other laatija's perusparannuspassi"
+          (let [delete-res (ts/handler (-> (mock/request :delete (str "/api/private/perusparannuspassit/2026/" ppp-id))
+                                           (mock/header "Accept" "application/json")
+                                           (laatija-test-data/with-suomifi-laatija2)))]
+            (assert-status delete-res 500)))
 
-      (t/testing "can't delete other laatija's perusparannuspassi"
-        (let [delete-res (ts/handler (-> (mock/request :delete (str "/api/private/perusparannuspassit/2026/" ppp-id))
-                                         (mock/header "Accept" "application/json")
-                                         (laatija-test-data/with-suomifi-laatija2)))]
-          (t/is (= 500 (:status delete-res)))))
-
-      (t/testing "Get requires authentication"
-        (let [unauth-get-res (ts/handler (-> (mock/request :get (str "/api/private/perusparannuspassit/2026/" ppp-id))
-                                   (mock/header "Accept" "application/json")))]
-          (t/is (= 403 (:status unauth-get-res)))))))))
+        (t/testing "Get requires authentication"
+          (let [unauth-get-res (ts/handler (-> (mock/request :get (str "/api/private/perusparannuspassit/2026/" ppp-id))
+                                               (mock/header "Accept" "application/json")))]
+            (assert-status unauth-get-res 403)))))))
 
 (t/deftest ppp-numeric-validation-test
   (t/testing "PPP numeric validation for kaukolampo-hinta"
@@ -139,7 +139,7 @@
                                    (mock/header "Content-Type" "application/json")
                                    (mock/body et-body)
                                    (laatija-test-data/with-suomifi-laatija)))]
-      (t/is (= 201 (:status post-res)) "Expected status 201 from ET creation")
+      (assert-status post-res 201 "Expected status 201 from ET creation")
       (let [et-id (-> post-res :body (j/read-value object-mapper) :id)]
 
         (t/testing "PPP with kaukolampo-hinta at 3 cents should succeed with warnings (outside warning range 5-20)"
@@ -183,7 +183,7 @@
                                    (mock/header "Content-Type" "application/json")
                                    (mock/body et-body)
                                    (laatija-test-data/with-suomifi-laatija)))]
-      (t/is (= 201 (:status post-res)) "Expected status 201 from ET creation")
+      (assert-status post-res 201 "Expected status 201 from ET creation")
       (let [et-id (-> post-res :body (j/read-value object-mapper) :id)]
 
         (t/testing "PPP with vaihe uusiutuvan-energian-hyodynnetty-osuus above 100 should fail with 400"
