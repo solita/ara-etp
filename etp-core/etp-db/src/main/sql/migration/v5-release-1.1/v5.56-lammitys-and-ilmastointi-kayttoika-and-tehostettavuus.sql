c81b9ee9d399d026f885f5e6b37a4421437a663b
Juho Haapakoski <juho.haapakoski@solita.fi>
2025-12-31 11:54:35 +0200

    AE-2630: Add columns to energiatodistus for lammitys and iv_ilmastointi to track 2026 version specific things
    Namely kayttoaikaa_jaljella_arvio_vuosina and asetukset_tehostettavissa.

---
+++ etp-core/etp-db/src/main/sql/migration/v5-release-1.1/v5.56-lammitys-and-ilmastointi-kayttoika-and-tehostettavuus.sql
@@ -0,0 +1,5 @@
+alter table energiatodistus
+    add column h$lammitys_kayttoikaa_jaljella_arvio_vuosina int,
+    add column h$lammitys_asetukset_tehostettavissa boolean,
+    add column h$iv_ilmastointi_kayttoikaa_jaljella_arvio_vuosina int,
+    add column h$iv_ilmastointi_asetukset_tehostettavissa boolean;
