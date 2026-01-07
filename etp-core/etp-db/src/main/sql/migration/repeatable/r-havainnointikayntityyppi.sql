3a1b285ca1ad5e20164303dbc0acaa112e96182e
Juho Haapakoski <juho.haapakoski@solita.fi>
2026-01-07 10:44:35 +0200

    AE-2630: Make havainnointikayntityyppi id start from 0
    Ordinal starts from 1.

--- etp-core/etp-db/src/main/sql/migration/repeatable/r-havainnointikayntityyppi.sql
+++ etp-core/etp-db/src/main/sql/migration/repeatable/r-havainnointikayntityyppi.sql
@@ -1,7 +1,7 @@
 insert into havainnointikayntityyppi(id, label_fi, label_sv, ordinal)
 values
-(1, 'Paikan päällä käynti', 'Paikan päällä käynti (sv)', 1),
-(2, 'Virtuaalikäynti', 'Virtuaalikäynti (sv)', 2)
+(0, 'Paikan päällä käynti', 'Paikan päällä käynti (sv)', 1),
+(1, 'Virtuaalikäynti', 'Virtuaalikäynti (sv)', 2)
 on conflict (id) do update set
   label_fi = excluded.label_fi,
   label_sv = excluded.label_sv,
