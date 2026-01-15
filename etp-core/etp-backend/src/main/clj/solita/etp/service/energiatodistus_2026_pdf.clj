7365d45bf17f1f8eb50008b7d03a6b9e4b8e0e83
etahto <elias.tahto@solita.fi>
2026-01-15 12:53:23 +0200

    AE-2560: Refactor PDF generation styles and structure for Energiatodistus 2026, enhancing layout and footer positioning

--- etp-core/etp-backend/src/main/clj/solita/etp/service/energiatodistus_2026_pdf.clj
+++ etp-core/etp-backend/src/main/clj/solita/etp/service/energiatodistus_2026_pdf.clj
@@ -15,17 +15,7 @@
   (str
    "@page {
       size: A4;
-      margin: 20mm;
-      @bottom-center {
-        content: counter(page) \" / \" counter(pages);
-        font-family: roboto, sans-serif;
-        font-size: 10pt;
-      }
-      @bottom-right {
-        content: string(id-string);
-        font-family: roboto, sans-serif;
-        font-size: 10pt;
-      }
+      margin: 0;
     }
 
     * {
@@ -39,41 +29,76 @@
       font-size: 11pt;
     }
 
+    .page {
+      page-break-after: always;
+      position: relative;
+      width: 210mm;
+      min-height: 297mm;
+      padding: 0;
+    }
+
+    .page:last-child {
+      page-break-after: auto;
+    }
+
+    .page-content {
+      padding: 20mm;
+    }
+
+    .page-footer {
+       position: absolute;
+       bottom: 1cm;
+       left: 0;
+       right: 0;
+       text-align: center;
+       font-size: 10pt;
+       color: #666;
+       font-family: roboto, sans-serif;
+    }
+
     h1 {
       font-size: 24pt;
       margin-bottom: 20px;
-    }
+    }"))
 
-    .id-string {
-      string-set: id-string content();
-      display: none;
-    }
-   "))
+(defn- page-footer [id page-num]
+  [:div {:class "page-footer"}
+   (str "Todistustunnus: " id " | " page-num)])
+
+(defn- render-page [page-data page-num id]
+  (let [{:keys [title content]} page-data]
+    [:div {:class "page"}
+     [:div {:class "page-content"}
+      (when title [:h1 title])
+      content]
+     (page-footer id page-num)]))
 
 (defn generate-document-html
   "Generate complete HTML document for Energiatodistus 2026 PDF."
-  [content-hiccup id]
-  (hiccup/html
-   [:html
-    [:head
-     [:meta {:charset "UTF-8"}]
-     [:title "Energiatodistus"]
-     [:style (styles)]]
-    [:body
-     [:div.id-string id]
-     content-hiccup]]))
+  [pages id]
+  (let [pages-html (map-indexed
+                     (fn [idx page-data]
+                       (render-page page-data (inc idx) id))
+                     pages)]
+    (hiccup/html
+     [:html
+      [:head
+       [:meta {:charset "UTF-8"}]
+       [:title "Energiatodistus"]
+       [:style (styles)]]
+      [:body
+       pages-html]])))
 
 (defn- generate-energiatodistus-ohtp-pdf
   "Use OpenHTMLToPDF to generate PDF, return as a byte array"
   [energiatodistus kieli]
   (let [output-stream (ByteArrayOutputStream.)
         ;; Placeholder content for now
-        content [:div
-                 [:h1 "Energiatodistus 2026"]
-                 [:p "Tämä on energiatodistuksen 2026 placeholder-sisältö."]
-                 [:p (str "Todistuksen ID: " (:id energiatodistus))]
-                 [:p (str "Kieli: " kieli)]]]
-    (-> (generate-document-html content (:id energiatodistus))
+        pages [{:title "Energiatodistus 2026"
+                :content [:div
+                          [:p "Tämä on energiatodistuksen 2026 placeholder-sisältö."]
+                          [:p (str "Kieli: " kieli)]]}]]
+    (-> (generate-document-html pages (:id energiatodistus))
         (pdf-service/html->pdf output-stream))
     (-> output-stream .toByteArray)))
 
