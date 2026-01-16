ec42c967893cc8ef2b785974ef9bebb9df66226c
Joonas Sarajärvi <joonas.sarajarvi@solita.fi>
2026-01-16 16:57:26 +0200

    AE-2722 Adjust error/warn limits energy carrier price

--- etp-core/etp-db/src/main/sql/migration/repeatable/r-validation-limits-ppp.sql
+++ etp-core/etp-db/src/main/sql/migration/repeatable/r-validation-limits-ppp.sql
@@ -2,11 +2,11 @@ insert into ppp_validation_numeric_column
   (versio, column_name, warning$min, warning$max, error$min, error$max)
 values
 -- Tulokset / Energian hinnat (prices in cents/kWh or similar)
-(2026, 't$kaukolampo_hinta', 5, 20, 2, 100),
-(2026, 't$sahko_hinta', 5, 100, 2, 100),
-(2026, 't$uusiutuvat_pat_hinta', 0, 100, 0, 999),
-(2026, 't$fossiiliset_pat_hinta', 0, 100, 0, 999),
-(2026, 't$kaukojaahdytys_hinta', 0, 100, 0, 999),
+(2026, 't$kaukolampo_hinta', 0, 20, 0, 200),
+(2026, 't$sahko_hinta', 0, 20, 0, 200),
+(2026, 't$uusiutuvat_pat_hinta', 0, 20, 0, 200),
+(2026, 't$fossiiliset_pat_hinta', 0, 20, 0, 200),
+(2026, 't$kaukojaahdytys_hinta', 0, 20, 0, 200),
 -- Rakennuksen perustiedot / Ehdotetut tasot (U-arvot)
 (2026, 'rpt$ulkoseinat_ehdotettu_taso', 0.08, 0.81, 0.05, 2),
 (2026, 'rpt$ylapohja_ehdotettu_taso', 0.05, 0.47, 0.03, 2),
