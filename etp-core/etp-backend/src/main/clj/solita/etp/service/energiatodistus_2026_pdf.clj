9292a3d2c4c9ed4700bf568457bb095e70aa8777
Areta Santos <areta.santos@solita.fi>
2026-01-19 08:55:47 +0200

    AE-2701: add styles and pdf generating similar to ppp pdf

--- etp-core/etp-backend/src/main/clj/solita/etp/service/energiatodistus_2026_pdf.clj
+++ etp-core/etp-backend/src/main/clj/solita/etp/service/energiatodistus_2026_pdf.clj
@@ -1,12 +1,14 @@
 (ns solita.etp.service.energiatodistus-2026-pdf
   (:require [hiccup.core :as hiccup]
-            [solita.etp.service.pdf :as pdf-service]
-            [solita.etp.service.watermark-pdf :as watermark-pdf]
             [solita.etp.config :as config]
+            [solita.etp.service.pdf :as pdf-service]
             [solita.etp.service.localization :as loc]
+            [solita.etp.service.watermark-pdf :as watermark-pdf]
             [solita.etp.service.energiatodistus-pdf.etusivu-yleistiedot :as et-etusivu-yleistiedot]
-            [solita.etp.service.energiatodistus-pdf.laskennallinen-ostoenergia :as et-laskennallinen-ostoenergia])
-  (:import [java.io ByteArrayOutputStream]))
+            [solita.etp.service.energiatodistus-pdf.laskennallinen-ostoenergia :as et-laskennallinen-ostoenergia]
+            [solita.etp.service.energiatodistus :as energiatodistus-service]
+            [solita.etp.service.kayttotarkoitus :as kayttotarkoitus-service])
+  (:import [java.io ByteArrayInputStream ByteArrayOutputStream]))
 
 (def draft-watermark-texts {"fi" "LUONNOS"
                             "sv" "UTKAST"})
@@ -77,6 +79,117 @@
     .id-string {
       string-set: id-string content();
       display: none;
+    }
+
+    dl.et-etusivu-yleistiedot {
+      display: table;
+      width: 90%;
+      background-color: white;
+      border-collapse: collapse;
+      -fs-border-rendering: no-bevel;
+      margin: 10px 15px;
+    }
+
+    dl.et-etusivu-yleistiedot div {
+      display: table-row;
+    }
+
+    dl.et-etusivu-yleistiedot dt,
+    dl.et-etusivu-yleistiedot dd {
+      display: table-cell;
+      fs-border-rendering: no-bevel;
+      padding-top: 3px;
+      padding-bottom: 3px;
+    }
+
+    dl.et-etusivu-yleistiedot dt {
+      color: #23323e;
+      font-weight: bold;
+      white-space: nowrap;
+      width: 45%;
+      padding-right: 30px;
+    }
+
+    dl.et-etusivu-yleistiedot dd {
+      background-color: white;
+    }
+
+    dl.et-etusivu-yleistiedot div:last-child dd {
+      font-weight: bold;
+      color: #23323e;
+      white-space: nowrap;
+    }
+
+    table.ostoenergia {
+      width: 95%;
+      margin: 0 auto;
+      border-collapse: collapse;
+      margin: 10px 15px;
+    }
+
+    table.ostoenergia th,
+    table.ostoenergia td {
+      border: 1px solid #bdc6cc;
+      padding: 2px 2px;
+      font-weight: normal;
+      font-size: 13px;
+      text-align: center;
+      color: #23323e;
+    }
+
+    table.ostoenergia th:first-child,
+    table.ostoenergia td:first-child {
+      border-left: none;
+      border-right: none;
+      font-weight: bold;
+      text-align: left;
+      width: 25%;
+    }
+
+    table.ostoenergia th.oe-otsikko {
+      font-weight: bold;
+    }
+
+    table.ostoenergia th:nth-child(2),
+    table.ostoenergia td:nth-child(2) {
+      border-left: none;
+    }
+
+    table.ostoenergia th:last-child,
+    table.ostoenergia td:last-child {
+      border-right: none;
+    }
+
+    table.ostoenergia th.empty,
+    table.ostoenergia th.oe-otsikko {
+      border: none;
+      background: none;
+    }
+
+    dl.et-etusivu-yleistiedot-ostoenergia {
+      display: table;
+      width: 90%;
+      background-color: white;
+      border-collapse: collapse;
+      -fs-border-rendering: no-bevel;
+      margin: 10px 10px;
+    }
+
+    dl.et-etusivu-yleistiedot-ostoenergia div {
+      display: table-row;
+    }
+
+    dl.et-etusivu-yleistiedot-ostoenergia dt,
+    dl.et-etusivu-yleistiedot-ostoenergia dd {
+      display: table-cell;
+      padding: 5px 5px;
+    }
+
+    dl.et-etusivu-yleistiedot-ostoenergia dt {
+      color: #23323e;
+      font-weight: bold;
+      white-space: nowrap;
+      width: 1px;
     }"))
 
 (defn- page-header [title]
@@ -84,8 +197,7 @@
    [:h1 {:class "page-title"} title]])
 
 (defn- page-footer [et-tunnus page-num total-pages]
-  [:div {:class "page-footer"}
-   (str "Energiatodistuksen tunnus: " et-tunnus " | " page-num "/" total-pages)])
+  [:div {:class "page-footer"}])
 
 (defn- render-page [page-data page-num total-pages et-tunnus]
   (let [{:keys [title content header]} page-data]
@@ -112,30 +224,61 @@
     [:body
      pages-html]])))
 
-(defn- generate-energiatodistus-ohtp-pdf
+
+(defn generate-energiatodistus-html
   "Use OpenHTMLToPDF to generate PDF, return as a byte array"
-  [energiatodistus kieli]
-  (let [_ (println "kieli:" kieli)
-        output-stream (ByteArrayOutputStream.)
-        l (kieli loc/et-pdf-localization kieli)
-
+  [{:keys [energiatodistus kieli] :as params}]
+  (let [l (kieli loc/ppp-pdf-localization)
         pages [{:title (l :energiatodistus)
-                  :content
-                  [:div
-                   ;(et-etusivu-yleistiedot/et-etusivu-yleistiedot [energiatodistus kieli alakayttotarkoitukset])
-                   ;(et-laskennallinen-ostoenergia/ostoenergia energiatodistus kieli)
-                   ;(et-laskennallinen-ostoenergia/ostoenergia-tiedot [energiatodistus kieli])
-                   ]}]]
+                :content
+                [:div
+                 (et-etusivu-yleistiedot/et-etusivu-yleistiedot params)
+                 (et-laskennallinen-ostoenergia/ostoenergia params)
+                 (et-laskennallinen-ostoenergia/ostoenergia-tiedot params)]}]]
 
-    (-> (generate-document-html pages (:id energiatodistus))
+    (generate-document-html pages (:id energiatodistus))))
+
+(defn- generate-energiatodistus-ohtp-pdf
+  "Use OpenHTMLToPDF to generate a energiatodistus PDF, return as a byte array"
+  [params]
+  (let [output-stream (ByteArrayOutputStream.)]
+    (-> (generate-energiatodistus-html params)
         (pdf-service/html->pdf output-stream))
     (-> output-stream .toByteArray)))
 
-(defn generate-pdf [energiatodistus kieli draft?]
-  (let [pdf-bytes (generate-energiatodistus-ohtp-pdf energiatodistus kieli)]
-    (cond
-      draft?
-      (watermark-pdf/apply-watermark-to-bytes pdf-bytes (get draft-watermark-texts kieli "LUONNOS"))
-      (contains? #{"local-dev" "dev" "test"} config/environment-alias)
-      (watermark-pdf/apply-watermark-to-bytes pdf-bytes (get test-watermark-texts kieli "TESTI"))
-      :else pdf-bytes)))
+(defn generate-energiatodistus-pdf
+  "Generate a energiatodistus PDF and return it as a byte array."
+  [energiatodistus alakayttotarkoitukset kieli draft?]
+  (let [kieli-keyword (keyword kieli)
+        pdf-bytes
+
+        ;; Generate the PDF to byte array
+        (generate-energiatodistus-ohtp-pdf
+          {:energiatodistus       energiatodistus
+           :alakayttotarkoitukset alakayttotarkoitukset
+           :kieli                 kieli-keyword})
+        watermark-text (cond
+                         draft? (draft-watermark-texts kieli)
+                         (contains? #{"local-dev" "dev" "test"} config/environment-alias) (test-watermark-texts kieli)
+                         :else nil)]
+
+    (if (some? watermark-text)
+      (watermark-pdf/apply-watermark-to-bytes pdf-bytes watermark-text)
+      pdf-bytes)))
+
+(defn- generate-pdf-as-input-stream
+  "Generate a energiatodistus PDF and return it as an InputStream.
+   Applies watermarks via post-processing with PDFBox."
+  [energiatodistus alakayttotarkoitukset kieli draft?]
+  (let [pdf-bytes (generate-energiatodistus-pdf energiatodistus alakayttotarkoitukset kieli draft?)]
+    (ByteArrayInputStream. pdf-bytes)))
+
+
+(defn find-energiatodistus2026-pdf
+  [db whoami et-id kieli]
+  (when-let [energiatodistus (energiatodistus-service/find-energiatodistus db whoami et-id)]
+    (let [versio (:versio energiatodistus)
+          alakayttotarkoitukset (kayttotarkoitus-service/find-alakayttotarkoitukset db versio)
+          draft? true]
+      ;; Always show draft watermark for now (no signing yet)
+      (generate-pdf-as-input-stream energiatodistus alakayttotarkoitukset kieli draft?))))
