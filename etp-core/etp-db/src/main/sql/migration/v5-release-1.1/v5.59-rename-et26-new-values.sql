fce7e6513d1da6ad0a9b896ce398e6d4113ae637
Juho Haapakoski <juho.haapakoski@solita.fi>
2026-01-27 17:07:56 +0200

    AE-2551: Rename more et26 columns to fit the frontend better

--- etp-core/etp-db/src/main/sql/migration/v5-release-1.1/v5.59-rename-et26-new-values.sql
+++ etp-core/etp-db/src/main/sql/migration/v5-release-1.1/v5.59-rename-et26-new-values.sql
@@ -6,3 +6,16 @@ alter table energiatodistus
     rename column h$iv_ilmastointi_kayttoikaa_jaljella_arvio_vuosina to h$iv_ilmastointi$kayttoikaa_jaljella_arvio_vuosina;
 alter table energiatodistus
     rename column h$iv_ilmastointi_asetukset_tehostettavissa to h$iv_ilmastointi$asetukset_tehostettavissa;
+
+alter table energiatodistus
+    rename column t$uusiutuvat_omavaraisenergiat$aurinkolampo_kokonaistuotanto to t$uusiutuvat_omavaraisenergiat$kokonaistuotanto$aurinkolampo;
+alter table energiatodistus
+    rename column t$uusiutuvat_omavaraisenergiat$aurinkosahko_kokonaistuotanto to t$uusiutuvat_omavaraisenergiat$kokonaistuotanto$aurinkosahko;
+alter table energiatodistus
+    rename column t$uusiutuvat_omavaraisenergiat$tuulisahko_kokonaistuotanto to t$uusiutuvat_omavaraisenergiat$kokonaistuotanto$tuulisahko;
+alter table energiatodistus
+    rename column t$uusiutuvat_omavaraisenergiat$lampopumppu_kokonaistuotanto to t$uusiutuvat_omavaraisenergiat$kokonaistuotanto$lampopumppu;
+alter table energiatodistus
+    rename column t$uusiutuvat_omavaraisenergiat$muulampo_kokonaistuotanto to t$uusiutuvat_omavaraisenergiat$kokonaistuotanto$muulampo;
+alter table energiatodistus
+    rename column t$uusiutuvat_omavaraisenergiat$muusahko_kokonaistuotanto to t$uusiutuvat_omavaraisenergiat$kokonaistuotanto$muusahko;
