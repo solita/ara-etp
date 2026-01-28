94c14b3f091ae412cb9fa02807dbbd02299fdff4
Areta Santos <areta.santos@solita.fi>
2026-01-28 07:23:04 +0200

    AE-2702: add e-luku and format texts in the graphics to be more clear

--- etp-core/etp-backend/src/main/clj/solita/etp/service/energiatodistus_pdf/etusivu_grafiikka.clj
+++ etp-core/etp-backend/src/main/clj/solita/etp/service/energiatodistus_pdf/etusivu_grafiikka.clj
@@ -3,12 +3,13 @@
     [solita.etp.service.localization :as loc]))
 
 (defn arrow
-  [{:keys [x y height body-width tip-width color text text-color]}]
+  [{:keys [x y height body-width tip-width color letter numbers text-color]}]
   (let [h height
         bw body-width
         tw tip-width
         cy (+ y (/ h 2))
-        text-padding 5]
+        letter-padding 5
+        numbers-x 25]
     [:g
      [:polygon
       {:points (str
@@ -20,7 +21,7 @@
        :fill color}]
 
      [:text
-      {:x (+ x text-padding)
+      {:x (+ x letter-padding)
        :y (+ y (/ h 2))
        :dy "0.35em"
        :text-anchor "start"
@@ -28,18 +29,29 @@
        :font-family "roboto, sans-serif"
        :font-weight "bold"
        :fill text-color}
-      text]]))
+      letter]
+
+     [:text
+      {:x (+ x numbers-x)
+       :y (+ y (/ h 2))
+       :dy "0.35em"
+       :text-anchor "start"
+       :font-size 11
+       :font-family "roboto, sans-serif"
+       :font-weight "bold"
+       :fill text-color}
+      numbers]]))
 
 (def arrows
-  [{:text "A+ &lt; A0-20%" :color "#02af50" :text-color "#ffffff" :luokka "A+"}
-   {:text "A0 &lt; 40" :color "#39d611" :luokka "A0"}
-   {:text "A &lt; 80" :color "#3bee07" :luokka "A"}
-   {:text "B 80-100" :color "#93d14f" :luokka "B"}
-   {:text "C 120-150" :color "#b5f40b" :luokka "C"}
-   {:text "D 150-220" :color "#fff000" :luokka "D"}
-   {:text "E 220-300" :color "#ffc300" :luokka "E"}
-   {:text "F 300-400" :color "#ec670b" :text-color "#ffffff" :luokka "F"}
-   {:text "G > 400" :color "#d82e10" :text-color "#ffffff" :luokka "G"}])
+  [{:letter "A+" :numbers "&lt; A0-20%" :color "#02af50" :text-color "#ffffff" :luokka "A+"}
+   {:letter "A0" :numbers "&lt; 40" :color "#39d611" :luokka "A0"}
+   {:letter "A" :numbers "&lt; 80" :color "#3bee07" :luokka "A"}
+   {:letter "B" :numbers "80-100" :color "#93d14f" :luokka "B"}
+   {:letter "C" :numbers "120-150" :color "#b5f40b" :luokka "C"}
+   {:letter "D" :numbers "150-220" :color "#fff000" :luokka "D"}
+   {:letter "E" :numbers "220-300" :color "#ffc300" :luokka "E"}
+   {:letter "F" :numbers "300-400" :color "#ec670b" :text-color "#ffffff" :luokka "F"}
+   {:letter "G" :numbers "> 400" :color "#d82e10" :text-color "#ffffff" :luokka "G"}])
 
 (def arrow-height 20)
 (def arrow-spacing 4)
@@ -52,17 +64,19 @@
 (def svg-width (+ indicator-line-length e-luokka-indicator-margin))
 
 (defn e-luokka-indicator
-  [{:keys [e-luokka]}]
+  [{:keys [e-luokka e-luku]}]
   (let [row-height (+ arrow-height arrow-spacing)
         arrow-index (first (keep-indexed (fn [i a] (when (= (:luokka a) e-luokka) i)) arrows))
         arrow-y (when arrow-index (* arrow-index row-height))
-        indicator-width 90
+        indicator-width 140
         indicator-tip-width 15
         x-end (+ indicator-line-length e-luokka-indicator-margin)
         x-start (- x-end indicator-width)
         x-tip (- x-start indicator-tip-width)
         h arrow-height
-        cy (when arrow-y (+ arrow-y (/ h 2)))]
+        cy (when arrow-y (+ arrow-y (/ h 2)))
+        luokka-x (+ x-tip 15)
+        luku-x (+ x-tip 35)]
     (when arrow-index
       [:g
        [:polygon
@@ -74,7 +88,7 @@
                    x-end "," (+ arrow-y h))
          :fill "#000000"}]
        [:text
-        {:x (+ x-tip 15)
+        {:x luokka-x
          :y cy
          :dy "0.35em"
          :text-anchor "start"
@@ -82,7 +96,17 @@
          :font-family "roboto, sans-serif"
          :font-weight "bold"
          :fill "#ffffff"}
-        e-luokka]])))
+        e-luokka]
+       [:text
+        {:x luku-x
+         :y cy
+         :dy "0.35em"
+         :text-anchor "start"
+         :font-size 11
+         :font-family "roboto, sans-serif"
+         :font-weight "bold"
+         :fill "#ffffff"}
+        e-luku " kWhE/m2/vuosi"]])))
 
 (defn indicator-line
   [{:keys [arrow-index label]}]
@@ -106,12 +130,12 @@
        :fill "#000000"}
       label]]))
 
-(defn stacked-arrows [{:keys [kieli e-luokka]}]
+(defn stacked-arrows [{:keys [kieli e-luokka e-luku]}]
   (let [l (kieli loc/et-pdf-localization)
         num-arrows (count arrows)
         row-height (+ arrow-height arrow-spacing)
         total-height (* row-height num-arrows)
-        arrow-elements (for [[i {:keys [text color text-color]}] (map-indexed vector arrows)]
+        arrow-elements (for [[i {:keys [letter numbers color text-color]}] (map-indexed vector arrows)]
                          (let [body-width (+ min-body-width (* width-increment i))
                                tip-w (+ tip-width (* tip-width-increment i))]
                            (arrow
@@ -121,7 +145,8 @@
                               :body-width body-width
                               :tip-width tip-w
                               :color color
-                              :text text
+                              :letter letter
+                              :numbers numbers
                               :text-color (or text-color "#000000")})))]
     (into
       [:svg
@@ -133,11 +158,12 @@
       (concat
         arrow-elements
         [(indicator-line {:arrow-index 1 :label (l :paastoton-rakennus)})
-         (e-luokka-indicator {:e-luokka e-luokka})]))))
+         (e-luokka-indicator {:e-luokka e-luokka :e-luku e-luku})]))))
 
 (defn et-etusivu-grafiikka [{:keys [kieli energiatodistus]}]
   (let [l (kieli loc/et-pdf-localization)
-        e-luokka (get-in energiatodistus [:tulokset :e-luokka])]
+        e-luokka (get-in energiatodistus [:tulokset :e-luokka])
+        e-luku (get-in energiatodistus [:tulokset :e-luku])]
     [:div {:class "etusivu-grafiikka"}
      [:div {:class "etusivu-grafiikka-otsikko"} (l :energiatehokkuusluokka-otsikko)]
-     (stacked-arrows {:kieli kieli :e-luokka e-luokka})]))
+     (stacked-arrows {:kieli kieli :e-luokka e-luokka :e-luku e-luku})]))
