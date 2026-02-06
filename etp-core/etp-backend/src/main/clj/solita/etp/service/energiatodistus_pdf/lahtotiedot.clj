9e22f76d028cf2a958407143185f2e575e0ac7dd
etahto <elias.tahto@solita.fi>
2026-02-06 10:23:04 +0200

    AE-2705: Refactor 'lahtotiedot' page to extract data from energiatodistus, replacing mock data with dynamic content for Energiatodistus 2026 PDF

--- etp-core/etp-backend/src/main/clj/solita/etp/service/energiatodistus_pdf/lahtotiedot.clj
+++ etp-core/etp-backend/src/main/clj/solita/etp/service/energiatodistus_pdf/lahtotiedot.clj
@@ -1,35 +1,128 @@
 (ns solita.etp.service.energiatodistus-pdf.lahtotiedot
-  "E-luvun laskennan lähtötiedot page for Energiatodistus 2026 PDF")
+  "E-luvun laskennan lähtötiedot page for Energiatodistus 2026 PDF"
+  (:require [solita.common.formats :as formats]))
 
-;; Mock data - to be replaced with real values later
-(def mock-data
-  {:ilmanvuotoluku "12345"
-   :rakennusvaippa [{:nimi "Ulkoseinät" :a "123" :u "123" :osuus "12"}
-                    {:nimi "Yläpohja" :a "123" :u "123" :osuus "12"}
-                    {:nimi "Alapohja" :a "123" :u "123" :osuus "12"}
-                    {:nimi "Ikkunat" :a "123" :u "123" :osuus "12"}
-                    {:nimi "Ulko-ovet" :a "123" :u "123" :osuus "12"}
-                    {:nimi "Kylmäsillat" :a "" :u "" :osuus "12"}]
-   :ilmanvaihto {:kuvaus "Koneellinen tulo ja poisto"
-                 :jarjestelmat [{:ilmavirta "0,04" :sfp "1" :lto "78"}]}
-   :lammitys {:kuvaus "Kaukolämpö"
-              :lammonjako "Vesikiertoinen lattialämmitys"
-              :jarjestelmat [{:nimi "Tilojen ja ilmanvaihdon lämmitys"
-                              :jaon-hyotysuhde "1"
-                              :tuoton-hyotysuhde "1"
-                              :lampokerroin "1/1"
-                              :apulaitteet "1"}
-                             {:nimi "Lämpimän käyttöveden valmistus"
-                              :jaon-hyotysuhde "1"
-                              :tuoton-hyotysuhde "1"
-                              :lampokerroin "1/1"
-                              :apulaitteet "1"}]
-              :lisalaitteet [{:nimi "Varaava tulisija" :maara "1" :tuotto "1"}
-                             {:nimi "Ilmalämpöpumppu" :maara "1" :tuotto "1"}]}
-   :jaahdytys {:kylmakerroin "XX"}
-   :lampokuormat [{:nimi "Henkilöt" :kayttoaste "1" :lampokuorma "1"}
-                  {:nimi "Kuluttajalaitteet" :kayttoaste "1" :lampokuorma "1"}
-                  {:nimi "Valaistus" :kayttoaste "1" :lampokuorma "1"}]})
+(defn- fmt
+  "Format number with specified decimal places. Returns empty string for nil."
+  ([value] (fmt value 2))
+  ([value decimals] (or (formats/format-number value decimals false) "")))
+
+(defn- fmt-pct
+  "Format a ratio (0-1) as percentage without decimals."
+  [value]
+  (or (formats/format-number value 0 true) ""))
+
+(defn- extract-rakennusvaippa-data
+  "Extract rakennusvaippa table data from energiatodistus."
+  [energiatodistus]
+  (let [vaippa (get-in energiatodistus [:lahtotiedot :rakennusvaippa])]
+    [{:nimi "Ulkoseinät"
+      :a (fmt (get-in vaippa [:ulkoseinat :ala]))
+      :u (fmt (get-in vaippa [:ulkoseinat :U]))
+      :osuus (fmt-pct (get-in vaippa [:ulkoseinat :osuus-lampohaviosta]))}
+     {:nimi "Yläpohja"
+      :a (fmt (get-in vaippa [:ylapohja :ala]))
+      :u (fmt (get-in vaippa [:ylapohja :U]))
+      :osuus (fmt-pct (get-in vaippa [:ylapohja :osuus-lampohaviosta]))}
+     {:nimi "Alapohja"
+      :a (fmt (get-in vaippa [:alapohja :ala]))
+      :u (fmt (get-in vaippa [:alapohja :U]))
+      :osuus (fmt-pct (get-in vaippa [:alapohja :osuus-lampohaviosta]))}
+     {:nimi "Ikkunat"
+      :a (fmt (get-in vaippa [:ikkunat :ala]))
+      :u (fmt (get-in vaippa [:ikkunat :U]))
+      :osuus (fmt-pct (get-in vaippa [:ikkunat :osuus-lampohaviosta]))}
+     {:nimi "Ulko-ovet"
+      :a (fmt (get-in vaippa [:ulkoovet :ala]))
+      :u (fmt (get-in vaippa [:ulkoovet :U]))
+      :osuus (fmt-pct (get-in vaippa [:ulkoovet :osuus-lampohaviosta]))}
+     {:nimi "Kylmäsillat"
+      :a ""
+      :u ""
+      :osuus (fmt-pct (get-in vaippa [:kylmasillat-osuus-lampohaviosta]))}]))
+
+(defn- extract-ilmanvaihto-data
+  "Extract ilmanvaihto data from energiatodistus."
+  [energiatodistus kieli]
+  (let [iv (get-in energiatodistus [:lahtotiedot :ilmanvaihto])
+        kuvaus-key (if (= kieli :sv) :label-sv :label-fi)
+        paaiv (:paaiv iv)]
+    {:kuvaus (get iv kuvaus-key)
+     :jarjestelmat [{:ilmavirta (fmt (:tulo paaiv))
+                     :sfp (fmt (:sfp paaiv))
+                     :lto (fmt-pct (:lto-vuosihyotysuhde iv))}]}))
+
+(defn- format-lampokerroin-tuotto-osuus
+  "Format lämpökerroin and tuotto-osuus as 'kerroin/osuus'."
+  [kerroin osuus]
+  (str (fmt kerroin 1)
+       "/"
+       (fmt-pct osuus)))
+
+(defn- extract-lammitys-data
+  "Extract lämmitys data from energiatodistus."
+  [energiatodistus kieli]
+  (let [lammitys (get-in energiatodistus [:lahtotiedot :lammitys])
+        kuvaus-key (if (= kieli :sv) :lammitysmuoto-label-sv :lammitysmuoto-label-fi)
+        lammonjako-key (if (= kieli :sv) :lammonjako-label-sv :lammonjako-label-fi)
+        tilat-ja-iv (:tilat-ja-iv lammitys)
+        lammin-kayttovesi (:lammin-kayttovesi lammitys)
+        takka (:takka lammitys)
+        ilmalampopumppu (:ilmalampopumppu lammitys)]
+    {:kuvaus (get lammitys kuvaus-key)
+     :lammonjako (get lammitys lammonjako-key)
+     :jarjestelmat [{:nimi "Tilojen ja ilmanvaihdon lämmitys"
+                     :jaon-hyotysuhde (fmt (:jaon-hyotysuhde tilat-ja-iv))
+                     :tuoton-hyotysuhde (fmt (:tuoton-hyotysuhde tilat-ja-iv))
+                     :lampokerroin (format-lampokerroin-tuotto-osuus
+                                    (:lampokerroin tilat-ja-iv)
+                                    (:lampopumppu-tuotto-osuus tilat-ja-iv))
+                     :apulaitteet (fmt (:apulaitteet tilat-ja-iv))}
+                    {:nimi "Lämpimän käyttöveden valmistus"
+                     :jaon-hyotysuhde (fmt (:jaon-hyotysuhde lammin-kayttovesi))
+                     :tuoton-hyotysuhde (fmt (:tuoton-hyotysuhde lammin-kayttovesi))
+                     :lampokerroin (format-lampokerroin-tuotto-osuus
+                                    (:lampokerroin lammin-kayttovesi)
+                                    (:lampopumppu-tuotto-osuus lammin-kayttovesi))
+                     :apulaitteet (fmt (:apulaitteet lammin-kayttovesi))}]
+     :lisalaitteet [{:nimi "Varaava tulisija"
+                     :maara (str (:maara takka))
+                     :tuotto (fmt (:tuotto takka) 0)}
+                    {:nimi "Ilmalämpöpumppu"
+                     :maara (str (:maara ilmalampopumppu))
+                     :tuotto (fmt (:tuotto ilmalampopumppu) 0)}]}))
+
+(defn- extract-jaahdytys-data
+  "Extract jäähdytys data from energiatodistus."
+  [energiatodistus]
+  {:kylmakerroin (fmt
+                   (get-in energiatodistus
+                           [:lahtotiedot :jaahdytysjarjestelma :jaahdytyskauden-painotettu-kylmakerroin]))})
+
+(defn- extract-lampokuormat-data
+  "Extract sisäiset lämpökuormat data from energiatodistus."
+  [energiatodistus]
+  (let [sis-kuorma (get-in energiatodistus [:lahtotiedot :sis-kuorma])]
+    [{:nimi "Henkilöt"
+      :kayttoaste (fmt (get-in sis-kuorma [:henkilot :kayttoaste]))
+      :lampokuorma (fmt (get-in sis-kuorma [:henkilot :lampokuorma]))}
+     {:nimi "Kuluttajalaitteet"
+      :kayttoaste (fmt (get-in sis-kuorma [:kuluttajalaitteet :kayttoaste]))
+      :lampokuorma (fmt (get-in sis-kuorma [:kuluttajalaitteet :lampokuorma]))}
+     {:nimi "Valaistus"
+      :kayttoaste (fmt (get-in sis-kuorma [:valaistus :kayttoaste]))
+      :lampokuorma (fmt (get-in sis-kuorma [:valaistus :lampokuorma]))}]))
+
+(defn- extract-lahtotiedot
+  "Extract all lähtötiedot from energiatodistus for PDF display."
+  [energiatodistus kieli]
+  {:ilmanvuotoluku (fmt
+                     (get-in energiatodistus [:lahtotiedot :rakennusvaippa :ilmanvuotoluku]))
+   :rakennusvaippa (extract-rakennusvaippa-data energiatodistus)
+   :ilmanvaihto (extract-ilmanvaihto-data energiatodistus kieli)
+   :lammitys (extract-lammitys-data energiatodistus kieli)
+   :jaahdytys (extract-jaahdytys-data energiatodistus)
+   :lampokuormat (extract-lampokuormat-data energiatodistus)})
 
 (defn- section-title [title]
   [:h3 {:class "lahtotiedot-section-title"} title])
@@ -148,13 +241,14 @@
 
 (defn lahtotiedot-page-content
   "Generate the content for the E-luvun laskennan lähtötiedot page.
-   Takes params map but currently uses mock data."
-  [_params]
-  [:div {:class "lahtotiedot-page"}
-   [:h2 {:class "lahtotiedot-page-title"} "E-LUVUN LASKENNAN LÄHTÖTIEDOT"]
-   [:div {:class "lahtotiedot-content"}
-    (rakennusvaippa-section mock-data)
-    (ilmanvaihto-section mock-data)
-    (lammitys-section mock-data)
-    (jaahdytys-section mock-data)
-    (lampokuormat-section mock-data)]])
+   Extracts data from the energiatodistus in params."
+  [{:keys [energiatodistus kieli]}]
+  (let [data (extract-lahtotiedot energiatodistus kieli)]
+    [:div {:class "lahtotiedot-page"}
+     [:h2 {:class "lahtotiedot-page-title"} "E-LUVUN LASKENNAN LÄHTÖTIEDOT"]
+     [:div {:class "lahtotiedot-content"}
+      (rakennusvaippa-section data)
+      (ilmanvaihto-section data)
+      (lammitys-section data)
+      (jaahdytys-section data)
+      (lampokuormat-section data)]]))
