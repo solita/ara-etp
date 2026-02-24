15eec5cfa94a3a6d6c800f461609fb0eba7c5cc6
solita-areta <areta.santos@solita.fi>
2026-02-24 14:29:37 +0200

    AE-2741: rollback

--- etp-core/etp-backend/src/main/clj/solita/etp/service/complete_energiatodistus.clj
+++ etp-core/etp-backend/src/main/clj/solita/etp/service/complete_energiatodistus.clj
@@ -107,7 +107,7 @@
   [energiatodistus {:keys [postinumerot kielisyydet laatimisvaiheet
                            kayttotarkoitukset alakayttotarkoitukset
                            ilmanvaihtotyypit lammitysmuodot lammonjaot
-                           havainnointikayntityypit ilmastoselvitys-laadintaperusteet]}]
+                           havainnointikayntityypit]}]
   (with-precision 20
     (let [{:keys [versio]} energiatodistus
           postinumero (find-by-id postinumerot (-> energiatodistus
@@ -165,11 +165,7 @@
                                      (find-by-id (alakayttotarkoitukset versio))
                                      :kayttotarkoitusluokka-id)
           paakayttotarkoitus (-> (get kayttotarkoitukset versio)
-                                 (find-by-id paakayttotarkoitus-id))
-          ilmastoselvitys-laadintaperuste-id (-> energiatodistus
-                                                 :ilmastoselvitys
-                                                 :ilmastoselvitys-laadintaperuste)
-          ilmastoselvitys-laadintaperuste (find-by-id ilmastoselvitys-laadintaperusteet ilmastoselvitys-laadintaperuste-id)]
+                                 (find-by-id paakayttotarkoitus-id))]
       (-> energiatodistus
           (assoc-in [:perustiedot :postitoimipaikka-fi] (:label-fi postinumero))
           (assoc-in [:perustiedot :postitoimipaikka-sv] (:label-sv postinumero))
@@ -184,8 +180,6 @@
           (assoc-in [:perustiedot :paakayttotarkoitus-id] paakayttotarkoitus-id)
           (assoc-in [:perustiedot :paakayttotarkoitus-fi] (:label-fi paakayttotarkoitus))
           (assoc-in [:perustiedot :paakayttotarkoitus-sv] (:label-sv paakayttotarkoitus))
-          (assoc-in [:ilmastoselvitys :laadintaperuste-fi] (:label-fi ilmastoselvitys-laadintaperuste))
-          (assoc-in [:ilmastoselvitys :laadintaperuste-sv] (:label-sv ilmastoselvitys-laadintaperuste))
           (update-in [:tulokset :kaytettavat-energiamuodot] (partial merge (energiamuotokertoimet versio)))
           (update-in [:tulokset :kuukausierittely] kuukausierittely-hyodynnetty)
           (assoc-kuukausierittely-summat)
@@ -539,8 +533,7 @@
    :ilmanvaihtotyypit        (luokittelu/find-ilmanvaihtotyypit db)
    :lammitysmuodot           (luokittelu/find-lammitysmuodot db)
    :lammonjaot               (luokittelu/find-lammonjaot db)
-   :havainnointikayntityypit (luokittelu/find-havainnointikayntityypit db)
-   :ilmastoselvitys-laadintaperuste (luokittelu/find-ilmastoselvitys-laadintaperusteet db)})
+   :havainnointikayntityypit (luokittelu/find-havainnointikayntityypit db)})
 
 (defn find-complete-energiatodistus
   ([db id]
