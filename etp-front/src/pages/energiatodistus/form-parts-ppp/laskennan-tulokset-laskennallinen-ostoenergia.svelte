37699017ef0a5c8c7869c7719673e612d8490580
Joonas Sarajärvi <joonas.sarajarvi@solita.fi>
2026-03-09 14:04:12 +0200

    AE-2760 E-luokka in PPP laskennallinen ostoenergia

--- etp-front/src/pages/energiatodistus/form-parts-ppp/laskennan-tulokset-laskennallinen-ostoenergia.svelte
+++ etp-front/src/pages/energiatodistus/form-parts-ppp/laskennan-tulokset-laskennallinen-ostoenergia.svelte
@@ -3,7 +3,9 @@
 
   import * as Maybe from '@Utility/maybe-utils';
   import * as EitherMaybe from '@Utility/either-maybe.js';
+  import * as Future from '@Utility/future-utils';
   import * as ETUtils from '@Pages/energiatodistus/energiatodistus-utils';
+  import * as api from '@Pages/energiatodistus/energiatodistus-api';
   import * as PPPUtils from './ppp-utils';
 
   import H4 from '@Component/H/H4';
@@ -22,6 +24,84 @@
         vaihe[pppLaskennallinenEnergiamuoto]
       ])
     )(PPPUtils.energiamuodot);
+
+  /**
+   * Returns Maybe of [apiParams, Future] where the Future resolves to the
+   * e-luokka letter (A–G). None when input data is insufficient.
+   */
+  const fetchVaiheEluokka = (
+    versio,
+    kayttotarkoitus,
+    nettoala,
+    vaiheEnergiamuodot
+  ) => {
+    const eLuku = ETUtils.eluku(versio, nettoala, vaiheEnergiamuodot);
+
+    return R.map(
+      params => [
+        params,
+        R.compose(
+          R.map(R.prop('e-luokka')),
+          R.apply(api.getEluokka(fetch, versio))
+        )(params)
+      ],
+      Maybe.toMaybeList([kayttotarkoitus, EitherMaybe.toMaybe(nettoala), eLuku])
+    );
+  };
+
+  // Vaihe index → e-luokka letter (A–G) retrieved from the e-luokka API.
+  // Displayed in the template.
+  let eluokkaPerVaihe = {};
+  // Vaihe index → [kayttotarkoitus, nettoala, eLuku] from the last API call.
+  // Used to skip redundant fetches when reactive updates fire.
+  let eluokkaParamsPerVaihe = {};
+
+  const updateEluokat = (et, vaiheet) => {
+    vaiheet.forEach((vaihe, vaiheIndex) => {
+      const hasAlkuPvm = Maybe.isSome(
+        EitherMaybe.toMaybe(vaihe.tulokset['vaiheen-alku-pvm'])
+      );
+
+      // Attempt to build the API call for this vaihe.
+      // Returns None if alku-pvm is missing or data is insufficient for e-luku.
+      const result = hasAlkuPvm
+        ? fetchVaiheEluokka(
+            et.versio,
+            et.perustiedot.kayttotarkoitus,
+            et.lahtotiedot['lammitetty-nettoala'],
+            etEnergiamuodotFromPppVaihe(vaihe.tulokset)
+          )
+        : Maybe.None();
+
+      Maybe.fold(
+        // None: clear any previously fetched e-luokka for this vaihe
+        () => {
+          eluokkaPerVaihe = R.dissoc(String(vaiheIndex), eluokkaPerVaihe);
+          eluokkaParamsPerVaihe = R.dissoc(
+            String(vaiheIndex),
+            eluokkaParamsPerVaihe
+          );
+        },
+        // Some: fetch e-luokka, but only if API params changed since last call
+        ([params, eluokkaFuture]) => {
+          if (!R.equals(params, eluokkaParamsPerVaihe[vaiheIndex])) {
+            eluokkaParamsPerVaihe[vaiheIndex] = params;
+            Future.fork(
+              _ => {},
+              eluokka => {
+                eluokkaPerVaihe[vaiheIndex] = eluokka;
+                eluokkaPerVaihe = eluokkaPerVaihe; // trigger Svelte reactivity
+              },
+              eluokkaFuture
+            );
+          }
+        },
+        result
+      );
+    });
+  };
+
+  $: updateEluokat(energiatodistus, perusparannuspassi.vaiheet);
 </script>
 
 <H4
@@ -97,12 +177,13 @@
       </tr>
     {/each}
     <tr class="et-table--tr">
-      <td class="et-table--td">TODO E-luokka</td>
-      <td class="et-table--td">TODO E-luokka</td>
-      {#each perusparannuspassi.vaiheet as vaihe}
+      <td class="et-table--td">{$_('energiatodistus.tulokset.e-luokka')}</td>
+      <td class="et-table--td"
+        >{Maybe.orSome('', energiatodistus.tulokset['e-luokka'])}</td>
+      {#each perusparannuspassi.vaiheet as vaihe, vaiheIndex}
         <td class="et-table--td">
           {#if Maybe.isSome(EitherMaybe.toMaybe(vaihe.tulokset['vaiheen-alku-pvm']))}
-            TODO E-luokka
+            {R.defaultTo('', eluokkaPerVaihe[vaiheIndex])}
           {/if}
         </td>
       {/each}
