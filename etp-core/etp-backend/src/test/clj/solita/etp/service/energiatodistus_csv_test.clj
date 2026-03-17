4d074b70d627aea26ff553582a048a69f1244547
Juho Haapakoski <juho.haapakoski@solita.fi>
2026-03-17 14:58:03 +0200

    AE-2622: Add new fields to csv

--- etp-core/etp-backend/src/test/clj/solita/etp/service/energiatodistus_csv_test.clj
+++ etp-core/etp-backend/src/test/clj/solita/etp/service/energiatodistus_csv_test.clj
@@ -67,26 +67,45 @@
     ;; Tests that all paths in generated energiatodistus are
     ;; found in columns listed in the service. Basically for finding typos
     ;; in configuration.
-    (t/is (every? #(contains? (set service/private-columns) %)
-                  (->> (energiatodistus-test-data/generate-adds 100 2018 true)
-                       (map #(complete-energiatodistus-service/complete-energiatodistus
-                               %
-                               luokittelut))
-                       (map #(dissoc % :kommentti :laskutusosoite-id))
-                       (map #(xmap/dissoc-in % [:tulokset :kuukausierittely]))
-                       (map #(xmap/dissoc-in % [:tulokset
-                                                :kuukausierittely-summat]))
-                       (map #(xmap/dissoc-in % [:perustiedot :yritys :katuosoite]))
-                       (map #(xmap/dissoc-in % [:perustiedot :yritys :postinumero]))
-                       (map #(xmap/dissoc-in % [:perustiedot :yritys :postitoimipaikka]))
-                       (map #(xmap/dissoc-in % [:tulokset :e-luokka-rajat]))
-                       (map #(xmap/dissoc-in % [:lahtotiedot :lammitys :lampohavio-lammittamaton-tila]))
-                       (map #(xmap/dissoc-in % [:perustiedot :havainnointikayntityyppi-fi]))
-                       (map #(xmap/dissoc-in % [:perustiedot :havainnointikayntityyppi-sv]))
-                       (map #(xmap/dissoc-in % [:perustiedot :havainnointikayntityyppi-id]))
-                       (map xmap/paths)
-                       (apply concat)
-                       (filter #(every? keyword? %)))))))
+    (t/testing "ET2018 paths are covered by private-columns"
+      (t/is (every? #(contains? (set service/private-columns) %)
+                    (->> (energiatodistus-test-data/generate-adds 10 2018 true)
+                         (map #(complete-energiatodistus-service/complete-energiatodistus
+                                 %
+                                 luokittelut))
+                         (map #(dissoc % :kommentti :laskutusosoite-id))
+                         (map #(xmap/dissoc-in % [:tulokset :kuukausierittely]))
+                         (map #(xmap/dissoc-in % [:tulokset
+                                                  :kuukausierittely-summat]))
+                         (map #(xmap/dissoc-in % [:perustiedot :yritys :katuosoite]))
+                         (map #(xmap/dissoc-in % [:perustiedot :yritys :postinumero]))
+                         (map #(xmap/dissoc-in % [:perustiedot :yritys :postitoimipaikka]))
+                         (map #(xmap/dissoc-in % [:tulokset :e-luokka-rajat]))
+                         (map #(xmap/dissoc-in % [:lahtotiedot :lammitys :lampohavio-lammittamaton-tila]))
+                         (map xmap/paths)
+                         (apply concat)
+                         (filter #(every? keyword? %))))))
+
+    ;; Tests that all paths in generated ET2026 energiatodistus are
+    ;; found in columns listed in the service.
+    (t/testing "ET2026 paths are covered by private-columns"
+      (t/is (every? #(contains? (set service/private-columns) %)
+                    (->> (energiatodistus-test-data/generate-adds 1 2026 true)
+                         (map #(complete-energiatodistus-service/complete-energiatodistus
+                                 %
+                                 luokittelut))
+                         (map #(dissoc % :kommentti :laskutusosoite-id))
+                         (map #(xmap/dissoc-in % [:tulokset :kuukausierittely]))
+                         (map #(xmap/dissoc-in % [:tulokset
+                                                  :kuukausierittely-summat]))
+                         (map #(xmap/dissoc-in % [:perustiedot :yritys :katuosoite]))
+                         (map #(xmap/dissoc-in % [:perustiedot :yritys :postinumero]))
+                         (map #(xmap/dissoc-in % [:perustiedot :yritys :postitoimipaikka]))
+                         (map #(xmap/dissoc-in % [:tulokset :e-luokka-rajat]))
+                         (map #(xmap/dissoc-in % [:lahtotiedot :lammitys :lampohavio-lammittamaton-tila]))
+                         (map xmap/paths)
+                         (apply concat)
+                         (filter #(every? keyword? %))))))))
 
 (t/deftest column-ks->str-test
   (t/is (= "" (service/column-ks->str [])))
@@ -136,4 +155,83 @@
         (let [et-without-ppp-line (->> csv-lines-strings
                                        (filter #(str/starts-with? % (str (:et-id ppp-removed-case))))
                                        first)]
-          (t/is (str/starts-with? et-without-ppp-line (str (:et-id ppp-removed-case) ";;"))))))))
+          (t/is (str/starts-with? et-without-ppp-line (str (:et-id ppp-removed-case) ";;")))))
+
+      ;; --- AE-2622: ET2026 new field columns ---
+
+      (t/testing "Header contains ET2026 ilmastoselvitys columns"
+        (t/is (str/includes? header-line "Ilmastoselvitys / Laatimisajankohta"))
+        (t/is (str/includes? header-line "Ilmastoselvitys / Hiilijalanjalki / Rakennus / Rakennustuotteiden-valmistus"))
+        (t/is (str/includes? header-line "Ilmastoselvitys / Hiilikadenjalki / Rakennus / Uudelleenkaytto")))
+
+      (t/testing "Header contains ET2026 uusiutuvat omavaraisenergiat kokonaistuotanto columns"
+        (t/is (str/includes? header-line "Tulokset / Uusiutuvat-omavaraisenergiat-kokonaistuotanto / Aurinkosahko")))
+
+      (t/testing "Header contains ET2026 toteutunut ostoenergiankulutus new columns"
+        (t/is (str/includes? header-line "Toteutunut-ostoenergiankulutus / Tietojen-alkuperavuosi")))
+
+      (t/testing "Header contains kasvihuonepaastojen-muutos in huomiot toimenpide columns"
+        (t/is (str/includes? header-line "Kasvihuonepaastojen-muutos")))
+
+      (t/testing "Header contains havainnointikayntityyppi columns"
+        (t/is (str/includes? header-line "Perustiedot / Havainnointikayntityyppi-id")))
+
+      (t/testing "Header contains lahtotiedot new boolean columns"
+        (t/is (str/includes? header-line "Lahtotiedot / Energiankulutuksen-valmius-reagoida-ulkoisiin-signaaleihin"))
+        (t/is (str/includes? header-line "Lahtotiedot / Lammitys / Lammonjako-lampotilajousto")))
+
+      (t/testing "Header contains lammitys kayttoikaa-jaljella-arvio-vuosina column"
+        (t/is (str/includes? header-line "Huomiot / Lammitys / Kayttoikaa-jaljella-arvio-vuosina")))
+
+      (t/testing "Every data row has the same number of columns as the header"
+        (let [;; Strip quoted CSV values (which may contain ;) before splitting
+              strip-quotes #(str/replace % #"\"(?:[^\"]|\"\")*\"" "X")
+              header-col-count (count (str/split (strip-quotes header-line) #";" -1))
+              data-lines (rest csv-lines-strings)]
+          (t/is (every? #(= header-col-count
+                            (count (str/split (strip-quotes %) #";" -1)))
+                        data-lines))))
+
+      (t/testing "ET2026 row has non-empty ilmastoselvitys laadintaperuste value"
+        (let [headers (mapv #(str/replace % #"\"" "") (str/split header-line #";"))
+              laadintaperuste-idx (.indexOf headers "Ilmastoselvitys / Laadintaperuste")
+              ;; Find a 2026 row - versio is the 3rd column (index 2)
+              et2026-line (->> (rest csv-lines-strings)
+                              (filter #(let [cols (str/split % #";" -1)]
+                                         (= "2026" (nth cols 2 nil))))
+                              first)
+              et2026-cols (when et2026-line (str/split et2026-line #";" -1))]
+          (t/is (pos? laadintaperuste-idx)
+                "Ilmastoselvitys / Laadintaperuste column should exist in headers")
+          (t/is (some? et2026-line)
+                "There should be at least one ET2026 row in the CSV")
+          (when (and et2026-cols (pos? laadintaperuste-idx))
+            (t/is (not (str/blank? (nth et2026-cols laadintaperuste-idx "")))
+                  "ET2026 row should have a non-empty ilmastoselvitys laadintaperuste value"))))
+
+      (t/testing "ET2018 row has empty ilmastoselvitys laadintaperuste value"
+        (let [headers (mapv #(str/replace % #"\"" "") (str/split header-line #";"))
+              laadintaperuste-idx (.indexOf headers "Ilmastoselvitys / Laadintaperuste")
+              ;; Find a 2018 row
+              et2018-line (->> (rest csv-lines-strings)
+                              (filter #(let [cols (str/split % #";" -1)]
+                                         (= "2018" (nth cols 2 nil))))
+                              first)
+              et2018-cols (when et2018-line (str/split et2018-line #";" -1))]
+          (when (and et2018-cols (pos? laadintaperuste-idx))
+            (t/is (str/blank? (nth et2018-cols laadintaperuste-idx ""))
+                  "ET2018 row should have empty ilmastoselvitys laadintaperuste value"))))
+
+      (t/testing "ET2026 row has non-empty kayttoikaa-jaljella-arvio-vuosina value"
+        (let [headers (mapv #(str/replace % #"\"" "") (str/split header-line #";"))
+              kayttoikaa-idx (.indexOf headers "Huomiot / Lammitys / Kayttoikaa-jaljella-arvio-vuosina")
+              et2026-line (->> (rest csv-lines-strings)
+                              (filter #(let [cols (str/split % #";" -1)]
+                                         (= "2026" (nth cols 2 nil))))
+                              first)
+              et2026-cols (when et2026-line (str/split et2026-line #";" -1))]
+          (t/is (pos? kayttoikaa-idx)
+                "Huomiot / Lammitys / Kayttoikaa-jaljella-arvio-vuosina column should exist")
+          (when (and et2026-cols (pos? kayttoikaa-idx))
+            (t/is (not (str/blank? (nth et2026-cols kayttoikaa-idx "")))
+                  "ET2026 row should have a non-empty kayttoikaa-jaljella-arvio-vuosina value")))))))
