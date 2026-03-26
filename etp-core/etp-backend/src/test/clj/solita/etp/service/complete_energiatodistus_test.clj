ae997356f5c1611dc6f472119caac5d0c73bdbd6
Joonas Sarajärvi <joonas.sarajarvi@solita.fi>
2026-03-26 18:23:39 +0200

    AE-2774 Tests

--- etp-core/etp-backend/src/test/clj/solita/etp/service/complete_energiatodistus_test.clj
+++ etp-core/etp-backend/src/test/clj/solita/etp/service/complete_energiatodistus_test.clj
@@ -116,3 +116,70 @@
     (t/is (= (etp-test/catch-ex-data
               #(service/find-complete-energiatodistus ts/*db* {:rooli 3} id))
              {:type :forbidden}))))
+
+(t/deftest co2-paastot-et-test
+  (t/testing "Laskee CO2-päästöt oikein kaikilla energiamuodoilla"
+    ;; 100*0.059 + 200*0.05 + 50*0.027 + 30*0.306 + 40*0.014 = 26.99
+    (t/is (== 26.99
+              (service/co2-paastot-et {:kaukolampo 100.0
+                                       :sahko 200.0
+                                       :uusiutuva-polttoaine 50.0
+                                       :fossiilinen-polttoaine 30.0
+                                       :kaukojaahdytys 40.0}))))
+
+  (t/testing "Puuttuvat energiamuodot käsitellään nollina"
+    ;; 1000*0.059 + 2000*0.05 = 59.0 + 100.0 = 159.0
+    (t/is (== 159.0
+              (service/co2-paastot-et {:kaukolampo 1000.0
+                                       :sahko 2000.0}))))
+
+  (t/testing "Palauttaa 0.0 nil-syötteellä"
+    (t/is (== 0.0 (service/co2-paastot-et nil)))))
+
+(t/deftest kasvihuonepaastot-in-complete-energiatodistus-test
+  (let [laatijat (laatija-test-data/generate-and-insert! 1)
+        laatija-id (-> laatijat keys sort first)
+        energiatodistus-adds (concat
+                              (energiatodistus-test-data/generate-adds 1 2013 true)
+                              (energiatodistus-test-data/generate-adds 1 2018 true)
+                              (energiatodistus-test-data/generate-adds 1 2026 true))
+        energiatodistus-ids (energiatodistus-test-data/insert!
+                             energiatodistus-adds laatija-id)
+        luokittelut (service/luokittelut ts/*db*)]
+    (doseq [id energiatodistus-ids]
+      (let [et (energiatodistus-service/find-energiatodistus ts/*db* id)
+            completed (service/complete-energiatodistus et luokittelut)
+            kaytettavat (-> completed :tulokset :kaytettavat-energiamuodot)
+            expected-co2 (service/co2-paastot-et kaytettavat)
+            nettoala (-> completed :lahtotiedot :lammitetty-nettoala)
+            kasvihuonepaastot (-> completed :tulokset :kasvihuonepaastot)
+            kasvihuonepaastot-nettoala (-> completed :tulokset :kasvihuonepaastot-nettoala)]
+        (t/testing (str "versio " (:versio completed) " id " id)
+          (t/is (some? kasvihuonepaastot)
+                "kasvihuonepaastot pitää olla laskettu")
+          (t/is (== expected-co2 kasvihuonepaastot)
+                "kasvihuonepaastot vastaa co2-paastot-et tulosta")
+          (when (and nettoala (pos? nettoala))
+            (t/is (some? kasvihuonepaastot-nettoala)
+                  "kasvihuonepaastot-nettoala pitää olla laskettu kun nettoala > 0")
+            (t/is (== (/ (double expected-co2) (double nettoala))
+                      (double kasvihuonepaastot-nettoala))
+                  "kasvihuonepaastot-nettoala = kasvihuonepaastot / nettoala")))))))
+
+(t/deftest kasvihuonepaastot-with-null-nettoala-test
+  (let [laatijat (laatija-test-data/generate-and-insert! 1)
+        laatija-id (-> laatijat keys sort first)
+        energiatodistus-adds (energiatodistus-test-data/generate-adds 1 2018 true)
+        [id] (energiatodistus-test-data/insert! energiatodistus-adds laatija-id)
+        luokittelut (service/luokittelut ts/*db*)]
+    (jdbc/execute!
+     ts/*db*
+     ["UPDATE energiatodistus SET lt$lammitetty_nettoala = NULL where id = ?" id])
+    (let [et (energiatodistus-service/find-energiatodistus ts/*db* id)
+          completed (service/complete-energiatodistus et luokittelut)
+          kasvihuonepaastot (-> completed :tulokset :kasvihuonepaastot)
+          kasvihuonepaastot-nettoala (-> completed :tulokset :kasvihuonepaastot-nettoala)]
+      (t/is (some? kasvihuonepaastot)
+            "kasvihuonepaastot lasketaan myös ilman nettoalaa")
+      (t/is (nil? kasvihuonepaastot-nettoala)
+            "kasvihuonepaastot-nettoala on nil kun nettoala puuttuu"))))
