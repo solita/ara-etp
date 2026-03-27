3e90ad0354ac128e1310a8895b7a47d97f566901
solita-areta <areta.santos@solita.fi>
2026-03-27 13:49:36 +0200

    AE-2770: Add new laatimisvaiheet and needed validations

--- etp-core/etp-db/src/main/sql/migration/repeatable/r-laatimisvaihe.sql
+++ etp-core/etp-db/src/main/sql/migration/repeatable/r-laatimisvaihe.sql
@@ -1,8 +1,10 @@
 insert into laatimisvaihe (id, label_fi, label_sv)
 values
-(0, 'Rakennuslupa', 'Bygglov'),
-(1, 'Käyttöönotto', 'Införandet'),
-(2, 'Olemassa oleva rakennus', 'Befintlig byggnad')
+(0, 'Rakennuslupavaihe, uudisrakennus', 'Bygglov'),
+(1, 'Käyttöönottovaihe, uudisrakennus', 'Införandet'),
+(2, 'Olemassa oleva rakennus', 'Befintlig byggnad'),
+(3, 'Rakennuslupavaihe, laajamittainen perusparannus', 'Rakennuslupavaihe, laajamittainen perusparannus (sv)'),
+(4, 'Käyttöönottovaihe, laajamittainen perusparannus', 'Käyttöönottovaihe, laajamittainen perusparannus (sv)')
 on conflict (id) do update set
   label_fi = excluded.label_fi,
   label_sv = excluded.label_sv
