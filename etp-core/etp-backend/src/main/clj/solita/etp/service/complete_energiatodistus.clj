f67507c0ed6bed97072177587c57974acc3f172f
solita-areta <areta.santos@solita.fi>
2026-04-02 13:43:23 +0300

    AE-2770: update to match main

--- etp-core/etp-backend/src/main/clj/solita/etp/service/complete_energiatodistus.clj
+++ etp-core/etp-backend/src/main/clj/solita/etp/service/complete_energiatodistus.clj
@@ -103,6 +103,16 @@
     (partial map/map-keys #(-> % name (str "-kerroin") keyword))
     e-luokka-service/energiamuotokerroin))
 
+(defn co2-paastot-et
+  [tulokset]
+  (if tulokset
+   (+ (* (or (:kaukolampo tulokset) 0) (:kaukolampo co2-kertoimet))
+          (* (or (:sahko tulokset) 0) (:sahko co2-kertoimet))
+          (* (or (:uusiutuva-polttoaine tulokset) 0) (:uusiutuvat-pat co2-kertoimet))
+          (* (or (:fossiilinen-polttoaine tulokset) 0) (:fossiiliset-pat co2-kertoimet))
+          (* (or (:kaukojaahdytys tulokset) 0) (:kaukojaahdytys co2-kertoimet)))
+    0.0))
+
 (defn complete-energiatodistus
   [energiatodistus {:keys [postinumerot kielisyydet laatimisvaiheet
                            kayttotarkoitukset alakayttotarkoitukset
@@ -520,7 +530,10 @@
                         0
                         [:lahtotiedot :lammitys :lampohavio-lammittamaton-tila]
                         [:lahtotiedot :lammitys :tilat-ja-iv :lampohavio-lammittamaton-tila]
-                        [:lahtotiedot :lammitys :lammin-kayttovesi :lampohavio-lammittamaton-tila])))))
+                        [:lahtotiedot :lammitys :lammin-kayttovesi :lampohavio-lammittamaton-tila])
+          (#(assoc-in % [:tulokset :kasvihuonepaastot]
+                      (co2-paastot-et (get-in % [:tulokset :kaytettavat-energiamuodot]))))
+          (assoc-div-nettoala [:tulokset :kasvihuonepaastot])))))
 
 (defn luokittelut [db]
   {:postinumerot             (geo/find-all-postinumerot db)
@@ -544,16 +557,6 @@
    (some->
      (energiatodistus-service/find-energiatodistus db whoami id)
      (complete-energiatodistus (luokittelut db)))))
-
-(defn co2-paastot-et
-  [tulokset]
-  (if tulokset
-   (+ (* (or (:kaukolampo tulokset) 0) (:kaukolampo co2-kertoimet))
-          (* (or (:sahko tulokset) 0) (:sahko co2-kertoimet))
-          (* (or (:uusiutuva-polttoaine tulokset) 0) (:uusiutuvat-pat co2-kertoimet))
-          (* (or (:fossiilinen-polttoaine tulokset) 0) (:fossiiliset-pat co2-kertoimet))
-          (* (or (:kaukojaahdytys tulokset) 0) (:kaukojaahdytys co2-kertoimet)))
-    0.0))
 
 ;; These coefficients are defined independently in the spec:
 ;; "tuotettu sähkö kertoimella 0,9 ja tuotettu aurinkolämpö kertoimella 0,38"
