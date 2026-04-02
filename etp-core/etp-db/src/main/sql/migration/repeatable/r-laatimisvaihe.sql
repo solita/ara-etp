c0a5eb0cf10e7f1101996cb3b04fd1fc4f232224
solita-areta <areta.santos@solita.fi>
2026-04-02 08:27:41 +0300

    AE-2770: Update laatimisvaihe SQL scripts to support versioning and enhance label handling

--- etp-core/etp-db/src/main/sql/migration/repeatable/r-laatimisvaihe.sql
+++ etp-core/etp-db/src/main/sql/migration/repeatable/r-laatimisvaihe.sql
@@ -1,11 +1,21 @@
+-- Version 2018: 3 rows with short-form labels
 insert into laatimisvaihe (id, label_fi, label_sv, versio)
 values
-(0, 'Rakennuslupavaihe, uudisrakennus', 'Bygglov', 2018),
-(1, 'Käyttöönottovaihe, uudisrakennus', 'Införandet', 2018),
-(2, 'Olemassa oleva rakennus', 'Befintlig byggnad', 2018),
-(3, 'Rakennuslupavaihe, laajamittainen perusparannus', 'Rakennuslupavaihe, laajamittainen perusparannus (sv)', 2026),
-(4, 'Käyttöönottovaihe, laajamittainen perusparannus', 'Käyttöönottovaihe, laajamittainen perusparannus (sv)', 2026)
-on conflict (id) do update set
+(0, 'Rakennuslupa', 'Bygglov', 2018),
+(1, 'Käyttöönotto', 'Införandet', 2018),
+(2, 'Olemassa oleva rakennus', 'Befintlig byggnad', 2018)
+on conflict (id, versio) do update set
   label_fi = excluded.label_fi,
-  label_sv = excluded.label_sv,
-  versio = excluded.versio
+  label_sv = excluded.label_sv;
+
+-- Version 2026: 5 rows with long-form labels
+insert into laatimisvaihe (id, label_fi, label_sv, versio)
+values
+(0, 'Rakennuslupavaihe, uudisrakennus', 'Bygglovsfasen, nybyggnad', 2026),
+(1, 'Käyttöönottovaihe, uudisrakennus', 'Införandefasen, nybyggnad', 2026),
+(2, 'Olemassa oleva rakennus', 'Befintlig byggnad', 2026),
+(3, 'Rakennuslupavaihe, laajamittainen perusparannus', 'Bygglovsfasen, omfattande grundlig renovering', 2026),
+(4, 'Käyttöönottovaihe, laajamittainen perusparannus', 'Införandefasen, omfattande grundlig renovering', 2026)
+on conflict (id, versio) do update set
+  label_fi = excluded.label_fi,
+  label_sv = excluded.label_sv;
