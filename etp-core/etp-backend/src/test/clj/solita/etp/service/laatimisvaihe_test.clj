15d512807cf495e092f95bf6ba1e4fafa1f98368
solita-areta <areta.santos@solita.fi>
2026-04-07 12:26:09 +0300

    AE-2770: add tests for laatimisvaihe validation and filtering by version

--- etp-core/etp-backend/src/test/clj/solita/etp/service/laatimisvaihe_test.clj
+++ etp-core/etp-backend/src/test/clj/solita/etp/service/laatimisvaihe_test.clj
@@ -1,9 +1,13 @@
 (ns solita.etp.service.laatimisvaihe-test
   (:require [clojure.test :as t]
-            [solita.etp.service.laatimisvaihe :as laatimisvaihe]))
+            [solita.etp.service.laatimisvaihe :as laatimisvaihe]
+            [solita.etp.test :as etp-test]))
 
 (defn et-2018-in-vaihe [vaihe]
   {:versio 2018 :perustiedot {:laatimisvaihe vaihe}})
+
+(defn et-2026-in-vaihe [vaihe]
+  {:versio 2026 :perustiedot {:laatimisvaihe vaihe}})
 
 (defn et-2013 [uudisrakennus?]
   {:versio 2013 :perustiedot {:uudisrakennus uudisrakennus?}})
@@ -15,3 +19,53 @@
 
   (t/is (= (laatimisvaihe/olemassaoleva-rakennus? (et-2013 false)) true))
   (t/is (= (laatimisvaihe/olemassaoleva-rakennus? (et-2013 true)) false)))
+
+(t/deftest vaihe-key-existing-ids
+  (t/testing "given existing vaihe ids 0, 1, 2, when vaihe-key is called, then returns correct keys"
+    (t/is (= (laatimisvaihe/vaihe-key 0) :rakennuslupa))
+    (t/is (= (laatimisvaihe/vaihe-key 1) :kayttoonotto))
+    (t/is (= (laatimisvaihe/vaihe-key 2) :olemassaolevarakennus))))
+
+(t/deftest vaihe-key-new-perusparannus-ids
+  (t/testing "given new vaihe id 3, when vaihe-key is called, then returns :rakennuslupa-perusparannus"
+    (t/is (= (laatimisvaihe/vaihe-key 3) :rakennuslupa-perusparannus)))
+
+  (t/testing "given new vaihe id 4, when vaihe-key is called, then returns :kayttoonotto-perusparannus"
+    (t/is (= (laatimisvaihe/vaihe-key 4) :kayttoonotto-perusparannus))))
+
+(t/deftest validate-laatimisvaihe-valid-2018
+  (t/testing "given a 2018 energiatodistus with valid laatimisvaihe ids 0, 1, 2, when validating, then does not throw"
+    (doseq [vaihe-id [0 1 2]]
+      (t/is (nil? (laatimisvaihe/validate-laatimisvaihe-for-versio! (et-2018-in-vaihe vaihe-id)))))))
+
+(t/deftest validate-laatimisvaihe-invalid-2018-id-3
+  (t/testing "given a 2018 energiatodistus with laatimisvaihe 3, when validating, then throws :invalid-laatimisvaihe"
+    (let [result (etp-test/catch-ex-data
+                   #(laatimisvaihe/validate-laatimisvaihe-for-versio! (et-2018-in-vaihe 3)))]
+      (t/is (= (:type result) :invalid-laatimisvaihe)))))
+
+(t/deftest validate-laatimisvaihe-invalid-2018-id-4
+  (t/testing "given a 2018 energiatodistus with laatimisvaihe 4, when validating, then throws :invalid-laatimisvaihe"
+    (let [result (etp-test/catch-ex-data
+                   #(laatimisvaihe/validate-laatimisvaihe-for-versio! (et-2018-in-vaihe 4)))]
+      (t/is (= (:type result) :invalid-laatimisvaihe)))))
+
+(t/deftest validate-laatimisvaihe-valid-2026
+  (t/testing "given a 2026 energiatodistus with laatimisvaihe ids 0-4, when validating, then does not throw for any"
+    (doseq [vaihe-id [0 1 2 3 4]]
+      (t/is (nil? (laatimisvaihe/validate-laatimisvaihe-for-versio! (et-2026-in-vaihe vaihe-id)))))))
+
+(t/deftest validate-laatimisvaihe-2013-not-validated
+  (t/testing "given a 2013 energiatodistus, when validating, then does not throw (2013 doesn't use laatimisvaihe)"
+    (t/is (nil? (laatimisvaihe/validate-laatimisvaihe-for-versio! (et-2013 true))))))
+
+(t/deftest validate-laatimisvaihe-invalid-unknown-id
+  (t/testing "given a 2026 energiatodistus with laatimisvaihe 99, when validating, then throws :invalid-laatimisvaihe"
+    (let [result (etp-test/catch-ex-data
+                   #(laatimisvaihe/validate-laatimisvaihe-for-versio! (et-2026-in-vaihe 99)))]
+      (t/is (= (:type result) :invalid-laatimisvaihe)))))
+
+(t/deftest validate-laatimisvaihe-nil-does-not-throw
+  (t/testing "given a 2018 energiatodistus with nil laatimisvaihe, when validating, then does not throw (nil handled by validate-required!)"
+    (t/is (nil? (laatimisvaihe/validate-laatimisvaihe-for-versio!
+                  {:versio 2018 :perustiedot {:laatimisvaihe nil}})))))
--- etp-front/src/pages/energiatodistus/laatimisvaiheet_test.js
+++ etp-front/src/pages/energiatodistus/laatimisvaiheet_test.js
@@ -25,6 +25,34 @@ const et2013 = uudisrakennus => ({
   }
 });
 
+const allLaatimisvaiheet = [
+  {
+    id: 0,
+    'label-fi': 'Rakennuslupavaihe, uudisrakennus',
+    'label-sv': 'Bygglov'
+  },
+  {
+    id: 1,
+    'label-fi': 'Käyttöönottovaihe, uudisrakennus',
+    'label-sv': 'Införandet'
+  },
+  {
+    id: 2,
+    'label-fi': 'Olemassa oleva rakennus',
+    'label-sv': 'Befintlig byggnad'
+  },
+  {
+    id: 3,
+    'label-fi': 'Rakennuslupavaihe, laajamittainen perusparannus',
+    'label-sv': 'Rakennuslupavaihe, laajamittainen perusparannus (sv)'
+  },
+  {
+    id: 4,
+    'label-fi': 'Käyttöönottovaihe, laajamittainen perusparannus',
+    'label-sv': 'Käyttöönottovaihe, laajamittainen perusparannus (sv)'
+  }
+];
+
 describe('Laatimisvaiheet: ', () => {
   it('Olemassaoleva rakennus - 2018', () => {
     expect(Laatimisvaiheet.isOlemassaOlevaRakennus(et2018inVaihe(2))).toEqual(
@@ -52,5 +80,83 @@ describe('Laatimisvaiheet: ', () => {
     expect(Laatimisvaiheet.isOlemassaOlevaRakennus(et2013(true))).toEqual(
       false
     );
+  });
+});
+
+describe('filterByVersion: ', () => {
+  it('given version 2018 and all laatimisvaiheet, when filtering, then returns only ids 0, 1, 2', () => {
+    // given
+    const version = 2018;
+
+    // when
+    const result = Laatimisvaiheet.filterByVersion(version, allLaatimisvaiheet);
+
+    // then
+    expect(result).toEqual([
+      allLaatimisvaiheet[0],
+      allLaatimisvaiheet[1],
+      allLaatimisvaiheet[2]
+    ]);
+  });
+
+  it('given version 2026 and all laatimisvaiheet, when filtering, then returns all ids 0, 1, 2, 3, 4', () => {
+    // given
+    const version = 2026;
+
+    // when
+    const result = Laatimisvaiheet.filterByVersion(version, allLaatimisvaiheet);
+
+    // then
+    expect(result).toEqual(allLaatimisvaiheet);
+  });
+
+  it('given version 2018 and empty laatimisvaiheet, when filtering, then returns empty array', () => {
+    // given
+    const version = 2018;
+
+    // when
+    const result = Laatimisvaiheet.filterByVersion(version, []);
+
+    // then
+    expect(result).toEqual([]);
+  });
+
+  it('given version 2013, when filtering, then returns empty array', () => {
+    // given
+    const version = 2013;
+
+    // when
+    const result = Laatimisvaiheet.filterByVersion(version, allLaatimisvaiheet);
+
+    // then
+    expect(result).toEqual([]);
+  });
+
+  it('given version 2018 and partial laatimisvaiheet with ids 0 and 3, when filtering, then returns only id 0', () => {
+    // given
+    const version = 2018;
+    const partial = [allLaatimisvaiheet[0], allLaatimisvaiheet[3]];
+
+    // when
+    const result = Laatimisvaiheet.filterByVersion(version, partial);
+
+    // then
+    expect(result).toEqual([allLaatimisvaiheet[0]]);
+  });
+
+  it('given version 2018, when filtering, then returned objects preserve all original properties', () => {
+    // given
+    const version = 2018;
+
+    // when
+    const result = Laatimisvaiheet.filterByVersion(version, allLaatimisvaiheet);
+
+    // then
+    expect(result[0]).toHaveProperty('id', 0);
+    expect(result[0]).toHaveProperty(
+      'label-fi',
+      'Rakennuslupavaihe, uudisrakennus'
+    );
+    expect(result[0]).toHaveProperty('label-sv', 'Bygglov');
   });
 });
