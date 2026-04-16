af3ac3d0f179556482f6050a890b36ad91f26471
etahto <elias.tahto@solita.fi>
2026-04-16 10:16:42 +0300

    AE-2775: Update NVD configuration to disable OSS Index analyzer and adjust nvd.sh script

--- etp-core/etp-backend/nvd.sh
+++ etp-core/etp-backend/nvd.sh
@@ -9,4 +9,4 @@ if [ -z "$NVD_API_TOKEN" ]; then
 fi
 
 clojure -Ttools install nvd-clojure/nvd-clojure '{:mvn/version "5.0.0"}' :as nvd
-clojure -J-Dclojure.main.report=stderr -Tnvd nvd.task/check :classpath \""$(clojure -Spath)\"" :config-filename \""nvd-config.edn\"" :nvd-api '{:key "'"$NVD_API_TOKEN"'"}'
+clojure -J-Dclojure.main.report=stderr -J-Danalyzer.ossindex.enabled=false -Tnvd nvd.task/check :classpath \""$(clojure -Spath)\"" :config-filename \""nvd-config.edn\"" :nvd-api '{:key "'"$NVD_API_TOKEN"'"}'
