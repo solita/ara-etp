6d1319139ca3378cf82c68398c2902b0b5678164
solita-areta <areta.santos@solita.fi>
2026-06-04 09:23:52 +0300

    AE-2790: change to lowercase

--- etp-core/etp-db/src/main/sql/migration/v5-release-1.1/v5.66-uusiutuva-energia-lahtotilanne.sql
+++ etp-core/etp-db/src/main/sql/migration/v5-release-1.1/v5.66-uusiutuva-energia-lahtotilanne.sql
@@ -1,2 +1,2 @@
-ALTER TABLE Perusparannuspassi
-  ADD COLUMN rpt$uusiutuva_energia_lahtotilanne integer references perusparannuspassi_uusiutuva_energia(id);
+alter table perusparannuspassi
+  add column rpt$uusiutuva_energia_lahtotilanne integer references perusparannuspassi_uusiutuva_energia(id);
