e4cf77ff69e0298ba6e6208ca3c2b4c0990ddafb
solita-areta <areta.santos@solita.fi>
2026-07-06 09:41:28 +0300

    AE-2840: add new fields to the audit table

--- etp-core/etp-db/src/main/sql/migration/v5-release-1.1/v5.67-add-ilmastoselvitys-fields.sql
+++ etp-core/etp-db/src/main/sql/migration/v5-release-1.1/v5.67-add-ilmastoselvitys-fields.sql
@@ -1,3 +1,9 @@
 alter table Energiatodistus
     add column is$hiilikadenjalki$rakennus$hyodyntaminen_energiana numeric,
     add column is$hiilikadenjalki$rakennuspaikka$hyodyntaminen_energiana numeric;
+
+alter table audit.energiatodistus
+    add column is$hiilikadenjalki$rakennus$hyodyntaminen_energiana numeric,
+    add column is$hiilikadenjalki$rakennuspaikka$hyodyntaminen_energiana numeric;
+
+call audit.create_audit_procedure('energiatodistus'::name);
