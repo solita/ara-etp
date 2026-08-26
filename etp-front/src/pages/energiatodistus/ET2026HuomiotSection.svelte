52611a34f769a77bc404017e4c9935854f50861f
Juho Haapakoski <juho.haapakoski@solita.fi>
2026-08-26 14:12:40 +0300

    AE-2710: Add comment

--- etp-front/src/pages/energiatodistus/ET2026HuomiotSection.svelte
+++ etp-front/src/pages/energiatodistus/ET2026HuomiotSection.svelte
@@ -1,4 +1,6 @@
 <script>
+  // The purpose of this component is to handle the display of toimenpide-ehdotukset (huomiot)
+  // depending on whether the PPP is in use or not.
   import { _ } from '@Language/i18n';
   export let hasPpp = false;
 </script>
